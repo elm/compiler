37cab4561aed2f5d571128ac02954b37dc056f77
Abhinav Gupta <abhinav@Abhinavs-MacBook-Pro.local>
2012-04-27 23:57:11 -0700

    More HLint warnings

--- src/Replace.hs
+++ src/Replace.hs
@@ -1,8 +1,9 @@
 module Replace (replace, depth) where
 
 import Ast
-import Data.Set (singleton,empty,unions,member)
+import Data.Set (singleton,empty,unions,member, Set)
 
+replace :: String -> Expr -> Expr -> Expr
 replace y v expr =
     let f = replace y v in
     case expr of
@@ -14,22 +15,25 @@ replace y v expr =
       Lift e es -> Lift (f e) (map f es)
       Fold e1 e2 e3 -> Fold (f e1) (f e2) (f e3)
       Async e -> Async (f e)
-      Let defs e -> if elem y vs then Let defs e else Let (zip vs (map f es)) (f e)
+      Let defs e -> if y `elem` vs then Let defs e else Let (zip vs (map f es)) (f e)
               where (vs,es) = unzip defs
       Var x -> if x == y then v else Var x
       Data name es -> Data name (map f es)
-      Case e cases -> Case (f e) $ map (case_replace y v) cases
+      Case e cases -> Case (f e) $ map (caseReplace y v) cases
       _ -> expr
 
-case_replace y v (p,e) =
-    if member y (pattern_vars p) then (p,e) else (p, replace y v e)
+caseReplace :: String -> Expr -> (Pattern, Expr) -> (Pattern, Expr)
+caseReplace y v (p,e) =
+    if member y (patternVars p) then (p,e) else (p, replace y v e)
 
-pattern_vars pattern =
+patternVars :: Pattern -> Set String
+patternVars pattern =
     case pattern of
-      PData _ ps -> unions (map pattern_vars ps)
+      PData _ ps -> unions (map patternVars ps)
       PVar x -> singleton x
       PAnything -> empty
 
+depth :: Expr -> Integer
 depth = depth' 0
 depth' d expr =
     let f = depth' (d+1) in
