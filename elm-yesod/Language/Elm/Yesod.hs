4fa6d8051f2d8dbde0c0e6a610168c638f226468
Vincent Ambo <v.ambo@me.com>
2012-05-28 18:13:37 +0200

    * documentation for Language.Elm.Yesod

--- elm-yesod/Language/Elm/Yesod.hs
+++ elm-yesod/Language/Elm/Yesod.hs
@@ -1,5 +1,20 @@
 {-# LANGUAGE QuasiQuotes #-}
+{- | This module provides a function for compiling Elm source code into a Yesod widget.
+     In order to use this with your Yesod app, you need to define a defaultLayout like
+     function that embeds the elm-min.js file /in the <head> tag/.
 
+     For example, you could modify your Yesod instance as follows:
+
+   > instance Yesod App where
+   >    jsLoader _ = BottomOfHeadBlocking -- moves JS to the <head> tag
+   >    defaultLayout widget = do
+   >        pc <- widgetToPageContent $ do
+   >            addScriptRemote $ "http://somecdn.org/link/to/elm-min.js"
+   >            ...
+   >        ...
+
+     A full example implementation is provided in the examples folder of the Elm github repository.
+-}
 module Language.Elm.Yesod (generateWidget) where
 
 import Text.Blaze (preEscapedToMarkup)
@@ -21,7 +36,11 @@ a:link, a:visited, a:active { text-decoration: none}
 a:hover {text-decoration: underline; color: #ff8f12;}
 |]
 
-generateWidget :: String -> GWidget sub master ()
+-- |generateWidget takes some Elm code in String format and produces a widget. Usage example:
+-- 
+-- > generateWidget [elmFile|elm-source/somePage.elm|]
+generateWidget :: String -- ^ The Elm source code
+               -> GWidget sub master ()
 generateWidget source =
   let expr = initialize source
       js = compileToJS expr
