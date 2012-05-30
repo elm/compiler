c6f9c1804c525c93c4695dd61f96182a01ef192b
Vincent Ambo <v.ambo@me.com>
2012-05-30 21:33:19 +0200

    * updated elm-yesod example (URL interpolation)

--- Examples/elm-yesod/Main.hs
+++ Examples/elm-yesod/Main.hs
@@ -36,31 +36,34 @@ mkYesod "ElmTest" [parseRoutes|
 -- generateWidget is called with the result of an Elm QuasiQuoter (which is just a 
 -- string containing some Elm code, with proper newline escaping)
 getMouseR :: Handler RepHtml
-getMouseR = defaultLayout $ do
-    setTitle "Mouse position demo"
-    elmWidget mousePage
+getMouseR = do
+    widget <- elmWidget mousePage
+    defaultLayout $ do
+      setTitle "Mouse position demo"
+      widget
 
 getClockR :: Handler RepHtml
-getClockR = defaultLayout $ do
-    setTitle "A clock"
-    elmWidget clockPage
+getClockR = do 
+    widget <- elmWidget clockPage
+    defaultLayout $ do
+      setTitle "A clock"
+      widget
 
 getShapesR :: Handler RepHtml
-getShapesR = defaultLayout $ do
-    setTitle "Simple shapes"
-    elmWidget shapesPage
+getShapesR = do
+    widget <- elmWidget shapesPage
+    defaultLayout $ do
+      setTitle "Simple shapes"
+      widget
 
 -- URLs are rendered manually and then passed on to the function containing
 -- the elm QuasiQuoter.
 getRootR :: Handler RepHtml
 getRootR = do
-    render <- getUrlRender
+    widget <- elmWidget $(elmFile "elm_source/index.elm")
     defaultLayout $ do
-      let mouse = render MouseR
-          clock = render ClockR
-          shapes = render ShapesR
       setTitle "Welcome!"
-      elmWidget $(elmFile "elm_source/index.elm")
+      widget
 
 
 -- Our Yesod instance contains the default layout, which inserts the elm-min.js
