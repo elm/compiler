e68982f50a87a23b94042386002e26165c0672fd
evancz <info@elm-lang.org>
2012-05-30 15:53:08 -0500

    Add a note about Elm's online examples in the docs.

--- elm/src/Language/Elm.hs
+++ elm/src/Language/Elm.hs
@@ -6,7 +6,8 @@
      understands. The provided instances for String, Text and QuasiQuoted Elm source code 
      should be sufficient.
 
-     The documentation for the Elm language is available at <http://elm-lang.org/Documentation.elm>
+     The documentation for the Elm language is available at <http://elm-lang.org/Documentation.elm>,
+     and many interactive examples are available at <http://elm-lang.org/Examples.elm>
 
      Example implementations using Yesod and Happstack are available at <https://github.com/tazjin/Elm/tree/master/Examples>
 -}
