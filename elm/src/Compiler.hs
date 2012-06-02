5dada7dfe03a006a2b5fd87254befd14488de29f
evancz <info@elm-lang.org>
2012-06-02 15:53:43 -0500

    Bump version number.

--- elm/src/Compiler.hs
+++ elm/src/Compiler.hs
@@ -11,7 +11,7 @@ main = getArgs >>= parse
 
 parse :: [String] -> IO ()
 parse ("--help":_) = putStrLn usage
-parse ("--version":_) = putStrLn "The Elm Compiler 0.1.2"
+parse ("--version":_) = putStrLn "The Elm Compiler 0.1.2.1"
 parse [loc,file]
   | "--runtime-location=" `isPrefixOf` loc =
       produceHtml (tail $ dropWhile (/='=') loc) file
