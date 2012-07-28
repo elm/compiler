45f834e0fc59f4fb42c6e09e9c4f2a73b16a261c
evancz <info@elm-lang.org>
2012-07-28 20:45:42 +0200

    Add context to type constraints for better error messages. Fix big with generated constraints in which some constraints could be ignored.

--- elm/src/Types/Constrain.hs
+++ elm/src/Types/Constrain.hs
@@ -6,7 +6,7 @@ import Types
 import Data.List (foldl')
 import qualified Data.Set as Set
 import qualified Data.Map as Map
-import Control.Monad (liftM,mapM)
+import Control.Monad (liftM,mapM,zipWithM)
 import Control.Monad.State (evalState)
 import Guid
 
@@ -14,53 +14,64 @@ beta = VarT `liftM` guid
 unionA = Map.unionWith (++)
 unionsA = Map.unionsWith (++)
 
+data Context a b = Context a b deriving (Eq,Show)
+ctx e = Context e
+
+instance (Ord a, Eq c) => Ord (Context c a) where
+    compare (Context _ x) (Context _ y) = compare x y
+
 constrain hints expr = do
     (as,cs,t) <- inference expr
-    let cMap = Map.intersectionWith (\s -> map (\v -> VarT v:<<: s)) (Map.fromList hints) as
-    return $ Set.toList cs ++ (concat . map snd $ Map.toList cMap)
+    let cMap = Map.intersectionWithKey combine (Map.fromList hints) as
+            where combine k s vs = map (\v -> ctx (Var k) $ VarT v :<<: s) vs
+    return (Set.toList cs ++ (concatMap snd $ Map.toList cMap))
 
-inference :: Expr -> GuidCounter (Map.Map String [X], Set.Set Constraint, Type)
+inference :: Expr -> GuidCounter (Map.Map String [X], Set.Set (Context Expr Constraint), Type)
 inference (Var x) =
     do b <- guid
        return (Map.singleton x [b], Set.empty, VarT b)
-inference (App e1 e2) =
+inference e@(App e1 e2) =
     do (a1,c1,t1) <- inference e1
        (a2,c2,t2) <- inference e2
        b <- beta
        return ( unionA a1 a2
-              , Set.unions [c1,c2,Set.singleton $ t1 :=: (LambdaT t2 b)]
+              , Set.unions [c1,c2,Set.singleton . ctx e $ t1 :=: (LambdaT t2 b)]
               , b )
 inference (Lambda x e) =
     do (a,c,t) <- inference e
        b <- beta
+       v <- guid
        return ( Map.delete x a
-              , Set.union c . Set.fromList . map (\x -> VarT x :=: b) $
-                          Map.findWithDefault [] x a
+              , Set.union c . Set.fromList . map (\x -> ctx e $ VarT x :=: b) $
+                          Map.findWithDefault [v] x a
               , LambdaT b t )
 inference (Let defs e) =
     do (as,cs,t) <- inference e
        (ass, schemes) <- liftM unzip (mapM defScheme defs)
        let assumptions = unionsA (as:ass)
        let names = map (\(Definition x _ _) -> x) defs
-       let genCs name s = map (\x -> VarT x :<<: s) $ Map.findWithDefault [] name assumptions
-       let cs' = Set.fromList . concat $ zipWith genCs names schemes
+       let genCs name s = do
+             v <- guid
+             return . map (\x -> ctx (Var name) $ VarT x :<<: s) $ Map.findWithDefault [v] name assumptions
+       cs' <- zipWithM genCs names schemes
        return ( foldr Map.delete assumptions names
-              , Set.union cs' cs
+              , Set.union (Set.fromList . concat $ cs') cs
               , t )
 
-inference (Case e cases) =
+inference ce@(Case e cases) =
     do (as,cs,t) <- inference e
        (ass,css,ts) <- liftM unzip3 $ mapM (caseInference t) cases
        return ( unionsA $ as:ass
-              , Set.unions $ Set.fromList (zipWith (:=:) ts $ tail ts) : cs : css
+              , let combine t1 t2 = ctx ce (t1 :=: t2) in
+                Set.unions $ Set.fromList (zipWith combine ts $ tail ts) : cs : css
               , head ts)
 
-inference (If e1 e2 e3) =
+inference e@(If e1 e2 e3) =
     do (a1,c1,t1) <- inference e1
        (a2,c2,t2) <- inference e2
        (a3,c3,t3) <- inference e3
        return ( unionsA [a1,a2,a3]
-              , Set.unions [c1,c2,c3, Set.fromList [ t1 :=: bool, t2 :=: t3 ] ]
+              , Set.unions [c1,c2,c3, Set.fromList [ ctx e1 (t1 :=: bool), ctx e (t2 :=: t3) ] ]
               , t2 )
 
 inference (Data name es) = inference $ foldl' App (Var name) es
@@ -71,7 +82,7 @@ inference (Range e1 e2) = inference (Var "elmRange" `App` e1 `App` e2)
 inference other =
     case other of
       IntNum _ -> do t <- beta
-                     return (Map.empty, Set.singleton (t :<: number), t)
+                     return (Map.empty, Set.singleton (ctx other $ t :<: number), t)
       FloatNum _ -> primitive float
       Chr _ -> primitive char
       Str _ -> primitive string
@@ -93,8 +104,11 @@ namesIn (PData name ps) = concatMap namesIn ps
 defScheme (Definition name args e) =
     do argDict <- mapM (\a -> liftM ((,) a) guid) args 
        (as,cs,t) <- inference e
-       let genCs (arg,x) = map (\y -> VarT x :=: VarT y) $ Map.findWithDefault [] arg as
-       let cs' = concatMap genCs argDict
+       let genCs (arg,x) = do
+             v <- guid
+             return . map (\y -> VarT x :=: VarT y) $ Map.findWithDefault [v] arg as
+       cs' <- concat `liftM` mapM genCs argDict
+       let cs'' = map (\(Context _ c) -> c) (Set.toList cs)
        let tipe = foldr (==>) t $ map (VarT . snd) argDict
-       return (foldr Map.delete as args
-              , Forall (map snd argDict) (cs' ++ Set.toList cs) tipe)
+       return ( foldr Map.delete as args
+              , Forall (map snd argDict) (cs' ++ cs'') tipe)
--- elm/src/Types/Unify.hs
+++ elm/src/Types/Unify.hs
@@ -1,5 +1,5 @@
 
-module Unify where
+module Unify (unify) where
 
 import Constrain
 import Control.Arrow (second)
@@ -15,67 +15,77 @@ unify hints expr = run $ do
   cs <- constrain hints expr
   solver cs []
 
+eq ctx t1 t2 = Context ctx (t1 :=: t2)
+
 solver [] subs = return $ Right subs
 
 --------  Destruct Type-constructors  --------
 
-solver ((t1@(ADT n1 ts1) :=: t2@(ADT n2 ts2)) : cs) subs =
-    if n1 /= n2 then uniError t1 t2 else
-        solver (zipWith (:=:) ts1 ts2 ++ cs) subs
-solver ((LambdaT t1 t2 :=: LambdaT t1' t2') : cs) subs =
-    solver ([ t1 :=: t1', t2 :=: t2' ] ++ cs) subs
+solver ((Context ctx (t1@(ADT n1 ts1) :=: t2@(ADT n2 ts2))) : cs) subs =
+    if n1 /= n2 then uniError ctx t1 t2 else
+        solver (zipWith (eq ctx) ts1 ts2 ++ cs) subs
+solver ((Context ctx (LambdaT t1 t2 :=: LambdaT t1' t2')) : cs) subs =
+    solver ([ eq ctx t1 t1', eq ctx t2 t2' ] ++ cs) subs
 
 --------  Type-equality  --------
 
-solver ((VarT x :=: t) : cs) subs =
+solver ((Context ctx (VarT x :=: t)) : cs) subs =
     solver (map (cSub x t) cs) . map (second $ tSub x t) $ (x,t):subs
-solver ((t :=: VarT x) : cs) subs = solver ((VarT x :=: t) : cs) subs
-solver ((t1 :=: t2) : cs) subs
+solver ((Context ctx (t :=: VarT x)) : cs) subs = solver ((Context ctx (VarT x :=: t)) : cs) subs
+solver ((Context ctx (t1 :=: t2)) : cs) subs
     | t1 == t2  = solver cs subs
-    | otherwise = uniError t1 t2
+    | otherwise = uniError ctx t1 t2
 
 --------  subtypes  --------
 
-solver (c@(VarT x :<: SuperType n ts) : cs) subs
-    | all isSuper cs = solver ((VarT x :=: ADT n []) : cs) subs
+solver (c@(Context ctx (VarT x :<: SuperType n ts)) : cs) subs
+    | all isSuper cs = solver ((Context ctx (VarT x :=: ADT n [])) : cs) subs
     | otherwise = solver (cs ++ [c]) subs
-    where isSuper (VarT _ :<: _) = True
+    where isSuper (Context _ (VarT _ :<: _)) = True
           isSuper _ = False
-solver ((t@(ADT n1 []) :<: st@(SuperType n2 ts)) : cs) subs
+solver ((Context ctx (t@(ADT n1 []) :<: st@(SuperType n2 ts))) : cs) subs
     | n1 == n2 || Set.member t ts = solver cs subs
     | otherwise = return . Left $ "Type error: " ++ show t ++
-                                  " is not a " ++ show st
-solver ((t@(ADT "List" [_]) :<: st@(SuperType n' ts')) : cs) subs
-    | any f (Set.toList ts') = solver cs subs
+                                  " is not a " ++ show st ++
+                                  " in context " ++ show ctx
+solver ((Context ctx (t@(ADT "List" [_]) :<: st@(SuperType n' ts'))) : cs) subs
+    | any f (Set.toList ts') || Set.member t ts' = solver cs subs
     | otherwise = return . Left $ "Type error: " ++ show t ++
-                                  " is not a " ++ show st 
+                                  " is not a " ++ show st ++
+                                  " in context " ++ show ctx
         where f (ADT "List" [VarT _]) = True
               f _ = False
 
-solver ((t :<: st@(SuperType n ts)) : cs) subs
+solver ((Context ctx (t :<: st@(SuperType n ts))) : cs) subs
     | Set.member t ts = solver cs subs
     | otherwise = return . Left $ "Type error: " ++ show t ++
                                   " is not a " ++ show st
 
-solver ((t1 :<<: Forall xs cs' t2) : cs) subs = do
+solver ((Context ctx (t1 :<<: Forall xs cs' t2)) : cs) subs = do
   pairs <- mapM (\x -> liftM ((,) x . VarT) guid) xs
   let t2'  = foldr (uncurry tSub) t2 pairs
-  let cs'' = foldr (\(k,v) -> map (cSub k v)) cs' pairs
-  solver ((t1 :=: t2') : cs'' ++ cs) subs
+  let cs'' = foldr (\(k,v) -> map (cSub k v)) (map (Context ctx) cs') pairs
+  let constrs = ((Context ctx (t1 :=: t2')) : cs'' ++ cs)
+  solver constrs subs
 
 
-cSub k v (t1 :=: t2) = force $ tSub k v t1 :=: tSub k v t2
-cSub k v (t :<: super) = force $ tSub k v t :<: super
-cSub k v (t :<<: poly) = force $ tSub k v t :<<: poly
+cSub k v (Context c (t1 :=: t2)  ) = force . Context c $ tSub k v t1 :=: tSub k v t2
+cSub k v (Context c (t :<: super)) = force . Context c $ tSub k v t :<: super
+cSub k v (Context c (t :<<: poly)) = force . Context c $ tSub k v t :<<: poly
 
 tSub k v t@(VarT x) = if k == x then v else t
 tSub k v (LambdaT t1 t2) = force $ LambdaT (tSub k v t1) (tSub k v t2)
 tSub k v (ADT name ts) = ADT name (map (force . tSub k v) ts)
 
-uniError t1 t2 =
-    return . Left $ "Type error: " ++ show t1 ++ " is not equal to " ++ show t2
+uniError ctx t1 t2 =
+    return . Left $ "Type error: " ++ show t1 ++
+                    " is not equal to " ++ show t2 ++
+                    " in context " ++ show ctx
 
 force x = x `deepseq` x
+
+instance (NFData a) => NFData (Context c a) where
+  rnf (Context _ a) = a `deepseq` ()
 
 instance NFData Constraint where
   rnf (t1 :=: t2) = t1 `deepseq` t2 `deepseq` ()
