c7ffd7e987c320239d839b517b1e3b86b673c16d
evancz <info@elm-lang.org>
2012-10-04 21:40:49 -0400

    Implement some of the `Automaton` functions.

--- core-elm/Automaton.elm
+++ core-elm/Automaton.elm
@@ -1,7 +1,12 @@
 
 module Automaton where
 
-data Automaton inp out = Automaton (inp -> (out, Automaton inp out))
+data Automaton a b = Automaton (a -> (b, Automaton a b))
+
+-- Maybe once there are infix type constructors this could be
+--     data a ~> b
+-- or something like that.
+
 
 {--- API possibilies
 
@@ -12,13 +17,34 @@ How to run an automaton (some require typeclasses):
 
 run :: Automaton a b -> Signal a -> Signal b
 run :: OrderedContainer c => Automaton a b -> c a -> c b
-run :: Functor f => Automaton a b -> f a -> f b
-
 
 compose :: Automaton a b -> Automaton b c -> Automaton a c
+
 combine :: [Automaton a b] -> Automaton a [b]
+combine :: Container c => c (Automaton a b) -> Automaton a (c b)
 
 init  :: b -> (a -> b -> b) -> Automaton a b
 init' :: s -> (a -> s -> (b,s)) -> Automaton a b
 
+draggable :: Form -> Automaton (Bool,(Int,Int)) Form
+
 --}
+
+
+run (Automaton m0) input =
+    lift fst $ foldp1 (\a (b, Automaton m) -> m a) m0 input
+
+compose (Automaton m1) (Automaton m2) =
+    Automaton 
+    (\a -> let (b,m1') = m1 a in
+           let (c,m2') = m2 b in (c, compose (Automaton m1') (Automaton m2')))
+
+combine autos =
+    Automaton (\a -> let (bs,autos') = unzip $ map (\m -> m a) autos in
+                     (bs, combine autos'))
+
+init s step = 
+  Automaton (\a -> let s' = step a s in (s', init s' step))
+
+init' s step =
+  Automaton (\a -> let (b,s') = step a s in (b, init s' step))
