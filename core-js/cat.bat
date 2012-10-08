95b20dba54c5216337dc537a4d19d4f44afce573
evancz <info@elm-lang.org>
2012-10-07 20:34:32 -0700

    Update changelog.txt and add automatons to the RTS.

--- core-js/cat.bat
+++ core-js/cat.bat
@@ -1,4 +1,4 @@
-copy /B Guid.js+foreign\JavaScript.js+foreign\JSON.js+Value.js+List.js+Data.js+Graphics\Color.js+Graphics\Collage.js+Graphics\Element.js+Text.js+Graphics\Render.js+runtime\Dispatcher.js+runtime\Signal.js+Prelude.js+Everything.js ..\elm-mini.js
+copy /B Guid.js+foreign\JavaScript.js+foreign\JSON.js+Value.js+List.js+Data.js+Graphics\Color.js+Graphics\Collage.js+Graphics\Element.js+Text.js+Graphics\Render.js+runtime\Dispatcher.js+runtime\Signal.js+Prelude.js+Everything.js+Automaton.js ..\elm-mini.js
 
 cd ..\elm
 
