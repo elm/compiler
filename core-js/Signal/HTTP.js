d71f7a41e744d0dca65c17adecaa5e60221070f3
evancz <info@elm-lang.org>
2012-10-30 00:48:17 -0700

    Make sure that HTTP responses are sent immediately but delivered sequentially.

--- core-js/Signal/HTTP.js
+++ core-js/Signal/HTTP.js
@@ -14,21 +14,35 @@ Elm.HTTP = function() {
   function post(url) { return function(data) {
 	  return request("POST")(url)(data)(["Nil"]); }; }
 
-  function sendReq(responses) { return function(req) {
-    Dispatcher.notify(responses.id,["Waiting"]);
+  function registerReq(queue,responses) { return function(req) {
+    if (req.url !== "") { sendReq(queue,responses,req); }
+   };
+  }
+
+  function updateQueue(queue,responses) {
+      if (queue.length > 0) {
+	Dispatcher.notify(responses.id, queue[0].value);
+        if (queue[0].value[0] !== "Waiting") {
+	  queue.shift();
+	  setTimeout(function() { updateQueue(queue,responses); }, 0);
+	}
+      }
+  }
+
+  function sendReq(queue,responses,req) {
+    var response = { value: ["Waiting"] };
+    queue.push(response);
+
     var request = null;
-    if (window.XMLHttpRequest) {
-	  request = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-	  request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
+    if (window.ActiveXObject)  { request = new ActiveXObject("Microsoft.XMLHTTP"); }
+    if (window.XMLHttpRequest) { request = new XMLHttpRequest(); }
     request.onreadystatechange = function(e) {
 	if (request.readyState === 4) {
-	  Dispatcher.notify(
-	  responses.id,
-	  request.status === 200
-	  ? ["Success", toElmString(request.responseText)]
-	  : ["Failure", request.status, toElmString(request.statusText)]);
+	    response.value = (request.status === 200
+			      ? ["Success", toElmString(request.responseText)]
+			      : ["Failure", request.status,
+				 toElmString(request.statusText)]);
+	    setTimeout(function() { updateQueue(queue,responses); }, 0);
 	}
     };
     request.open(req.verb, req.url, true);
@@ -39,12 +53,11 @@ Elm.HTTP = function() {
 	  })(req.headers);
     request.send(req.data);
     return null;
-   };
   }
 
   function send(requests) {
     var responses = Elm.Signal.constant(["Waiting"]);
-    var sender = Elm.Signal.lift(sendReq(responses))(requests);
+    var sender = Elm.Signal.lift(registerReq([],responses))(requests);
     function f(x) { return function(y) { return x; } }
     var combine = Elm.Signal.lift2(f)(responses)(sender);
     return combine;
