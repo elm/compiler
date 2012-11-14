807a7ec28ec4135e5d06681cee599893762cf0c7
evancz <info@elm-lang.org>
2012-11-13 21:30:22 -0800

    Add Date library.

---
+++ core-elm/Date.elm
@@ -0,0 +1,8 @@
+
+module Date where
+
+data Weekday = Mon | Tue | Wed | Thu | Fri | Sat | Sun
+
+data Month = Jan | Feb | Mar | Apr
+           | May | Jun | Jul | Aug
+           | Sep | Oct | Nov | Dec
