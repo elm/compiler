ede839f78783ca72c87e38fcfa06d8e4aec523c5
evancz <info@elm-lang.org>
2012-11-13 21:31:06 -0800

    Add new signal primitives.

--- core-js/runtime/Signal.js
+++ core-js/runtime/Signal.js
@@ -111,7 +111,47 @@ Elm.Signal = function() {
     s1.kids.push(this);
     s2.kids.push(this);
   };
+
+  function delay(t) { return function(s) {
+      var delayed = new input(s.value);
+      function update(v) {
+	  setTimeout(function() { Dispatcher.notify(delayed.id,v); },t*1000);
+      }
+      function first(a) { return function(b) { return a; }; }
+      return new lift(first, [delayed, new lift(update,[s])]);
+    };
+  }
   
+  var merge = function(s1,s2) {
+    this.id = Guid.guid();
+    this.value = s1.value;
+    this.kids = [];
+    this.next = null;
+    this.count = 0;
+    this.changed = false;
+    
+    this.recv = function(timestep, changed, parentID) {
+      this.count += 1;
+      if (changed) {
+	  this.changed = true;
+	  if (parentID == s2.id && this.next === null) { this.next = s2.value; }
+	  if (parentID == s1.id) { this.next = s1.value; }
+      }
+
+      if (this.count == 2) {
+	  if (this.changed) {
+	      this.value = this.next;
+	      this.next = null;
+	  }
+	  send(this, timestep, this.changed);
+	  this.changed = false;
+	  this.count = 0;
+      }
+    };
+    s1.kids.push(this);
+    s2.kids.push(this);
+  };
+
   return {
     constant : function(v) { return new input(v); },
     lift  : function(f){return function(e){return new lift(f,[e]);};},
@@ -128,9 +168,14 @@ Elm.Signal = function() {
 		  return new fold(f,b,true,e); }; }; },
     foldp1 : function(f) { return function(e) {
 	      return new fold(f,function(x){return x;},true,e); }; },
+    delay : delay,
+    merge : function(s1) { return function(s2) { return new merge(s1,s2)}; },
     count : function(sig) {
 	  var incr = function(_){return function(c){return c+1;};};
 	  return new fold(incr,0,false,sig) },
+    countIf : function(pred) { return function(sig) {
+	      var incr = function(x){return function(c){return pred(x) ? c+1 : c;};};
+	      return new fold(incr,0,false,sig) }; },
     keepIf : function(pred) { return function(base) { return function(sig) {
 		  return new dropIf(function(x) { return !pred(x)},base,sig); }; }; },
     dropIf : function(pred) { return function(base) { return function(sig) {
