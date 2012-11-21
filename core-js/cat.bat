0ac5bbba7e3746b151c38d69164ea6e44cca5627
evancz <info@elm-lang.org>
2012-11-20 23:18:52 -0800

    Add `Date` to RTS.

--- core-js/cat.bat
+++ core-js/cat.bat
@@ -1,4 +1,4 @@
-copy /B Guid.js+foreign\JavaScript.js+foreign\JSON.js+Value.js+List.js+Maybe.js+Dict.js+Set.js+Char.js+Graphics\Color.js+Graphics\Collage.js+Graphics\Element.js+Text.js+Graphics\Render.js+runtime\Signal.js+runtime\Dispatcher.js+Signal\HTTP.js+Signal\Input.js+Signal\Keyboard.js+Signal\Mouse.js+Signal\Random.js+Signal\Time.js+Signal\Window.js+Prelude.js+Automaton.js ..\elm-mini.js
+copy /B Guid.js+foreign\JavaScript.js+foreign\JSON.js+Value.js+List.js+Maybe.js+Char.js+Graphics\Color.js+Graphics\Collage.js+Graphics\Element.js+Text.js+Graphics\Render.js+runtime\Signal.js+runtime\Dispatcher.js+Signal\HTTP.js+Signal\Input.js+Signal\Keyboard.js+Signal\Mouse.js+Signal\Random.js+Signal\Time.js+Signal\Window.js+Date.js+Prelude.js+Dict.js+Set.js+Automaton.js ..\elm-mini.js
 
 cd ..\elm
 
