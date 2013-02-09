dd9d219655b31de95a5d119f3fd62d680efc41a3
evancz <info@elm-lang.org>
2013-02-08 22:17:39 -0800

    Begin upgrading the List library. It is not complete though. List.js should be the final authority on what is native and what is not.

--- core-elm/List.elm
+++ core-elm/List.elm
@@ -1,59 +1,88 @@
 
-head (h:t) = h
-tail (h:t) = t
+module List where
 
-map f lst = case lst of { x:xs -> f x : map f xs; [] -> [] }
-foldl f b lst = case lst of { x:xs -> foldl f (f x b) xs; [] -> b }
-foldr f b lst = case lst of { x:xs -> f x (foldr f b xs); [] -> b }
+import Native.List as Native
 
-foldl1 f (x:xs) = foldl f x xs
-foldr1 f (x:xs) = foldr f x xs
+head : [a] -> a
+head = Native.head
 
-scanl f b xs = b : (case xs of { x:xs -> scanl f (f x b) xs; [] -> [] })
-scanl1 f lst = case lst of { x:xs -> scanl f x xs; [] -> [] }
+tail : [a] -> [a]
+tail = Native.tail
 
-filter pred lst =
-    case lst of
-    { x:xs -> if pred x then x : filter pred xs else filter pred xs
-    ; [] -> [] }
+map : (a -> b) -> [a] -> [b]
+map = Native.map
 
-length = foldl (\x c -> 1 + c) 0
-reverse = foldl (:) []
+foldl : (a -> b -> b) -> b -> [a] -> b
+foldl = Native.foldl
+
+foldr : (a -> b -> b) -> b -> [a] -> b
+foldr = Native.foldr
+
+foldl1 : (a -> a -> a) -> [a] -> a
+foldl1 f (x::xs) = foldl f x xs
+
+foldr1 : (a -> a -> a) -> [a] -> a
+foldr1 f (x::xs) = foldr f x xs
+
+scanl f b xs = b :: (case xs of { x::xs -> scanl f (f x b) xs; [] -> [] })
+scanl1 f lst = case lst of { x::xs -> scanl f x xs; [] -> [] }
+
+filter : (a -> Bool) -> [a] -> [a]
+filter = Native.filter
+
+length : [a] -> Int
+length = Native.length
+
+reverse : [a] -> [a]
+reverse = foldl (::) []
 
 concat = foldr (++) []
 concatMap f = concat . map f
 
+and : [Bool] -> Bool
 and = foldl (&&) True
+
+or : [Bool] -> Bool
 or = foldl (||) False
 
-forall pred = foldl (\x acc -> acc && pred x) True
-exists pred = foldl (\x acc -> acc || pred x) False
+any : (a -> Bool) -> [a] -> Bool
+any pred = foldl (\x acc -> acc && pred x) True
+
+all : (a -> Bool) -> [a] -> Bool
+all pred = foldl (\x acc -> acc || pred x) False
 
 sum = foldl (+) 0
 product = foldl (*) 1
 maximum = foldl1 max
 minimum = foldl1 min
 
+partition : (a -> Bool) -> [a] -> ([a],[a])
 partition pred lst =
     case lst of
-    { x:xs -> case partition pred xs of
-              { (as,bs) -> if pred x then (x:as,bs) else (as,x:bs) }
-    ; [] -> ([],[]) }
+      []    -> ([],[])
+      x::xs -> let (as,bs) = partition pred xs  in
+               if pred x then (x::as,bs) else (as,x::bs)
 
-zipWith f listA listB = case (listA, listB) of
-    { (a:as, b:bs) -> f a b : zipWith f as bs; _ -> [] }
+zipWith : (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith = Native.zipWith
 
-zip listA listB = case (listA, listB) of
-    { (a:as, b:bs) -> (a,b) : zip as bs; _ -> [] }
+zip : [a] -> [b] -> [(a,b)]
+zip = Native.zip
 
+unzip : [(a,b)] -> ([a],[b])
 unzip pairs =
   case pairs of
-  { p:ps -> case (p, unzip ps) of { ((x,y),(xs,ys)) -> (x:xs,y:ys) }
-  ; [] -> ([],[]) }
+    []        -> ([],[])
+    (x,y)::ps -> let (xs,ys) = (unzip ps) in (x::xs,y::ys)
 
 intersperse sep xs =
-  case xs of { a:b:cs -> a : sep : intersperse sep (b:cs)
-             ; a:[] -> [a] ; [] -> [] }
+  case xs of 
+    a::b::cs -> a :: sep :: intersperse sep (b::cs)
+    [a] -> [a]
+    []  -> []
+
 intercalate sep xs =
-  case xs of { a:b:cs -> a ++ sep ++ intercalate sep (b:cs)
-             ; a:[] -> a ; [] -> [] }
+  case xs of 
+    a::b::cs -> a ++ sep ++ intercalate sep (b::cs)
+    [a] -> a
+    []  -> []
