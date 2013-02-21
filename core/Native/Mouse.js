62d30f24b8cca68d6d763d243613db0acef50af7
evancz <info@elm-lang.org>
2013-02-21 23:43:32 +0100

    Update mouse code.

--- core/Native/Mouse.js
+++ core/Native/Mouse.js
@@ -1,44 +1,18 @@
-/*! Mouse
-  !*/
 
-Elm.Mouse = function() {
-  /*[Position]*/
-
-  /** position : Signal (Int,Int)
-      The current mouse position.
-  **/
+function nativeMouse(elm) {
   var position  = Elm.Signal.constant(Value.Tuple(0,0));
   position.defaultNumberOfKids = 2;
 
-  /** x : Signal Int
-      The current x-coordinate of the mouse.
-  **/
-  var x = Elm.Signal.lift(function(p){return p[1];})(position);
+  // do not get rid of x and y. By setting their default number
+  // of kids, it is possible to detatch the mouse listeners if
+  // they are not needed.
+  var x = Elm.Signal.lift(function(p){return p._0})(position);
   x.defaultNumberOfKids = 0;
-
-  /** y : Signal Int
-      The current y-coordinate of the mouse.
-  **/
-  var y = Elm.Signal.lift(function(p){return p[2];})(position);
+  var y = Elm.Signal.lift(function(p){return p._1})(position);
   y.defaultNumberOfKids = 0;
 
-  /*[Button Status]*/
-
-  /** isDown : Signal Bool
-      The current state of the left mouse-button.
-      True when the button is down, and false otherwise.
-   **/
   var isDown    = Elm.Signal.constant(false);
-
-  /** isClicked : Signal Bool
-      True immediately after the left mouse-button has been clicked,
-      and false otherwise.
-   **/
   var isClicked = Elm.Signal.constant(false);
-
-  /** clicks : Signal ()
-      Always equal to unit. Event triggers on every mouse click.
-   **/
   var clicks = Elm.Signal.constant(Value.Tuple());
   
   function getXY(e) {
@@ -80,12 +54,6 @@ Elm.Mouse = function() {
 		this.removeEventListener('mousemove',arguments.callee,false);
 	});
 
-  /** isClickedOn : Element -> (Element, Signal Bool)
-      Determine whether an element has been clicked. The resulting pair
-      is a signal of booleans that is true when its paired element has
-      been clicked. The signal is True immediately after the left
-      mouse-button has been clicked, and false otherwise.
-   **/
   var clickedOn = function(elem) {
 	var node = Render.render(elem);
 	var click = Elm.Signal.constant(false);
@@ -95,12 +63,11 @@ Elm.Mouse = function() {
 	  });
 	return Value.Tuple(Value.wrap(node), click);
   };
-  return {position: position,
-	  x:x,
-	  y:y,
-	  isClicked: isClicked,
-	  isDown: isDown,
-	  clicks: clicks,
-	  isClickedOn: clickedOn
-	  };
-  }();
+  elm.Native.Mouse = {position: position,
+		      x:x,
+		      y:y,
+		      isClicked: isClicked,
+		      isDown: isDown,
+		      clicks: clicks,
+		      isClickedOn: clickedOn};
+}
