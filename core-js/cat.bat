8fc0e24dc2d72a6be07d680bd1a3ad823497fb31
evancz <info@elm-lang.org>
2013-03-09 19:15:04 -0800

    Remove core-js/ directory. This branching thing is nice, I agree now :P

--- core-js/cat.bat
+++
@@ -1,5 +0,0 @@
-copy /B Guid.js+foreign\JavaScript.js+foreign\JSON.js+Value.js+List.js+Maybe.js+Either.js+Char.js+Graphics\Color.js+Graphics\Collage.js+Graphics\Element.js+Text.js+Graphics\Render.js+runtime\Signal.js+runtime\Dispatcher.js+Signal\HTTP.js+Signal\WebSocket.js+Signal\Input.js+Signal\Keyboard.js+Signal\Mouse.js+Signal\Random.js+Signal\Time.js+Signal\Window.js+Date.js+Prelude.js+Signal\Touch.js+Dict.js+Set.js+Automaton.js ..\elm-mini.js
-
-cd ..\elm
-
-copy /B ..\elm-mini.js elm-runtime-0.7.2.js
