b1d0c74c2decd0f5b064fb49e2c281b2f185e296
evancz <info@elm-lang.org>
2013-03-17 16:36:24 -0700

    Get elm-doc to extract comments!

--- compiler/Language/Elm.hs
+++ compiler/Language/Elm.hs
@@ -14,7 +14,7 @@
      at <https://github.com/tazjin/Elm/tree/master/Examples>
 -}
 module Language.Elm (
-    ElmSource (..),
+    compile, toHtml,
     runtimeLocation
     ) where
 
@@ -30,52 +30,21 @@ import Paths_Elm
 import qualified Data.Text as TS
 import qualified Data.Text.Lazy as TL
 
+-- |This function compiles Elm code to three separate parts: HTML, CSS,
+--  and JavaScript. The HTML is only the contents of the body, so the three
+--  parts must be combined in a basic HTML skeleton.
+compile :: String -> String
+compile source = either showErr jsModule modul
+  where modul = buildFromSource source
+
+-- |This function compiles Elm code into a full HTML page.
+toHtml :: String -- ^ Location of elm-min.js as expected by the browser
+       -> String -- ^ The page title
+       -> String -- ^ The elm source code
+       -> Html
+toHtml = generateHtml
+
 -- |The absolute path to Elm's runtime system.
 runtimeLocation :: IO FilePath
 runtimeLocation = getDataFileName "elm-runtime.js"
 
-class ElmSource a where
-  -- |This function compiles Elm code to three separate parts: HTML, CSS,
-  --  and JavaScript. The HTML is only the contents of the body, so the three
-  --  parts must be combined in a basic HTML skeleton.
-  compile :: a -> String
-
-  -- |This function compiles Elm code into a full HTML page.
-  toHtml :: String -- ^ Location of elm-min.js as expected by the browser
-         -> String -- ^ The page title
-         -> a      -- ^ The elm source code
-         -> Html
-
-instance ElmSource String where
-  compile = compileHelper
-  toHtml = generateHtml
-
-instance ElmSource Elm where
-  compile = compileHelper . TL.unpack . renderElm
-  toHtml elmL title  = generateHtml elmL title  . TL.unpack . renderElm
-
--- |Strict text
-instance ElmSource TS.Text where
-  compile = compileHelper . TS.unpack
-  toHtml elmL title = generateHtml elmL title . TS.unpack
-
--- |Lazy text
-instance ElmSource TL.Text where
-  compile = compileHelper . TL.unpack
-  toHtml elmL title = generateHtml elmL title . TL.unpack
-
--- | (urlRenderFn, urlRenderFn -> Elm)
-instance ElmSource (t, t -> Elm) where
-  compile (f, s) = compileHelper $ TL.unpack $ renderElm $ s f  
-  toHtml elmL title (f, s) = generateHtml elmL title $ TL.unpack $ renderElm $ s f
-
--- | to be used without URL interpolation
-instance ElmSource (t -> Elm) where
-  compile s = compileHelper $ TL.unpack $ renderElm $ s undefined
-  toHtml l t s = generateHtml l t $ TL.unpack $ renderElm $ s undefined
-
-
--- build helper to avoid boilerplate repetition
-compileHelper :: String -> String
-compileHelper source = either showErr jsModule modul
-  where modul = buildFromSource source
