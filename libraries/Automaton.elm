a66a4b1fabde3530f0b35934d505fc833eb91e07
evancz <info@elm-lang.org>
2013-03-27 18:56:30 -0700

    Fix Automaton library to match PLDI paper.

--- libraries/Automaton.elm
+++ libraries/Automaton.elm
@@ -4,53 +4,59 @@
 -- it can be used.
 module Automaton where
 
-data Automaton a b = Automaton (a -> (b, Automaton a b))
+data Automaton a b = Step (a -> (Automaton a b, b))
 
--- Run an automaton on a given signal. The automaton takes in &lsquo;a&rsquo; values and returns &lsquo;b&rsquo; values. The automaton steps forward whenever the input signal updates.
-run : Automaton a b -> Signal a -> Signal b
-run (Automaton m0) input =
-  lift fst $ foldp' (\a (b, Automaton m) -> m a) m0 input
+-- Run an automaton on a given signal. The automaton steps forward
+-- whenever the input signal updates.
+run : Automaton a b -> b -> Signal a -> Signal b
+run (Step f) base inputs =
+  let step a (Step f, _) = f a
+  in  lift snd $ foldp step base inputs
 
 -- Step an automaton forward once with a given input.
-step : Automaton a b -> a -> (b, Automaton a b)
-step (Automaton m) a = m a
-
+step : a -> Automaton a b -> (Automaton a b, b)
+step a (Step f) = f a
 
 -- Compose two automatons, chaining them together.
 (>>>) : Automaton a b -> Automaton b c -> Automaton a c
-a1 >>> a2 =
-  let Automaton m1 = a1
-      Automaton m2 = a2
-  in  Automaton (\a -> let (b,m1') = m1 a
-                           (c,m2') = m2 b
-                       in  (c, m1' >>> m2'))
+f >>> g =
+  Step (\a -> let (f', b) = step a f
+                  (g', c) = step b g
+              in  (f' >>> g', c))
 
 -- Compose two automatons, chaining them together.
 (<<<) : Automaton b c -> Automaton a b -> Automaton a c
-a2 <<< a1 = a1 >>> a2 
+g <<< f = f >>> g
 
 -- Combine a list of automatons into a single automaton that produces a list.
 combine : [Automaton a b] -> Automaton a [b]
 combine autos =
-  Automaton (\a -> let (bs,autos') = unzip $ map (\(Automaton m) -> m a) autos in
-                   (bs, combine autos'))
+  Step (\a -> let (autos', bs) = unzip $ map (step a) autos
+              in  (combine autos', bs))
 
--- Create an automaton with no memory. It just applies the given function to every input.
-pure  : (a -> b) -> Automaton a b
-pure  f      = Automaton (\x -> (f x, pure f))
+-- Create an automaton with no memory. It just applies the given function to
+-- every input.
+pure : (a -> b) -> Automaton a b
+pure f = Step (\x -> (pure f, f x))
 
--- Create an automaton with no memory. It just applies the given function to every input.
-init  : b -> (a -> b -> b) -> Automaton a b
-init  s step = Automaton (\a -> let s'     = step a s in (s', init  s' step))
+-- Create an automaton with state. Requires an initial state and a step
+-- function to step the state forward.
+init : b -> (a -> b -> b) -> Automaton a b
+init s f = Step (\x -> let s' = f x s
+                       in  (init s' f, s'))
 
--- Create an automaton with hidden state. Requires an initial state and a step function to step the state forward and produce an output.
-init' : s -> (a -> s -> (b,s)) -> Automaton a b
-init' s step = Automaton (\a -> let (b,s') = step a s in (b , init' s' step))
+-- Create an automaton with hidden state. Requires an initial state and a
+-- step function to step the state forward and produce an output.
+init' : s -> (a -> s -> (s,b)) -> Automaton a b
+init' s f = Step (\x -> let (s',out) = f x s
+                        in  (init' s' f, out))
 
 -- Count the number of steps taken.
 count : Automaton a Int
 count = init 0 (\_ c -> c + 1)
 
+
+{-- TODO(evancz): move this code to the Form library so people can find it.
 
 data DragState = Listen | Ignore | DragFrom (Int,Int)
 
@@ -71,15 +77,15 @@ stepDrag (press,pos) (ds,form) =
 -- Create a draggable form that can be dynamically created and added to a scene.
 draggable : Form -> Automaton (Bool,(Int,Int)) Form
 draggable form = init' (Listen,form) stepDrag
+--}
 
-
-{--- See the following papers for ideas on how to make this faster:
+{-- TODO(evancz): See the following papers for ideas on how to make this
+library faster and better:
 
 - Functional Reactive Programming, Continued
 - Causal commutative arrows and their optimization
 
 Speeding things up is a really low priority. Language features and
 libraries with nice APIs and are way more important!
-
 --}
 
