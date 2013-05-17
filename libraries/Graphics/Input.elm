9b1fa9683b651a3ecd5265efe7fbce24fca11177
evancz <info@elm-lang.org>
2013-05-17 23:13:32 +0200

    Document the basic input functions.

--- libraries/Graphics/Input.elm
+++ libraries/Graphics/Input.elm
@@ -8,6 +8,8 @@ id x = x
 
 buttons : a -> { button : a -> String -> Element, events : Signal a }
 
+-- Create a button with a given label. The result is an `Element` and
+-- a signal of units. This signal triggers whenever the button is pressed.
 button : String -> (Element, Signal ())
 button txt =
     let pool = N.buttons ()
@@ -16,6 +18,9 @@ button txt =
 customButtons : a -> { button : a -> Element -> Element -> Element -> Element,
                        events : Signal a }
 
+-- Create a button with custom states for up, hovering, and down
+-- (given in that order). The result is an `Element` and
+-- a signal of units. This signal triggers whenever the button is pressed.
 customButton : Element -> Element -> Element -> (Element, Signal ())
 customButton up hover down =
     let pool = N.customButtons ()
@@ -23,6 +28,9 @@ customButton up hover down =
 
 checkboxes : a -> { box : (Bool -> a) -> Bool -> Element, events : Signal a }
 
+-- Create a checkbox with a given start state. Notice that the result is a *signal*
+-- of elements. That is because a checkbox has state that updates based on user input.
+-- The boolean signal represents the current state of the checkbox.
 checkbox : Bool -> (Signal Element, Signal Bool)
 checkbox b =
     let cbs = N.checkboxes b
@@ -35,19 +43,24 @@ fields : a -> { field : (FieldState -> a) -> String -> FieldState -> Element,
 
 empty = { string="", start=0, end=0 }
 
+-- Create a field with the given default text. The output is an element that
+-- updates to match the user input and a signal of strings representing the
+-- content of the field.
 field : String -> (Signal Element, Signal String)
 field placeHolder =
     let tfs = N.fields empty
     in  (lift (tfs.field id placeHolder) tfs.events, lift .string tfs.events)
 
+-- Same as `field` but the UI element blocks out each characters.
 password : String -> (Signal Element, Signal String)
 password placeHolder =
     let tfs = N.passwords empty
     in  (lift (tfs.field id placeHolder) tfs.events, lift .string tfs.events)
 
+-- Same as `field` but it adds an annotation that this field is for email addresses.
+-- This is helpful for auto-complete and for mobile users who may get a custom keyboard
+-- with an `@` and `.com` button.
 email : String -> (Signal Element, Signal String)
 email placeHolder =
     let tfs = N.emails empty
     in  (lift (tfs.field id placeHolder) tfs.events, lift .string tfs.events)
-
--- file?
