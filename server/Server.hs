c24f04b2432f7e08b3a7c6d7ca336c8ca77f894f
Laszlo Pandy <laszlo.pandy@prezi.com>
2013-06-04 11:37:20 +0200

    [elm-server] add --port=... command line option.

--- server/Server.hs
+++ server/Server.hs
@@ -3,7 +3,7 @@ module Main where
 
 import Control.Monad (msum,guard)
 import Control.Monad.Trans (MonadIO(liftIO))
-import Data.List (isPrefixOf, isSuffixOf)
+import Data.List (isPrefixOf, isSuffixOf, (\\))
 import Data.Version (showVersion)
 import Happstack.Server
 import Happstack.Server.Compression
@@ -14,17 +14,18 @@ import Paths_elm_server
 
 runtime = "/elm-" ++ showVersion version ++ ".js"
 
-serve :: String -> IO ()
-serve libLoc = do
+serve :: Int -> String -> IO ()
+serve portNumber libLoc = do
   putStrLn ("Elm Server " ++ showVersion version ++
-            ": running at <http://localhost:8000>")
+            ": running at <http://localhost:" ++ (show portNumber) ++ ">")
   putStrLn "Just refresh a page to recompile it!"
-  simpleHTTP nullConf $ do
+  simpleHTTP httpConf $ do
          _ <- compressedResponseFilter
          msum [ uriRest serveElm
               , uriRest (serveLib libLoc)
               , serveDirectory EnableBrowsing [] "."
               ]
+  where httpConf = nullConf { port = portNumber }
 
 pageTitle :: String -> String
 pageTitle = dropExtension . takeBaseName
@@ -44,12 +45,16 @@ main = getArgs >>= parse
 parse :: [String] -> IO ()
 parse ("--help":_) = putStrLn usage
 parse ("--version":_) = putStrLn ("The Elm Server " ++ showVersion version)
-parse [] = serve =<< Elm.runtime
-parse [arg]
-    | "--runtime-location=" `isPrefixOf` arg =
-        serve . tail $ dropWhile (/='=') arg
-    | otherwise = putStrLn usageMini
-parse _ = putStrLn usageMini
+parse args = if null remainingArgs
+             then serve portNumber =<< elmRuntime
+             else putStrLn usageMini
+  where runtimeArg = filter (isPrefixOf "--runtime-location=") args
+        portArg = filter (isPrefixOf "--port=") args
+        remainingArgs = (args \\ runtimeArg) \\ portArg
+
+        argValue arg = tail $ dropWhile (/= '=') (head arg)
+        portNumber = if null portArg then 8000 else read (argValue portArg) :: Int
+        elmRuntime = if null runtimeArg then Elm.runtime else return $ argValue runtimeArg
 
 usageMini :: String
 usageMini =
@@ -61,6 +66,9 @@ usage =
   "Usage: elm-server [OPTIONS]\n\
   \Compiles and serves .elm files from the current directory.\n\
   \Example: elm-server\n\
+  \\n\
+  \Server configuration:\n\
+  \  --port               set the port to listen on (default: 8000)\n\
   \\n\
   \Resource Locations:\n\
   \  --runtime-location   set the location of the Elm runtime\n\
