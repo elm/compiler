9592d286bbf4689033054d6b0e0ada89b1e6daff
Evan Czaplicki <info@elm-lang.org>
2013-07-19 18:05:31 +0200

    Start using Data.Graph to work out dependency problems and other minor tweaks
    Print out the saved type environment, and get things working with the
    program field in MetadataModule.

--- compiler/Initialize.hs
+++ compiler/Initialize.hs
@@ -5,10 +5,11 @@ import Control.Monad.Error
 import Data.List (nub)
 import qualified Data.Map as Map
 import Data.Data
-import Data.List (intercalate,partition)
+import qualified Data.Graph as Graph
+import qualified Data.List as List
 import System.Exit
 import System.FilePath as FP
-import Text.PrettyPrint (Doc)
+import Text.PrettyPrint as P
 
 import SourceSyntax.Everything
 import SourceSyntax.Declaration (Assoc)
@@ -22,21 +23,24 @@ import qualified Type.Inference as TI
 import qualified Type.Constrain.Declaration as TcDecl
 import qualified Type.Type as T
 
+import System.IO.Unsafe
 
-buildFromSource :: (Data t, Data v) => Bool -> String -> Either [Doc] (MetadataModule t v)
+
+buildFromSource :: (Data t, Data v) => Bool -> String -> Either [P.Doc] (MetadataModule t v)
 buildFromSource noPrelude src =
   do modul <- Parse.program src
 
      -- check for structural errors
      Module names exs ims decls <- checkMistakes modul
 
-     -- reorder AST into strongly connected components
+     
      let metaModule = MetadataModule {
            names = names,
            path = FP.joinPath names,
            exports = exs,
            imports = ims,
-           defs = fst . SD.flattenLets [] . SD.sortDefs . dummyLet $ TcDecl.toExpr decls,
+           -- reorder AST into strongly connected components
+           program = SD.sortDefs . dummyLet $ TcDecl.toExpr decls,
            types = Map.empty,
            datatypes = [ (name, vars, ctors) | Datatype name vars ctors <- decls ],
            fixities = [ (assoc,level,op) | Fixity assoc level op <- decls ],
@@ -46,7 +50,12 @@ buildFromSource noPrelude src =
           }
 
      types <- TI.infer metaModule
-     return $ metaModule { types = types }
+
+     let env = forM (Map.toList types) $ \(n,t) -> do
+                 pt <- T.extraPretty t
+                 print $ P.text n <+> P.text ":" <+> pt
+
+     return $ unsafePerformIO env `seq` metaModule { types = types }
 
   where
     checkMistakes modul@(Module _ _ _ decls) = 
@@ -62,17 +71,16 @@ getSortedModuleNames root =
 type Deps = (String, [String])
 
 sortDeps :: [Deps] -> IO [String]
-sortDeps deps = go [] (nub deps)
-    where
-      msg = "A cyclical or missing module dependency or was detected in: "
+sortDeps depends =
+    if null mistakes
+    then return (concat sccs)
+    else print (msg ++ show mistakes) >> exitFailure
+  where
+    graph = map (\(name, deps) -> (toFilePath name, name, deps)) depends
+    sccs = map Graph.flattenSCC $ Graph.stronglyConnComp graph
 
-      go :: [String] -> [Deps] -> IO [String]
-      go sorted [] = return (map toFilePath sorted)
-      go sorted unsorted =
-          case partition (all (`elem` sorted) . snd) unsorted of
-            ([],m:ms) -> do putStrLn (msg ++ intercalate ", " (map fst (m:ms)) ++ show sorted ++ show unsorted)
-                            exitFailure
-            (srtd,unsrtd) -> go (sorted ++ map fst srtd) unsrtd
+    mistakes = filter (\scc -> length scc > 1) sccs
+    msg = "A cyclical module dependency or was detected in: "
 
 readDeps :: [FilePath] -> FilePath -> IO [Deps]
 readDeps seen root = do
@@ -86,12 +94,12 @@ readDeps seen root = do
            return ((name, realDeps) : concat rest)
         where
           realDeps = filter (`notElem` builtIns) deps
-          newDeps = filter (\d -> d `notElem` seen && not (isNative d)) realDeps
+          newDeps = filter (\d -> not (d `elem` seen || isNative d)) realDeps
           seen' = root : seen ++ newDeps
           builtIns = Map.keys Libs.libraries
                        
 
-isNative name = takeWhile (/='.') name == "Native"
+isNative name = List.isPrefixOf "Native." name
 
 toFilePath :: String -> FilePath
 toFilePath name = map swapDots name ++ ext
