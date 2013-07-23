7b90975dadd0badcc3cf77a146aa3fa5231654dd
Evan Czaplicki <info@elm-lang.org>
2013-07-23 16:15:56 +0200

    Simplify code to check for structural errors

--- compiler/Transform/Check.hs
+++ compiler/Transform/Check.hs
@@ -13,9 +13,8 @@ import Text.PrettyPrint as P
 
 mistakes :: (Data t, Data v) => [Declaration t v] -> [Doc]
 mistakes decls =
-    map prettify $ concatMap findErrors (getLets decls)
+    map P.text $ concatMap findErrors (getLets decls)
   where
-    prettify = P.sep . map P.text . words
     findErrors defs = duplicates defs ++ badOrder defs
 
 
@@ -32,9 +31,12 @@ getLets decls = defs : concatMap getSubLets defs
 
 duplicates :: [Def t v] -> [String]
 duplicates defs =
-    map defMsg (dup definition) ++ map annMsg (dup annotation)
+    map defMsg (dups definitions) ++ map annMsg (dups annotations)
   where
-    dup f = map head . filter ((>1) . length) . List.group . List.sort $ concatMap f defs
+    annotations = List.sort [ name | TypeAnnotation name _ <- defs ]
+    definitions = List.sort $ concatMap Set.toList [ boundVars pattern | Def pattern _ <- defs ]
+
+    dups = map head . filter ((>1) . length) . List.group
 
     msg = "Syntax Error: There can only be one "
     defMsg x = msg ++ " definition of '" ++ x ++ "'."
@@ -42,49 +44,20 @@ duplicates defs =
 
 
 badOrder :: [Def t v] -> [String]
-badOrder defs =
-    map msg $ missings (List.sort $ expectedPairs as ds) (List.sort $ actualPairs defs)
+badOrder defs = go defs
     where
       msg x = "Syntax Error: The type annotation for '" ++ x ++
               "' must be directly above its definition."
-      as = List.sort $ concatMap annotation defs
-      ds = List.sort $ concatMap definition defs
 
-      expectedPairs :: [String] -> [String] -> [String]
-      expectedPairs as ds =
-          case (as,ds) of
-            (x:xs, y:ys) -> case compare x y of
-                              LT -> expectedPairs xs (y:ys)
-                              EQ -> x : expectedPairs xs ys
-                              GT -> expectedPairs (x:xs) ys
-            ( _  ,  _  ) -> []
-
-      actualPairs defs =
+      go defs =
           case defs of
-            TypeAnnotation name _ : Def (PVar name') _ : rest ->
-                (if name == name' then [name] else []) ++ actualPairs rest
-            t:s:rest -> actualPairs (s:rest)
+            TypeAnnotation name _ : Def (PVar name') _ : rest
+                | name == name' -> go rest
+
+            TypeAnnotation name _  : rest -> [msg name] ++ go rest
+
+            _ : rest -> go rest
+
             _ -> []
 
-      missings :: [String] -> [String] -> [String]
-      missings expected actual =
-          case (expected, actual) of
-            (e:es, a:as) -> case compare e a of
-                              LT -> e : missings es (a:as)
-                              EQ -> missings es as
-                              GT -> a : missings (e:es) as
-            ( [] ,  _  ) -> actual
-            (  _ ,  [] ) -> expected
 
-
-annotation :: Def t v -> [String]
-annotation def =
-    case def of
-      TypeAnnotation name _ -> [name]
-      _ -> []
-           
-definition :: Def t v -> [String]
-definition def =
-    case def of
-      Def pattern _ -> Set.toList (boundVars pattern)
-      _ -> []
