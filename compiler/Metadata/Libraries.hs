a4468d2eacd09f4005b6ac23e8b0d87fdf594d07
Evan Czaplicki <info@elm-lang.org>
2013-07-25 20:39:23 +0200

    Combine two Metadata files into one

--- compiler/Metadata/Libraries.hs
+++ compiler/Metadata/Libraries.hs
@@ -1,26 +1,51 @@
-module Metadata.Libraries (libraries, addPrelude) where
+module Metadata.Libraries (interfaces, addPrelude) where
 
-import Control.Applicative ((<$>),(<*>))
 import qualified Data.Map as Map
-import Data.List (inits)
-import Text.JSON
-import Metadata.Load as Load
+import qualified Control.Exception as E
+import qualified Paths_Elm as Path
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.IO.Unsafe (unsafePerformIO)
 import SourceSyntax.Module
+import Data.Binary
+
 
 addPrelude :: Module t v -> Module t v
 addPrelude (Module name exs ims stmts) = Module name exs (customIms ++ ims) stmts
-    where customIms = concatMap addModule prelude
+    where
+      customIms = concatMap addModule prelude
 
-          addModule (n, method) = case lookup n ims of
-                                    Nothing     -> [(n, method)]
-                                    Just (As m) -> [(n, method)]
-                                    Just _      -> []
+      addModule (n, method) = case lookup n ims of
+                                Nothing     -> [(n, method)]
+                                Just (As m) -> [(n, method)]
+                                Just _      -> []
 
+prelude :: [(String, ImportMethod)]
 prelude = text : map (\n -> (n, Hiding [])) modules
   where
     text = ("Text", Hiding ["link", "color", "height"])
     modules = [ "Prelude", "Signal", "List", "Maybe", "Time"
               , "Graphics.Element", "Color", "Graphics.Collage" ]
 
-libraries :: Map.Map String (Map.Map String String)
-libraries = Map.empty
+{-# NOINLINE interfaces #-}
+interfaces :: Interfaces
+interfaces =
+    unsafePerformIO (safeReadDocs =<< Path.getDataFileName "interfaces.data")
+
+safeReadDocs :: FilePath -> IO Interfaces
+safeReadDocs name =
+    E.catch (readDocs name) $ \err -> do
+      putStrLn $ unlines [ "Error reading types for standard library!"
+                         , "  " ++ show (err :: IOError)
+                         , "The file should be located here: " ++ name
+                         , "  If you are using a stable version of Elm,"
+                         , "  please report an issue at github.com/evancz/Elm"
+                         , "  and specify your versions of Elm and your OS" ]
+      exitFailure
+
+readDocs :: FilePath -> IO Interfaces
+readDocs name = do
+  exists <- doesFileExist name
+  if exists then decodeFile name
+            else ioError . userError $ "File Not Found"
