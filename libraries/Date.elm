435822d8d84f36256abd56aa69e0b628f9829e2f
Evan Czaplicki <info@elm-lang.org>
2013-07-26 11:42:00 +0200

    Get Date and JS libraries compiling

--- libraries/Date.elm
+++ libraries/Date.elm
@@ -4,6 +4,11 @@
 -- locale formatting or something.
 module Date where
 
+import Native.Date as Native
+import Time
+
+data Date = Date
+
 -- Represents the days of the week.
 data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sun
 
@@ -14,37 +19,45 @@ data Month = Jan | Feb | Mar | Apr
 
 -- Attempt to read a date from a string.
 read : String -> Maybe Date
+read = Native.read
 
 -- Convert a date into a time since midnight (UTC) of 1 January 1990 (i.e.
 -- [UNIX time](http://en.wikipedia.org/wiki/Unix_time)). Given the date 23 June
 -- 1990 at 11:45AM this returns the corresponding time.
 toTime : Date -> Time
+toTime = Native.toTime
 
 -- Extract the year of a given date. Given the date 23 June 1990 at 11:45AM
 -- this returns the integer `1990`.
-year  : Date -> Int
+year : Date -> Int
+year = Native.year
 
 -- Extract the month of a given date. Given the date 23 June 1990 at 11:45AM
 -- this returns the Month `Jun` as defined below.
 month : Date -> Month
-
+month = Native.month
 
 -- Extract the day of a given date. Given the date 23 June 1990 at 11:45AM
 -- this returns the integer `23`.
-day   : Date -> Int
+day : Date -> Int
+day = Native.day
 
 -- Extract the day of the week for a given date. Given the date 23 June
 -- 1990 at 11:45AM this returns the Day `Thu` as defined below.
 dayOfWeek : Date -> Day
+dayOfWeek = Native.dayOfWeek
 
 -- Extract the hour of a given date. Given the date 23 June 1990 at 11:45AM
 -- this returns the integer `11`.
-hour   : Date -> Int
+hour : Date -> Int
+hour = Native.hour
 
 -- Extract the minute of a given date. Given the date 23 June 1990 at 11:45AM
 -- this returns the integer `45`.
 minute : Date -> Int
+minute = Native.minute
 
 -- Extract the second of a given date. Given the date 23 June 1990 at 11:45AM
 -- this returns the integer `0`.
 second : Date -> Int
+second = Native.second
--- libraries/JavaScript.elm
+++ libraries/JavaScript.elm
@@ -1,26 +1,54 @@
 
 module JavaScript where
 
+import Native.JavaScript as Native
+
+data JSNumber = JSNumber
+data JSBool = JSBool
+data JSString = JSString
+data JSArray a = JSArray a
+data JSDomNode = JSDomNode
 
 -- Requires that the input array be uniform (all members have the same type)
 toList : JSArray a -> [a]
+toList = Native.toList
+
 toInt : JSNumber -> Int
+toInt = Native.toInt
+
 toFloat : JSNumber -> Float
+toFloat = Native.toFloat
+
 toBool : JSBool -> Bool
+toBool = Native.toBool
+
 toString : JSString -> String
+toString = Native.toString
 
 
 -- Produces a uniform JavaScript array with all members of the same type.
 fromList : [a] -> JSArray a
+fromList = Native.fromList
+
 fromInt : Int -> JSNumber
+fromInt = Native.fromInt
+
 fromFloat : Float -> JSNumber
+fromFloat = Native.fromFloat
+
 fromBool : Bool -> JSBool
+fromBool = Native.fromBool
+
 fromString : String -> JSString
+fromString = Native.fromString
 
 
 -- Turn an `Element` into a plain old DOM node.
 fromElement : Element -> JSDomNode
+fromElement = Native.fromElement
 
 -- Turn a DOM node into an `Element`. You can resize the node
 -- using the normal `width` and `height` functions.
 toElement : Int -> Int -> JSDomNode -> Element
+toElement = Native.toElement
+
