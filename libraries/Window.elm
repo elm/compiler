7d00618169c51aeeb88cde762f492e4056864036
Evan Czaplicki <info@elm-lang.org>
2013-07-26 17:36:01 +0200

    Make Window work

--- libraries/Window.elm
+++ libraries/Window.elm
@@ -1,14 +1,17 @@
 
 module Window where
 
-import Native.Window as W
+import Native.Window as Native
 
 -- The current dimensions of the window (i.e. the area viewable to the
 -- user, not including scroll bars).
 dimensions : Signal (Int,Int)
+dimensions = Native.dimensions
 
 -- The current width of the window.
 width : Signal Int
+width = Native.width
 
 -- The current height of the window.
 height : Signal Int
+height = Native.height
