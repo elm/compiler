e22c7d455e85a6e782572ad9b2863803d6a3a420
Evan Czaplicki <info@elm-lang.org>
2013-07-29 11:54:33 +0200

    Add types and `deprime` function to SourceSyntax.Helpers

--- compiler/SourceSyntax/Helpers.hs
+++ compiler/SourceSyntax/Helpers.hs
@@ -3,12 +3,19 @@ module SourceSyntax.Helpers where
 
 import qualified Data.Char as Char
 
+brkt :: String -> String
 brkt s = "{ " ++ s ++ " }"
 
+isTuple :: String -> Bool
 isTuple name =
     take 6 name == "_Tuple" && all Char.isDigit (drop 6 name)
 
+isOp :: String -> Bool
 isOp = all isSymbol
 
+isSymbol :: Char -> Bool
 isSymbol c =
     Char.isSymbol c || elem c "+-/*=.$<>:&|^?%#@~!"
+
+deprime :: String -> String
+deprime = map (\c -> if c == '\'' then '$' else c)
