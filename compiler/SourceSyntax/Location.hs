177a03750b3268d63349589d303bc712d7eb3b36
Evan Czaplicki <info@elm-lang.org>
2013-08-03 11:40:53 -0700

    Add source code to locations for better error messages

--- compiler/SourceSyntax/Location.hs
+++ compiler/SourceSyntax/Location.hs
@@ -1,40 +1,56 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module SourceSyntax.Location where
 
-import qualified Text.Parsec.Pos as Parsec
 import Data.Data
+import Text.PrettyPrint
+import SourceSyntax.PrettyPrint
+import qualified Text.Parsec.Pos as Parsec
 
 data SrcPos = Pos { line :: Int, column :: Int }
     deriving (Eq, Ord, Data, Typeable)
 
-data SrcSpan = Span SrcPos SrcPos | NoSpan
+data SrcSpan = Span SrcPos SrcPos String | NoSpan String
     deriving (Eq, Ord, Data, Typeable)
 
 data Located e = L SrcSpan e
     deriving (Eq, Ord, Data, Typeable)
 
+none e = L (NoSpan (render $ pretty e)) e
+noneNoDocs = L (NoSpan "")
+
+at start end e = L (Span (Pos (Parsec.sourceLine start) (Parsec.sourceColumn start))
+                         (Pos (Parsec.sourceLine end  ) (Parsec.sourceColumn end  ))
+                         (render $ pretty e)) e
+
+merge (L s1 _) (L s2 _) e = L (span (render $ pretty e)) e
+    where span = case (s1,s2) of
+                   (Span start _ _, Span _ end _) -> Span start end
+                   (Span start end _, _) -> Span start end
+                   (_, Span start end _) -> Span start end
+                   (_, _) -> NoSpan
+
+mergeOldDocs (L s1 _) (L s2 _) e = L span e
+    where span = case (s1,s2) of
+                   (Span start _ d1, Span _ end d2) -> Span start end (d1 ++ "\n\n" ++ d2)
+                   (Span _ _ _, _) -> s1
+                   (_, Span _ _ _) -> s2
+                   (_, _) -> NoSpan ""
+
+sameAs (L s _) = L s
+
 
 instance Show SrcPos where
-    show (Pos r c) = "Line " ++ show r ++ ", Column " ++ show c
+    show (Pos r c) = show r ++ "," ++ show c
 
 instance Show SrcSpan where
   show span = 
       case span of
-        Span start end -> show start
-        NoSpan -> ""
+        Span start end _ -> show start ++ " to " ++ show end
+        NoSpan str -> str
 
 instance Show e => Show (Located e) where
-  show (L _ e) = show e
+  show (L s e) = "L (" ++ show s ++ ") (" ++ show e ++ ")"
 
-none = L NoSpan
+instance Pretty a => Pretty (Located a) where
+  pretty (L _ e) = pretty e
 
-at start end = L (Span (Pos (Parsec.sourceLine start) (Parsec.sourceColumn start))
-                       (Pos (Parsec.sourceLine end  ) (Parsec.sourceColumn end  )))
-
-merge (L s1 _) (L s2 _) = L span
-    where span = case (s1,s2) of
-                   (Span start _, Span _ end) -> Span start end
-                   (_, NoSpan) -> s1
-                   (NoSpan, _) -> s2
-
-sameAs (L s _) = L s
--- compiler/SourceSyntax/PrettyPrint.hs
+++ compiler/SourceSyntax/PrettyPrint.hs
@@ -2,14 +2,10 @@
 module SourceSyntax.PrettyPrint where
 
 import Text.PrettyPrint
-import SourceSyntax.Location
 import qualified SourceSyntax.Helpers as Help
 
 class Pretty a where
   pretty :: a -> Doc
-
-instance Pretty a => Pretty (Located a) where
-  pretty (L _ e) = pretty e
 
 instance Pretty () where
   pretty () = empty
