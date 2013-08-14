7c19935d863359fc62a6f59b8ee9dcee2177b4dc
Evan Czaplicki <info@elm-lang.org>
2013-08-14 00:44:40 -0700

    reprime when printing types

--- compiler/Type/PrettyPrint.hs
+++ compiler/Type/PrettyPrint.hs
@@ -2,6 +2,7 @@
 module Type.PrettyPrint where
 
 import Text.PrettyPrint
+import qualified SourceSyntax.PrettyPrint as Src
 
 data ParensWhen = Fn | App | Never
 
@@ -11,3 +12,5 @@ class PrettyType a where
 commaSep docs = sep (punctuate comma docs)
 
 parensIf bool doc = if bool then parens doc else doc
+
+reprime = Src.reprime
