6980de6865a381a82d834d18b0af5aaf4c4c68ae
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2013-08-25 18:42:56 +0000

    removed old

--- compiler/Type/ExtraChecks.hs
+++ compiler/Type/ExtraChecks.hs
@@ -35,7 +35,6 @@ mainCheck rules env =
             Left [ P.vcat [ P.text "Type Error:"
                           , P.text "Bad type for 'main'. It must have type Element or a (Signal Element)"
                           , P.text "Instead 'main' has type:\n"
-                          , P.nest 4 . pretty $ Alias.realias rules tipe
                           , P.nest 4 . pretty $ Alias.canonicalRealias (fst rules) tipe
                           , P.text " " ]
                  ]
