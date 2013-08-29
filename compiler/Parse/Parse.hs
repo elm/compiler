65dcc0b57e5a228cef589bf5f53745f0138c3896
Evan Czaplicki <info@elm-lang.org>
2013-08-29 02:55:49 -0700

    Turn off custom OpTables for now

--- compiler/Parse/Parse.hs
+++ compiler/Parse/Parse.hs
@@ -49,7 +49,7 @@ setupParserWithTable table p source =
          overlap | not (Map.null overlap) -> Left [ msg overlap ]
                  | otherwise -> 
                      flip setupParser source $ do
-                       putState (Map.union initialTable localTable)
+                       putState initialTable --(Map.union initialTable localTable)
                        p
     where
       msg overlap =
