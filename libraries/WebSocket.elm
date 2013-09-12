5e9c9933d450c740b3deeb08b216ad0f6b442653
Evan Czaplicki <info@elm-lang.org>
2013-09-12 12:55:43 -0700

    Update to new doc format

--- libraries/WebSocket.elm
+++ libraries/WebSocket.elm
@@ -1,14 +1,16 @@
--- A library for low latency HTTP communication. See the HTTP library for
--- standard requests like GET, POST, etc.
 module WebSocket where
+{-| A library for low latency HTTP communication. See the HTTP library for
+standard requests like GET, POST, etc.
+-}
 
 import Signal (Signal)
 import Basics (String)
 import Native.WebSocket
 
--- Create a web-socket. The first argument is the URL of the desired
--- web-socket server. The input signal holds the outgoing messages,
--- and the resulting signal contains the incoming ones.
+{-| Create a web-socket. The first argument is the URL of the desired
+web-socket server. The input signal holds the outgoing messages,
+and the resulting signal contains the incoming ones.
+-}
 connect : String -> Signal String -> Signal String
 connect = Native.WebSocket.connect
 
