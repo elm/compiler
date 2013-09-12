9e17f13ffa29a95ba218d63155337ad3046aa924
Evan Czaplicki <info@elm-lang.org>
2013-09-12 14:00:29 -0700

    Convert to new format

--- libraries/JavaScript/Experimental.elm
+++ libraries/JavaScript/Experimental.elm
@@ -1,32 +1,38 @@
-
 module JavaScript.Experimental where
+{-| A pair of very experimental functions for converting between
+Elm records and JavaScript objects. It currently uses a best effort
+conversion that can cause runtime errors. This API should change and
+improve as we find its key failings.
+-}
 
 import JavaScript (JSObject)
 import Native.JavaScript
 
--- Turn arbitrary JavaScript objects into Elm records.
--- Arrays are converted into lists, nested objects are allowed.
--- No `null` values or non-homogeneous arrays.
---
---         -- OK objects
---         { student:"Steve", scores:[83,94,99,72] }
---         { errorLevel:10, critical:true }
---
---         -- BAD objects
---         { answer:null }
---         { info:[true,42,'what'] }
+{-| Turn arbitrary JavaScript objects into Elm records.
+Arrays are converted into lists, nested objects are allowed.
+No `null` values or non-homogeneous arrays.
+
+        -- OK objects
+        { student:"Steve", scores:[83,94,99,72] }
+        { errorLevel:10, critical:true }
+
+        -- BAD objects
+        { answer:null }
+        { info:[true,42,'what'] }
+-}
 toRecord : JSObject -> a
 toRecord = Native.JavaScript.toRecord
 
--- Turn arbitrary Elm records into JavaScript objects.
--- Lists become arrays, nested records are allowed. No ADTs.
---
---         -- OK records
---         { student="Steve", scores=[83,94,99,72] }
---         { errorLevel=10, critical=True }
---
---         -- BAD records
---         { answer = Nothing }
---         { result = Left "An error occurred" }
+{-| Turn arbitrary Elm records into JavaScript objects.
+Lists become arrays, nested records are allowed. No ADTs.
+
+        -- OK records
+        { student="Steve", scores=[83,94,99,72] }
+        { errorLevel=10, critical=True }
+
+        -- BAD records
+        { answer = Nothing }
+        { result = Left "An error occurred" }
+-}
 fromRecord : a -> JSObject
 fromRecord = Native.JavaScript.fromRecord
