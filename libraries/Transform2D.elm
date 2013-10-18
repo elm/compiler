5b2ba67e61e6ecf50fcf198e1147b37ee3592af8
Evan Czaplicki <info@elm-lang.org>
2013-10-17 20:32:45 -0400

    Get rid of double backslashes, which have mysteriously changed behavior and now actually show up twice

--- libraries/Transform2D.elm
+++ libraries/Transform2D.elm
@@ -18,8 +18,8 @@ data Transform2D = Transform2D
 not change anything, but it can come in handy as a default or
 base case.
 
-          / 1 0 \\
-          \\ 0 1 /
+          / 1 0 \
+          \ 0 1 /
 -}
 identity : Transform2D
 identity = Native.Transform2D.identity
@@ -29,8 +29,8 @@ such as scales, shears, reflections, and translations.
 
       matrix a b c d dx dy
 
-          / a b \\
-          \\ c d /
+          / a b \
+          \ c d /
 
 And `dx` and `dy` are the translation values.
 -}
@@ -40,8 +40,8 @@ matrix = Native.Transform2D.matrix
 {-| Creates a [rotation matrix](http://en.wikipedia.org/wiki/Rotation_matrix).
 Given an angle t, it creates a counterclockwise rotation matrix:
 
-          / cos t  -sin t \\
-          \\ sin t   cos t /
+          / cos t  -sin t \
+          \ sin t   cos t /
 -}
 rotation : Float -> Transform2D
 rotation = Native.Transform2D.rotation
@@ -50,8 +50,8 @@ rotation = Native.Transform2D.rotation
 
       multiply a b
 
-          / a11 a12 \\  .  / b11 b12 \\
-          \\ a21 a22 /     \\ b21 b22 /
+          / a11 a12 \  .  / b11 b12 \
+          \ a21 a22 /     \ b21 b22 /
 -}
 multiply : Transform2D -> Transform2D -> Transform2D
 multiply = Native.Transform2D.multiply
