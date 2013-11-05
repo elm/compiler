c03c27fe4f6c45296dc376c54316c99948dc2779
Laszlo Pandy <laszlo.pandy@prezi.com>
2013-11-05 17:44:02 +0100

    Debugger: move postMessage to new function.

--- runtime/Debugger.js
+++ runtime/Debugger.js
@@ -8,21 +8,25 @@ Elm.debuggerAttach = function(module) {
     make: function(runtime) {
       var wrappedModule = debugModule(module, runtime);
       Elm.Debugger = debuggerInit(wrappedModule, runtime);
-      if (parent.window) {
-        var dispatch = function(_) {
-          parent.window.postMessage("elmDebuggerInit", window.location.origin);
-        };
-        if (parent.window.document.readyState === "complete") {
-          dispatch();
-        }
-        else {
-          parent.window.addEventListener("load", dispatch);
-        }
-      }
+      dispatchElmDebuggerInit();
       return wrappedModule.moduleInstance;
     }
   };
 };
+
+function dispatchElmDebuggerInit() {
+  if (parent.window) {
+    var dispatch = function(_) {
+      parent.window.postMessage("elmDebuggerInit", window.location.origin);
+    };
+    if (parent.window.document.readyState === "complete") {
+      dispatch();
+    }
+    else {
+      parent.window.addEventListener("load", dispatch);
+    }
+  }
+}
 
 function debugModule(module, runtime) {
   var programPaused = false;
