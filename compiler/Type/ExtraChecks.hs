f73a6ff9f92d287d5a9b3754ecd0f52138a6fba5
Evan Czaplicki <info@elm-lang.org>
2013-11-19 22:31:38 -0800

    Add an occurs check to finally resolve #294
    Add it in the more clever location suggested by Pottier and Remy. Also
    change the style of error messages to get rid of the expected/actual
    problem for now, and make hints read more smoothly.

--- compiler/Type/ExtraChecks.hs
+++ compiler/Type/ExtraChecks.hs
@@ -1,27 +1,25 @@
 -- This module contains checks to be run *after* type inference has
 -- completed successfully. At that point we still need to do occurs
 -- checks and ensure that `main` has an acceptable type.
-module Type.ExtraChecks (extraChecks) where
+module Type.ExtraChecks (extraChecks, occursCheck) where
 
 import Control.Applicative ((<$>),(<*>))
+import Control.Monad.State
 import qualified Data.Map as Map
 import qualified Data.UnionFind.IO as UF
 import Type.Type ( Variable, structure, Term1(..), toSrcType )
-import Type.State (Env)
+import qualified Type.State as TS
 import qualified Type.Alias as Alias
 import Text.PrettyPrint as P
 import SourceSyntax.PrettyPrint (pretty)
 import SourceSyntax.Type (Type)
+import qualified SourceSyntax.Location as Location
+import qualified SourceSyntax.Expression as Expr
 import qualified Data.Traversable as Traverse
 
-extraChecks :: Alias.Rules -> Env -> IO (Either [P.Doc] (Map.Map String Type))
-extraChecks rules env = do
-  eitherEnv <- occursCheck env
-  case eitherEnv of
-    Left errs -> return $ Left errs
-    Right env' ->
-        mainCheck rules <$> Traverse.traverse toSrcType env'
-           
+extraChecks :: Alias.Rules -> TS.Env -> IO (Either [P.Doc] (Map.Map String Type))
+extraChecks rules env =
+    mainCheck rules <$> Traverse.traverse toSrcType env
 
 mainCheck :: Alias.Rules -> (Map.Map String Type) -> Either [P.Doc] (Map.Map String Type)
 mainCheck rules env =
@@ -39,35 +37,37 @@ mainCheck rules env =
                           , P.text " " ]
                  ]
 
-occursCheck :: Env -> IO (Either [P.Doc] Env)
-occursCheck env = do
-  errors <- concat <$> mapM isFinite (Map.toList env)
-  return $ if null errors then Right env else Left errors
-
-isFinite :: (String, Variable) -> IO [P.Doc]
-isFinite (name, var) =
-  do varIsFinite <- go [] var
-     return $
-       case varIsFinite of
-         True -> []
-         False -> [ P.vcat [ P.text "Type Error:"
-                           , P.text $ "Cannot construct infinite type for '" ++ name ++ "'\n"
-                           ]
-                  ]
+occursCheck :: (String, Variable) -> StateT TS.SolverState IO ()
+occursCheck (name, variable) =
+  do vars <- liftIO $ infiniteVars [] variable
+     case vars of
+       [] -> return ()
+       var:_ -> do
+         desc <- liftIO $ UF.descriptor var
+         case structure desc of
+           Nothing ->
+               modify $ \state -> state { TS.sErrors = fallback : TS.sErrors state }
+           Just struct ->
+               do liftIO $ UF.setDescriptor var (desc { structure = Nothing })
+                  var' <- liftIO $ UF.fresh desc
+                  TS.addError (Location.NoSpan name) (Just msg) var var'
   where
-    go :: [Variable] -> Variable -> IO Bool
-    go seen var =
-        let check = go (var:seen) in
-        case var `elem` seen of
-          True -> return False
-          False -> do
-            desc <- UF.descriptor var
-            case structure desc of
-              Nothing -> return True
-              Just struct ->
-                  case struct of
-                    App1 a b -> (&&) <$> check a <*> check b
-                    Fun1 a b -> (&&) <$> check a <*> check b
-                    Var1 a   -> check a
-                    EmptyRecord1 -> return True
-                    Record1 fields ext -> and <$> mapM check (ext : concat (Map.elems fields))
+    msg = "Infinite types are not allowed"
+    fallback _ = return $ P.text msg
+
+    infiniteVars :: [Variable] -> Variable -> IO [Variable]
+    infiniteVars seen var =
+        let go = infiniteVars (var:seen) in
+        if var `elem` seen
+        then return [var]
+        else do
+          desc <- UF.descriptor var
+          case structure desc of
+            Nothing -> return []
+            Just struct ->
+                case struct of
+                  App1 a b -> (++) <$> go a <*> go b
+                  Fun1 a b -> (++) <$> go a <*> go b
+                  Var1 a   -> go a
+                  EmptyRecord1 -> return []
+                  Record1 fields ext -> concat <$> mapM go (ext : concat (Map.elems fields))
