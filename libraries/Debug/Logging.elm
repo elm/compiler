649b27e000175f000b9e0843e06ab9a9cdf88c86
Timothy Vladimír Hobbs <timothyhobbs@seznam.cz>
2013-12-22 10:31:15 +0100

    Add note to docs about not logging signals

--- libraries/Debug/Logging.elm
+++ libraries/Debug/Logging.elm
@@ -9,6 +9,9 @@ module Debug.Logging where
 import Prelude
 import Native.Debug.Logging
 
-{-| Print the value as a string to the error console. -}
+{-| Print the value as a string to the error console.
+
+Note: Does not work for values of type Signal
+-}
 unsafePrintLogMessage : a -> a
 unsafePrintLogMessage a = (\message -> a )(Native.Debug.Logging.unsafePrintLogMessage  (Prelude.show a))
