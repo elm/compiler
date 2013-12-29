840eca65735f11dc90288dcb9ff67fc70479ad99
Max New <maxsnew@gmail.com>
2013-12-29 13:44:25 -0600

    Migrate tests to test-framework and make them more specific.
    Use test-framework as it's more widely used/has better support for
    HUnit/QuickCheck.
    
    Make test cases test the `compile` function explicitly instead of the
    elm executable as that's all they're actually intended to test.

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,22 +1,38 @@
 module Main where
 
-import System.Directory
-import System.Exit (exitWith)
-import System.Environment (getArgs)
-import Test.Framework.TestManager
-import Test.Framework.BlackBoxTest
+import Data.Functor                   ((<$>))
+import Data.Traversable               (traverse)
+import System.FilePath.Find           (find, (==?), extension)
+import Test.Framework
+import Test.Framework.Providers.HUnit (testCase)
+import Test.HUnit                     ((@=?), Assertion)
+
+import Elm.Internal.Utils as Elm
 
 main :: IO () 
-main = do
-  args  <- getArgs
-  tests <- blackBoxTests "tests" "dist/build/elm/elm" ".elm" bbtArgs
-  code  <- runTestWithArgs args tests
-  removeDirectoryRecursive "cache"
-  removeDirectoryRecursive "build"
-  exitWith code
+main = defaultMain [compilerTests]
 
-bbtArgs = defaultBBTArgs { bbtArgs_stdoutDiff = ignoreDiff
-                         , bbtArgs_stderrDiff = ignoreDiff }
+compilerTests :: Test
+compilerTests = buildTest $ do
+  goods <- getElms "tests/good" >>= mkTests True
+  bads  <- getElms "tests/bad"  >>= mkTests False
 
-ignoreDiff :: Diff
-ignoreDiff _ _ = return Nothing
+  return $ testGroup "Compile Tests"
+    [
+      testGroup "Good Tests" goods
+    , testGroup "Bad Tests"  bads
+    ]
+
+  where getElms :: FilePath -> IO [FilePath]
+        getElms = find (return True) (extension ==? ".elm")
+
+        mkTests :: Bool -> [FilePath] -> IO [Test]
+        mkTests b = traverse setupTest
+          where setupTest f = testCase f . mkCompileTest b <$> readFile f
+
+mkCompileTest :: Bool      -- ^ Expect success?
+                 -> String -- ^ File Contents
+                 -> Assertion
+mkCompileTest succ modul = noCompileErr @=? succ
+  where noCompileErr = either (const False) (const True) . Elm.compile $ modul
+        expectation = "Compile " ++ if succ then "Success" else "Error"
