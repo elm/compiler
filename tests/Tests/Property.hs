24751a3d85a140fbcbe350f771e98e3747029493
Max New <maxsnew@gmail.com>
2013-12-29 20:18:45 -0600

    Add parse/print tests for Patterns.

--- tests/Tests/Property.hs
+++ tests/Tests/Property.hs
@@ -5,18 +5,23 @@ import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
 
 import SourceSyntax.Literal     (Literal)
-import SourceSyntax.PrettyPrint (pretty)
-import Parse.Helpers            (iParse)
+import SourceSyntax.PrettyPrint (Pretty, pretty)
+import Parse.Helpers            (IParser, iParse)
 import Parse.Literal            (literal)
+import Parse.Pattern            (expr)
 import Tests.Property.Arbitrary
 
 propertyTests :: Test
 propertyTests =
   testGroup "Parse/Print Agreement Tests"
   [
-    testProperty "Literal test" prop_literal_parse_print
+    testProperty "Literal test" $ prop_parse_print literal
+  , testProperty "Pattern test" $ prop_parse_print expr
   ]
 
-prop_literal_parse_print :: Literal -> Bool
-prop_literal_parse_print l =
-  either (const False) (== l) . iParse literal . show . pretty $ l
+prop_parse_print :: (Pretty a, Arbitrary a, Eq a) => IParser a -> a -> Bool
+prop_parse_print p x =
+  either (const False) (== x) . parse_print p $ x
+
+parse_print :: (Pretty a) => IParser a -> a -> Either String a
+parse_print p = either (Left . show) (Right) . iParse p . show . pretty
