49fc0b63780620e0ca9b28045639d5db590afbda
Evan Czaplicki <info@elm-lang.org>
2014-01-02 23:39:34 -0800

    Create way to crawl over all lets in a program, transforming defs and ports
    Used to combine type annotations with definitions and find ordering
    problems with Definitions and Ports

---
+++ compiler/Transform/Definition.hs
@@ -0,0 +1,29 @@
+{-# OPTIONS_GHC -Wall #-}
+module Transform.Definition where
+
+import Control.Applicative ((<$>))
+import qualified SourceSyntax.Pattern as P
+import SourceSyntax.Expression
+import qualified Transform.Expression as Expr
+
+combineAnnotations :: [ParseDef] -> Either String [Def]
+combineAnnotations = go
+    where
+      msg x = "Syntax Error: The type annotation for '" ++ x ++
+              "' must be directly above its definition."
+
+      go defs =
+          case defs of
+            TypeAnnotation name tipe : Def pat@(P.PVar name') expr : rest | name == name' ->
+                do expr' <- Expr.crawl combineAnnotations expr
+                   let def = Definition pat expr' (Just tipe)
+                   (:) def <$> go rest
+
+            TypeAnnotation name _  : _ -> Left (msg name)
+
+            Def pat expr : rest ->
+                do expr' <- Expr.crawl combineAnnotations expr
+                   let def = Definition pat expr' Nothing
+                   (:) def <$> go rest
+
+            [] -> return []
