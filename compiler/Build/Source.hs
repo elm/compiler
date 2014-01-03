ed4be06a6aa08358e757cd7ec619557efe904482
Evan Czaplicki <info@elm-lang.org>
2014-01-02 23:44:42 -0800

    Update to match new port and decl format

--- compiler/Build/Source.hs
+++ compiler/Build/Source.hs
@@ -14,6 +14,7 @@ import Text.PrettyPrint (Doc)
 import SourceSyntax.Declaration
 import SourceSyntax.Module
 import qualified SourceSyntax.Expression as Expr
+import qualified SourceSyntax.Pattern as Pattern
 import qualified SourceSyntax.Type as Type
 import qualified Parse.Parse as Parse
 import qualified Metadata.Prelude as Prelude
@@ -23,41 +24,41 @@ import qualified Type.Inference as TI
 import qualified Type.Constrain.Declaration as TcDecl
 import qualified Transform.Canonicalize as Canonical
 
-build :: Bool -> Interfaces -> String -> Either [Doc] (MetadataModule () ())
+build :: Bool -> Interfaces -> String -> Either [Doc] MetadataModule
 build noPrelude interfaces source =
   do let add = if noPrelude then id else Prelude.add
          infixes = Map.fromList . map (\(assoc,lvl,op) -> (op,(lvl,assoc)))
                  . concatMap iFixities $ Map.elems interfaces
 
-     modul@(Module _ _ _ decls') <- add `fmap` Parse.program infixes source
-
-     -- check for structural errors
-     Module names exs ims decls <-
-         case Check.mistakes decls' of
-           [] -> return modul
-           ms -> Left ms
+     Module names exs ims decls <- do
+           modul@(Module _ _ _ decls) <- add `fmap` Parse.program infixes source
+           case Check.mistakes decls of
+             [] -> return modul
+             ms -> Left ms
 
      let exports'
              | null exs =
-                 let get = Set.toList . SD.boundVars in
-                 concat [ get pattern | Definition (Expr.Def pattern _) <- decls ] ++
+                 let get = Set.toList . Pattern.boundVars in
+                 concat [ get pattern | Definition (Expr.Definition pattern _ _) <- decls ] ++
                  concat [ map fst ctors | Datatype _ _ ctors _ <- decls ] ++
                  [ name | TypeAlias name _ (Type.Record _ _) _ <- decls ]
              | otherwise = exs
 
-     metaModule <- Canonical.metadataModule interfaces $ MetadataModule {
-           names = names,
-           path = FP.joinPath names,
-           exports = exports',
-           imports = ims,
+     metaModule <- Canonical.metadataModule interfaces $
+         MetadataModule
+         { names = names
+         , path = FP.joinPath names
+         , exports = exports'
+         , imports = ims
            -- reorder AST into strongly connected components
-           program = SD.sortDefs . Expr.dummyLet $ TcDecl.toExpr decls,
-           types = Map.empty,
-           datatypes = [ (name,vars,ctors,ds) | Datatype name vars ctors ds <- decls ],
-           fixities = [ (assoc,level,op) | Fixity assoc level op <- decls ],
-           aliases = [ (name,tvs,tipe,ds) | TypeAlias name tvs tipe ds <- decls ],
-           ports = [ (name,tipe,maybe) | Port name tipe maybe <- decls ]
-          }
+         , program = SD.sortDefs . Expr.dummyLet $ TcDecl.toExpr decls
+         , types = Map.empty
+         , datatypes = [ (name,vars,ctors,ds) | Datatype name vars ctors ds <- decls ]
+         , fixities = [ (assoc,level,op) | Fixity assoc level op <- decls ]
+         , aliases = [ (name,tvs,tipe,ds) | TypeAlias name tvs tipe ds <- decls ]
+         , sendPorts = [ (name,expr,tipe) | Port (Send name expr tipe) <- decls ]
+         , recvPorts = [ (name,expr,tipe) | Port (Recv name expr tipe) <- decls ]
+         }
 
      types <- TI.infer interfaces metaModule
 
