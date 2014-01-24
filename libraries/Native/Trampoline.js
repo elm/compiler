4ef397c3dc06fd9d3a0697953074d13730c6d823
Evan Czaplicki <info@elm-lang.org>
2014-01-24 18:07:07 +0100

    Get rid of dead code, cannot be called because of the type checker

--- libraries/Native/Trampoline.js
+++ libraries/Native/Trampoline.js
@@ -4,8 +4,6 @@ Elm.Native.Trampoline.make = function(elm) {
     elm.Native.Trampoline = elm.Native.Trampoline || {};
     if (elm.Native.Trampoline.values) return elm.Native.Trampoline.values;
 
-    var _E = Elm.Native.Error.make(elm),
-
     // trampoline : Trampoline a -> a
     trampoline = function(t) {
         var tramp = t;
@@ -17,7 +15,6 @@ Elm.Native.Trampoline.make = function(elm) {
                 tramp = tramp._0({ctor: "_Tuple0"});
                 continue;
             }
-            _E.Case("Trampoline", "in Native.Trampoline.trampoline");
         }
     }
 
