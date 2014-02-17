1c832af6b0d3176e76742f5397748b59cef0ddba
Evan Czaplicki <info@elm-lang.org>
2014-02-17 17:38:37 +0100

    Attempt to revamp the Graphics.Input library, much testing still needed

--- runtime/Render/Element.js
+++ runtime/Render/Element.js
@@ -28,15 +28,28 @@ function setProps(props, e) {
         e.appendChild(a);
     }
     if (props.hover.ctor !== '_Tuple0') {
-        var overCount = 0;
+        e.elm_hover_handler = props.hover;
+        e.elm_hover_count = 0;
         e.addEventListener('mouseover', function() {
-            if (overCount++ > 0) return;
-            props.hover(true);
+            if (e.elm_hover_count++ > 0) return;
+            var handler = e.elm_hover_handler;
+            if (handler !== null) {
+                handler(true);
+            }
         });
         e.addEventListener('mouseout', function(evt) {
             if (e.contains(evt.toElement || evt.relatedTarget)) return;
-            overCount = 0;
-            props.hover(false);
+            e.elm_hover_count = 0;
+            var handler = e.elm_hover_handler;
+            if (handler !== null) {
+                handler(false);
+            }
+        });
+    }
+    if (props.click.ctor !== '_Tuple0') {
+        e.elm_click_handler = props.click;
+        e.addEventListener('click', function() {
+            e.elm_click_handler(Tuple0);
         });
     }
     return e;
@@ -318,6 +331,20 @@ function updateProps(node, curr, next) {
         } else {
             node.lastNode.href = props.href;
         }
+    }
+
+    // update hover handlers
+    if (props.hover.ctor !== '_Tuple0') {
+        e.elm_hover_handler = props.hover;
+    } else if (e.elm_hover_handler) {
+        e.elm_hover_handler = null;
+    }
+
+    // update click handlers
+    if (props.click.ctor !== '_Tuple0') {
+        e.elm_click_handler = props.click;
+    } else if (e.elm_click_handler) {
+        e.elm_click_handler = null;
     }
 }
 
