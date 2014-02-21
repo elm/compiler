0304f0b7cc8a187d8454dd7e5d3227e03f8feb92
Evan Czaplicki <info@elm-lang.org>
2014-02-21 13:02:18 -0500

    Change the name of JsonValue to Json and trim dead code

--- libraries/Json.elm
+++ libraries/Json.elm
@@ -4,7 +4,7 @@ module Json where
 {-| Library for working with [JSON](https://en.wikipedia.org/wiki/JSON) values.
 
 # Json Values
-@docs JsonValue
+@docs Json
 
 # Strings
 @docs fromString, toString
@@ -25,46 +25,46 @@ import Native.Json
 import JavaScript (JSObject, JSString)
 
 {-| This datatype can represent all valid values that can be held in a JSON
-object. In Elm, a proper JSON object is represented as a (Dict String JsonValue)
+object. In Elm, a proper JSON object is represented as a (Dict String Json)
 which is a mapping from strings to Json Values. -}
-data JsonValue
+data Json
     = String String
     | Number Float
     | Boolean Bool
     | Null
-    | Array [JsonValue]
-    | Object (Dict.Dict String JsonValue)
+    | Array [Json]
+    | Object (Dict.Dict String Json)
 
 
 -- String Converters
 
-{-| Convert a `JsonValue` into a prettified string.
+{-| Convert a `Json` into a prettified string.
 The first argument is a separator token (e.g. \" \", \"\\n\", etc.) that will
 be used for indentation in the prettified string version of the JSON. -}
-toString : String -> JsonValue -> String
+toString : String -> Json -> String
 toString sep v = JS.toString (Native.Json.toJSString sep v)
 
 {-| Convert a proper JSON object into a JavaScript string.
 Note that the type JSString seen here is not the same as the type constructor
 JsonString used elsewhere in this module. -}
-toJSString : String -> JsonValue -> JSString
+toJSString : String -> Json -> JSString
 toJSString = Native.Json.toJSString
 
 {-| Parse a string representation of a proper JSON object into
 its Elm representation. -}
-fromString : String -> Maybe JsonValue
+fromString : String -> Maybe Json
 fromString s = Native.Json.fromJSString (JS.fromString s)
 
 {-| Parse a JavaScript string representation of a proper JSON object into
 its Elm representation. -}
-fromJSString : JSString -> Maybe JsonValue
+fromJSString : JSString -> Maybe Json
 fromJSString = Native.Json.fromJSString
 
-{-| Convert a JS object into a `JsonValue`. -}
-fromJSObject : JSObject -> JsonValue
+{-| Convert a JS object into a `Json`. -}
+fromJSObject : JSObject -> Json
 fromJSObject = Native.Json.fromJSObject
 
-{-| Convert a `JsonValue` into a `JSObject`. Paired with the
+{-| Convert a `Json` into a `JSObject`. Paired with the
 [`JavaScript.Experimental` library](/docs/JavaScript/Experimental.elm),
 This lets you convert strings into Elm records:
 
@@ -72,63 +72,8 @@ This lets you convert strings into Elm records:
 
        stringToRecord str =
            case fromString str of
-             Just jsonValue -> Just (JS.toRecord (toJSObject jsonValue))
+             Just json -> Just (JS.toRecord (toJSObject json))
              Nothing -> Nothing
 -}
-toJSObject : JsonValue -> JSObject
+toJSObject : Json -> JSObject
 toJSObject = Native.Json.toJSObject
-
- {-- Extract Elm values from Json values
-
- string : JsonValue -> String
- string v = case v of { String s -> s ; _ -> "" }
-
- number : JsonValue -> Float
- number v = case v of { Number n -> n ; _ -> 0 }
-
- boolean : JsonValue -> Bool
- boolean v = case v of { Boolean b -> b ; _ -> False }
-
- array : JsonValue -> [JsonValue]
- array v = case v of { Array a -> a ; _ -> [] }
-
- object : JsonValue -> Dict String JsonValue
- object v = case v of { Object o -> o ; _ -> Dict.empty }
-
-
- -- Extract Elm values from dictionaries of Json values
-
- {-| Find a value in a Json Object using the passed get function. If the key is
- not found, this returns the given default/base value. -}
- find get base =
-   let f key dict =
-           case Dict.lookup key dict of
-             Nothing -> base
-             Just v  -> get v
-   in  f
-
- {-| Find a string value in an Elm Json object. If the key is not found or the
- value found is not a string, this returns the empty string. -}
- findString : String -> Object -> String
- findString = find string ""
-
- {-| Find a number value in an Elm Json object. If the key is not found or the
- value found is not a number, this returns 0 -}
- findNumber : String -> Object -> Float
- findNumber = find number 0
-
- {-| Find a boolean value in an Elm Json object. If the key is not found or the
- value found is not a boolean, this returns the False. -}
- findBoolean : String -> Object -> Bool
- findBoolean = find boolean False
-
- {-| Find an array value in an Elm Json object. If the key is not found or the
- value found is not an array, this returns an empty list. -}
- findArray : String -> Object -> [JsonValue]
- findArray = find array []
-
- {-| Find an object value in an Elm Json object. If the key is not found or the
- value found is not an object, this returns an empty object. -}
- findObject : String -> Object -> Object
- findObject = find object Dict.empty
- --}
