a7bb27d793c04a3cad3ac9efeec58c44dbe9303f
Evan Czaplicki <info@elm-lang.org>
2014-02-21 18:04:27 -0500

    Trim out parts of API that are no longer useful

--- libraries/Json.elm
+++ libraries/Json.elm
@@ -3,77 +3,47 @@ module Json where
 
 {-| Library for working with [JSON](https://en.wikipedia.org/wiki/JSON) values.
 
-# Json Values
-@docs Json
-
-# Strings
-@docs fromString, toString
-
-# JS Strings
-@docs fromJSString, toJSString
-
-# JS Objects
-@docs fromJSObject, toJSObject
+@docs Value, fromString, toString
 
 -}
 
-import Basics (..)
 import Dict
 import Maybe (Maybe)
-import JavaScript as JS
 import Native.Json
-import JavaScript (JSObject, JSString)
 
-{-| This datatype can represent all valid values that can be held in a JSON
-object. In Elm, a proper JSON object is represented as a (Dict String Json)
-which is a mapping from strings to Json Values. -}
-data Json
+{-| This datatype can represent all valid JSON values. A `Value` can be sent
+through ports, and the runtime will do a conversion like this:
+
+      [2,'abc',null] == Array [Number 2, String "abc", Null]
+
+JSON is an intentional subset of JavaScript values which excludes functions.
+This ensures that impure functions cannot sneak into Elm through ports.
+-}
+data Value
     = String String
     | Number Float
     | Boolean Bool
     | Null
-    | Array [Json]
-    | Object (Dict.Dict String Json)
+    | Array [Value]
+    | Object (Dict.Dict String Value)
 
 
--- String Converters
-
-{-| Convert a `Json` into a prettified string.
+{-| Convert a `Value` into a prettified string.
 The first argument is a separator token (e.g. \" \", \"\\n\", etc.) that will
-be used for indentation in the prettified string version of the JSON. -}
-toString : String -> Json -> String
-toString sep v = JS.toString (Native.Json.toJSString sep v)
+be used for indentation in the prettified string version of the JSON.
 
-{-| Convert a proper JSON object into a JavaScript string.
-Note that the type JSString seen here is not the same as the type constructor
-JsonString used elsewhere in this module. -}
-toJSString : String -> Json -> JSString
-toJSString = Native.Json.toJSString
+      toString "" Null         == "null"
+      toString "" (Number 4.0) == "4.0"
+-}
+toString : String -> Value -> String
+toString sep value = Native.Json.toString sep value
 
 {-| Parse a string representation of a proper JSON object into
-its Elm representation. -}
-fromString : String -> Maybe Json
-fromString s = Native.Json.fromJSString (JS.fromString s)
+its Elm representation.
 
-{-| Parse a JavaScript string representation of a proper JSON object into
-its Elm representation. -}
-fromJSString : JSString -> Maybe Json
-fromJSString = Native.Json.fromJSString
-
-{-| Convert a JS object into a `Json`. -}
-fromJSObject : JSObject -> Json
-fromJSObject = Native.Json.fromJSObject
-
-{-| Convert a `Json` into a `JSObject`. Paired with the
-[`JavaScript.Experimental` library](/docs/JavaScript/Experimental.elm),
-This lets you convert strings into Elm records:
-
-       import JavaScript.Experimental as JS
-
-       stringToRecord str =
-           case fromString str of
-             Just json -> Just (JS.toRecord (toJSObject json))
-             Nothing -> Nothing
+      fromString "null"      == Just Null
+      fromString "[true,3]"  == Just (Array [Boolean True, Number 3])
+      fromString "{'abc':4}" == Just (Object (Dict.fromList [("abc", Number 4)]))
 -}
-toJSObject : Json -> JSObject
-toJSObject = Native.Json.toJSObject
+fromString : String -> Maybe Value
+fromString str = Native.Json.fromString str
--- libraries/Native/Json.js
+++ libraries/Native/Json.js
@@ -15,11 +15,14 @@ Elm.Native.Json.make = function(elm) {
     switch (v.ctor) {
     case 'Null'   : return null;
     case 'String' : return JS.fromString(v._0);
+    case 'Number' : return v._0;
+    case 'Boolean': return v._0;
     case 'Object' :
       var obj = {};
       var array = JS.fromList(Dict.toList(v._0));
       for (var i = array.length; i--; ) {
-	obj[JS.fromString(array[i]._0)] = fromValue(array[i]._1);
+        var entry = array[i];
+        obj[JS.fromString(entry._0)] = fromValue(entry._1);
       }
       return obj;
     case 'Array'  :
@@ -28,20 +31,18 @@ Elm.Native.Json.make = function(elm) {
 	array[i] = fromValue(array[i]);
       }
       return array;
-    default :
-      return v._0;
     }
   }
 
-  function toPrettyJSString(sep, obj) {
-    return JSON.stringify(fromValue(obj), null, JS.fromString(sep));
+  function toString(sep, value) {
+    return JSON.stringify(fromValue(value), null, JS.fromString(sep));
   }
 
   function toValue(v) {
     switch (typeof v) {
-    case 'string' : return { ctor:"String", _0: JS.toString(v) };
-    case 'number' : return { ctor:"Number", _0: JS.toFloat(v)  };
-    case 'boolean': return { ctor:"Boolean"  , _0: JS.toBool(v)   };
+    case 'string' : return { ctor:"String" , _0: JS.toString(v) };
+    case 'number' : return { ctor:"Number" , _0: JS.toFloat(v)  };
+    case 'boolean': return { ctor:"Boolean", _0: JS.toBool(v)   };
     case 'object' :
       if (v === null) return { ctor:"Null" };
       if (v instanceof Array) {
@@ -54,19 +55,17 @@ Elm.Native.Json.make = function(elm) {
     }
   }
 
-  function fromJSString(str) {
+  function fromString(str) {
     try {
-	return Maybe.Just(toValue(JSON.parse(str)));
+	return Maybe.Just(toValue(JSON.parse(JS.fromString(str))));
     } catch (e) {
 	return Maybe.Nothing;
     }
   }
 
   return elm.Native.Json.values = {
-      toJSString : F2(toPrettyJSString),
-      fromJSString : fromJSString,
-      toJSObject : fromValue,
-      fromJSObject : toValue
+      toString : F2(toString),
+      fromString : fromString,
   };
 
 };
