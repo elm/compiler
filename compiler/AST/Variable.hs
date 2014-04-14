35e90f60aeebc638cb6274561030407916b51ad5
Evan Czaplicki <info@elm-lang.org>
2014-04-13 18:25:16 -0700

    Handle BuiltIn properly

--- compiler/AST/Variable.hs
+++ compiler/AST/Variable.hs
@@ -1,13 +1,9 @@
-
+{-# OPTIONS_GHC -W #-}
 module AST.Variable where
 
 import Data.Binary
 import Control.Applicative ((<$>), (<*>))
 import Text.PrettyPrint as P
-import AST.PrettyPrint
-
-import qualified Data.List as List
-import qualified Text.PrettyPrint as P
 import AST.PrettyPrint
 
 newtype Raw = Raw String
@@ -37,6 +33,7 @@ instance ToString Canonical where
   toString (Canonical home name) =
     case home of
       Local -> name
+      BuiltIn -> name
       Module path -> path ++ "." ++ name
 
 -- | A listing of values. Something like (a,b,c) or (..) or (a,b,..)
@@ -59,12 +56,7 @@ instance Pretty Raw where
     pretty (Raw var) = variable var
 
 instance Pretty Canonical where
-    pretty (Canonical home name) =
-        P.text (home' ++ name)
-        where
-          home' = case home of
-                    Local -> ""
-                    Module name -> name ++ "."
+    pretty var = P.text (toString var)
 
 instance Pretty a => Pretty (Listing a) where
   pretty (Listing explicits open) =
@@ -84,12 +76,14 @@ instance Binary Canonical where
     put (Canonical home name) =
         case home of
           Local -> putWord8 0 >> put name
-          Module path -> putWord8 1 >> put path >> put name
+          BuiltIn -> putWord8 1 >> put name
+          Module path -> putWord8 2 >> put path >> put name
 
     get = do tag <- getWord8
              case tag of
                0 -> Canonical Local <$> get
-               1 -> Canonical . Module <$> get <*> get
+               1 -> Canonical BuiltIn <$> get
+               2 -> Canonical . Module <$> get <*> get
                _ -> error "Unexpected tag when deserializing canonical variable"
 
 instance Binary Value where
@@ -97,14 +91,16 @@ instance Binary Value where
         let put' n info = putWord8 n >> put info in
         case portable of
           Value name -> put' 0 name
+          Alias name -> put' 1 name
           ADT name ctors ->
-              do put' 1 name
+              do put' 2 name
                  put ctors
 
     get = do tag <- getWord8
              case tag of
                0 -> Value <$> get
-               1 -> ADT <$> get <*> get
+               1 -> Alias <$> get
+               2 -> ADT <$> get <*> get
                _ -> error "Error reading valid import/export information from serialized string"
 
 instance (Binary a) => Binary (Listing a) where
