17d8b5878ed4919018f993ff4ffc1ea88acbed23
Evan Czaplicki <info@elm-lang.org>
2014-05-02 23:52:58 -0700

    Redo crate program with clearer names

--- tests/compiler/test-files/good/Crate.elm
+++ tests/compiler/test-files/good/Crate.elm
@@ -5,34 +5,60 @@ import Http (..)
 import MJS (..)
 import Graphics.WebGL (..)
 
-pul = {pos=v3 -1  1 0, coord=v3 0 1 0}
-pur = {pos=v3  1  1 0, coord=v3 1 1 0}
-pbl = {pos=v3 -1 -1 0, coord=v3 0 0 0}
-pbr = {pos=v3  1 -1 0, coord=v3 1 0 0}
+-- Define the mesh for a crate
+crate : [Triangle { pos:V3, coord:V3 }]
+crate = concatMap rotatedFace [ (0,0), (90,0), (180,0), (270,0), (0,90), (0,-90) ]
 
-face : [Triangle { pos : V3, coord : V3 }]
-face = [(pul,pur,pbl),(pbl,pur,pbr)]
+rotatedFace : (Float,Float) -> [Triangle { pos:V3, coord:V3 }]
+rotatedFace (angleX,angleY) = 
+  let x = makeRotate (degrees angleX) (v3 1 0 0)
+      y = makeRotate (degrees angleY) (v3 0 1 0)
+      t = x `mul` y `mul` makeTranslate (v3 0 0 1)
+  in
+      map (mapTriangle (\x -> {x | pos <- mul4x4 t x.pos })) face
 
-rotFace : Float -> Float -> [Triangle { pos : V3, coord : V3 }]
-rotFace turnH turnV = 
-  let x = makeRotate (turnV * pi / 2) (v3 1 0 0)
-      y = makeRotate (turnH * pi / 2) (v3 0 1 0)
-      s = makeTranslate (v3 0 0 1)
-      t = mul (mul x y) s
-  in map (mapTriangle (\x -> {x | pos <- mul4x4 t x.pos })) face
+face : [Triangle { pos:V3, coord:V3 }]
+face =
+  let topLeft     = { pos = v3 -1  1 0, coord = v3 0 1 0 }
+      topRight    = { pos = v3  1  1 0, coord = v3 1 1 0 }
+      bottomLeft  = { pos = v3 -1 -1 0, coord = v3 0 0 0 }
+      bottomRight = { pos = v3  1 -1 0, coord = v3 1 0 0 }
+  in
+      [ (topLeft,topRight,bottomLeft), (bottomLeft,topRight,bottomRight) ]
 
-mesh : [Triangle { pos : V3, coord : V3 }]
-mesh = concat . map (uncurry rotFace) <|
-        [ (0,0)
-        , (1,0)
-        , (2,0)
-        , (3,0)
-        , (0,1)
-        , (0,-1)
-        ]
+-- View
+view : Float -> M4x4
+view angle =
+    foldr1 mul [ perspective
+               , camera
+               , makeRotate (3*angle) (v3 0 1 0)
+               , makeRotate (2*angle) (v3 1 0 0)
+               ]
 
-vert : Shader { pos : V3, coord : V3 } { u | view : M4x4 } { vcoord : V2 }
-vert = [glShader|
+perspective : M4x4
+perspective = makePerspective 45 1 0.01 100
+
+camera : M4x4
+camera = makeLookAt (v3 0 0 5) (v3 0 0 0) (v3 0 1 0)
+
+-- Putting it together
+main : Signal Element
+main = webgl (400,400) <~ lift2 scene (loadTex "woodCrate.jpg") (view <~ angle)
+
+angle : Signal Float
+angle = foldp (\dt theta -> theta + dt / 10000) 0 (fps 25)
+
+scene : Response Texture -> M4x4 -> [Model]
+scene response view =
+  case response of
+    Waiting     -> []
+    Failure _ _ -> []
+    Success tex -> [model vertexShader fragmentShader crate { crate = tex, view = view }]
+
+-- Shaders
+vertexShader : Shader { pos:V3, coord:V3 } { u | view:M4x4 } { vcoord:V2 }
+vertexShader = [glShader|
+
 attribute vec3 pos;
 attribute vec3 coord;
 uniform mat4 view;
@@ -41,44 +67,17 @@ void main () {
   gl_Position = view * vec4(pos, 1.0);
   vcoord = coord.xy;
 }
+
 |]
 
-frag : Shader {} { u | crate : Texture } { vcoord : V2 }
-frag = [glShader|
+fragmentShader : Shader {} { u | crate:Texture } { vcoord:V2 }
+fragmentShader = [glShader|
+
 precision mediump float;
 uniform sampler2D crate;
 varying vec2 vcoord;
 void main () {
   gl_FragColor = texture2D(crate, vcoord);
 }
+
 |]
-
-crateTex : Signal (Response Texture)
-crateTex = loadTex "woodCrate.JPG"
-
-per : M4x4
-per = makePerspective 45 1 0.01 100
-
-cam : M4x4
-cam = makeLookAt (v3 0 0 5) (v3 0 0 0) (v3 0 1 0)
-
-angle : Signal Float
-angle = foldp (\_ n -> n + 0.02) 0 (fps 25)
-
-view : Float -> M4x4
-view t = 
-  let x = makeRotate (2*t) (v3 1 0 0)
-      y = makeRotate (3*t) (v3 0 1 0)
-  in foldr mul identity [per,cam,y,x]
-
-content : Response Texture -> M4x4 -> [Model]
-content response view = case response of
-  Waiting -> []
-  Failure i s -> []
-  Success tex -> [model vert frag mesh { crate = tex, view = view }]
-
-main = webgl (400,400) <~ (content <~ crateTex ~ (view <~ angle))
-
---main = asText (List.length mesh)
-
-
