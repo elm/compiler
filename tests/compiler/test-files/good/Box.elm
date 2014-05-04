244efb8ef8eb8a8524c177f45f5dd1efe87dd90f
Evan Czaplicki <info@elm-lang.org>
2014-05-04 09:23:15 -0700

    Revamp to be a bit clearer to beginners like me

--- tests/compiler/test-files/good/Box.elm
+++ tests/compiler/test-files/good/Box.elm
@@ -5,72 +5,77 @@ import MJS (V3,M4x4,v3,makeRotate,makePerspective,makeLookAt,mul)
 import Graphics.WebGL (Triangle, zipTriangle, Shader, Model, model, webgl)
 import Window(dimensions)
 
--- Define what our geometry looks like
+-- Create a cube in which each point has a color
 
-p0 = v3  1  1  1
-p1 = v3 -1  1  1
-p2 = v3 -1 -1  1
-p3 = v3  1 -1  1
-p4 = v3  1 -1 -1
-p5 = v3  1  1 -1
-p6 = v3 -1  1 -1
-p7 = v3 -1 -1 -1
+type ColorPoint = { color:V3, point:V3 }
 
-front  = [(p0,p1,p2),(p2,p3,p0)]
-back   = [(p5,p6,p7),(p7,p4,p5)]
-right  = [(p0,p3,p4),(p4,p5,p0)]
-left   = [(p1,p2,p7),(p7,p6,p1)]
-top    = [(p0,p5,p6),(p6,p1,p0)]
-bottom = [(p3,p4,p7),(p7,p2,p3)]
+face : Color -> V3 -> V3 -> V3 -> V3 -> [Triangle ColorPoint]
+face color a b c d =
+  let toV3 color =
+        let c = toRgb color
+        in  v3 (toFloat c.red / 255) (toFloat c.green / 255) (toFloat c.blue / 255)
+      p = ColorPoint (toV3 color)
+  in
+      [ (p a, p b, p c), (p c, p d, p a) ]
 
-positions = front ++ back ++ right ++ left ++ top ++ bottom
+cube : [Triangle ColorPoint]
+cube =
+  let rft = v3  1  1  1   -- right, front, top
+      lft = v3 -1  1  1   -- left,  front, top
+      lbt = v3 -1 -1  1
+      rbt = v3  1 -1  1
+      rbb = v3  1 -1 -1
+      rfb = v3  1  1 -1
+      lfb = v3 -1  1 -1
+      lbb = v3 -1 -1 -1
+  in
+      concat [ face green  rft rfb rbb rbt   -- right
+             , face blue   rft rfb lfb lft   -- front
+             , face purple lft lfb lbb lbt   -- left
+             , face orange rbt rbb lbb lbt   -- back
+             ]
 
-toV3 color =
-  let c = toRgb color
-  in  v3 (toFloat c.red / 255) (toFloat c.green / 255) (toFloat c.blue / 255)
+-- Create the scene
 
-colors : [Triangle V3]
-colors =
-  concatMap (\c' -> let c = toV3 c' in repeat 2 (c,c,c))
-  [gray,red,green,blue,yellow,purple]
-
-mesh : [Triangle {pos : V3, color : V3}]
-mesh = zipWith (zipTriangle (\pos color -> { pos = pos, color = color })) positions colors
-
-per : M4x4
-per = makePerspective 45 1 0.01 100
-
-cam : M4x4
-cam = makeLookAt (v3 0 0 5) (v3 0 0 0) (v3 0 1 0)
+main : Signal Element
+main = webgl (400,400) <~ lift scene angle
 
 angle : Signal Float
-angle = foldp (\dt theta -> theta + dt / 10000) 0 (fps 25)
+angle = foldp (\dt theta -> theta + dt / 5000) 0 (fps 25)
 
-rot : Signal { rot : M4x4, per : M4x4, cam : M4x4, shade : Float }
-rot = (\t -> { rot = mul (makeRotate (3*t) (v3 0 1 0)) (makeRotate (2*t) (v3 1 0 0)), per = per, cam = cam, shade = 0.8 }) <~ angle
+scene : Float -> [Model]
+scene angle =
+    [ model vertexShader fragmentShader cube (uniforms angle) ]
 
-draw : Signal [Model]
-draw = combine [ model vertexShader fragmentShader mesh <~ rot ]
+uniforms : Float -> { rotation:M4x4, perspective:M4x4, camera:M4x4, shade:Float }
+uniforms t =
+    { rotation = mul (makeRotate (3*t) (v3 0 1 0)) (makeRotate (2*t) (v3 1 0 0))
+    , perspective = makePerspective 45 1 0.01 100
+    , camera = makeLookAt (v3 0 0 5) (v3 0 0 0) (v3 0 1 0)
+    , shade = 0.8
+    }
 
-main = webgl <~ dimensions ~ draw
+-- Shaders
 
-vertexShader : Shader {a | pos : V3, color : V3} {b | rot : M4x4, per : M4x4, cam : M4x4} {vcolor : V3}
+vertexShader : Shader { attr | point:V3, color:V3 }
+                      { unif | rotation:M4x4, perspective:M4x4, camera:M4x4 }
+                      { vcolor:V3 }
 vertexShader = [glShader|
 
-attribute vec3 pos;
+attribute vec3 point;
 attribute vec3 color;
-uniform mat4 per;
-uniform mat4 cam;
-uniform mat4 rot;
+uniform mat4 perspective;
+uniform mat4 camera;
+uniform mat4 rotation;
 varying vec3 vcolor;
 void main () {
-    gl_Position = per * cam * rot * vec4(pos, 1.0);
+    gl_Position = perspective * camera * rotation * vec4(point, 1.0);
     vcolor = color;
 }
 
 |]
 
-fragmentShader : Shader {} {b | shade : Float} {vcolor : V3}
+fragmentShader : Shader {} {u | shade : Float} {vcolor : V3}
 fragmentShader = [glShader|
 
 precision mediump float;
