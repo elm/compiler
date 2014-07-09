6623419d9bd535e254a01548e77834998f3094ae
Evan Czaplicki <info@elm-lang.org>
2014-07-09 14:24:35 +0200

    Add native support for alt and meta keys

--- libraries/Native/Signal/Keyboard.js
+++ libraries/Native/Signal/Keyboard.js
@@ -17,27 +17,36 @@ Elm.Native.Keyboard.make = function(elm) {
     var NList = Elm.Native.List.make(elm);
     var Utils = Elm.Native.Utils.make(elm);
 
-    var downEvents = Signal.constant(0);
-    var upEvents = Signal.constant(0);
-    var blurEvents = Signal.constant(0);
+    var downEvents = Signal.constant(null);
+    var upEvents = Signal.constant(null);
+    var blurEvents = Signal.constant(null);
 
     elm.addListener([downEvents.id], document, 'keydown', function down(e) {
-        elm.notify(downEvents.id, e.keyCode);
+        elm.notify(downEvents.id, e);
     });
 
     elm.addListener([upEvents.id], document, 'keyup', function up(e) {
-        elm.notify(upEvents.id, e.keyCode);
+        elm.notify(upEvents.id, e);
     });
 
     elm.addListener([blurEvents.id], document, 'blur', function blur(e) {
-        elm.notify(blurEvents.id, NList.Nil);
+        elm.notify(blurEvents.id, null);
     });
+
+    function state(alt, meta, keyCodes) {
+        return {
+            alt: alt,
+            meta: meta,
+            keyCodes: keyCodes
+        };
+    }
+    var emptyState = state(false, false, NList.Nil);
 
     function KeyMerge(down, up, blur) {
         var args = [down,up,blur];
         this.id = Utils.guid();
         // Ignore starting values here
-        this.value = NList.Nil
+        this.value = emptyState;
         this.kids = [];
         
         var n = args.length;
@@ -48,18 +57,22 @@ Elm.Native.Keyboard.make = function(elm) {
             ++count;
             if (changed) { 
                 // We know this a change must only be one of the following cases
-                if (parentID === down.id && !A2(NList.member, down.value, this.value)) {
+                if (parentID === down.id && !A2(NList.member, down.value.keyCode, this.value.keyCodes)) {
                     isChanged = true;
-                    this.value = NList.Cons(down.value, this.value); 
+                    var v = down.value;
+                    var newCodes = NList.Cons(v.keyCode, this.value.keyCodes);
+                    this.value = state(v.altKey, v.metaKey, newCodes);
                 }
                 else if (parentID === up.id) {
                     isChanged = true;
-                    var notEq = function(kc) { return kc !== up.value };
-                    this.value = A2(NList.filter, notEq, this.value);
+                    var v = up.value;
+                    var notEq = function(kc) { return kc !== v.keyCode };
+                    var newCodes = A2(NList.filter, notEq, this.value.keyCodes);
+                    this.value = state(v.altKey, v.metaKey, newCodes);
                 }
                 else if (parentID === blur.id) {
                     isChanged = true;
-                    this.value = NList.Nil;
+                    this.value = emptyState;
                 }
             }
             if (count == n) {
@@ -71,45 +84,65 @@ Elm.Native.Keyboard.make = function(elm) {
 
         for (var i = n; i--; ) {
             args[i].kids.push(this);
+            args[i].defaultNumberOfKids += 1;
         }
     }
 
-    var keysDown = Signal.dropRepeats(new KeyMerge(downEvents,upEvents,blurEvents));
+    var keyMerge = new KeyMerge(downEvents,upEvents,blurEvents);
 
+    // select a part of a keyMerge and dropRepeats the result
     function keySignal(f) {
-        var signal = A2(Signal.lift, f, keysDown);
+        var signal = A2(Signal.lift, f, keyMerge);
         // must set the default number of kids to make it possible to filter
         // these signals if they are not actually used.
-        keysDown.defaultNumberOfKids += 1;
+        keyMerge.defaultNumberOfKids += 1;
         signal.defaultNumberOfKids = 1;
-        var filtered = Signal.dropRepeats(signal)
+        var filtered = Signal.dropRepeats(signal);
         filtered.defaultNumberOfKids = 0;
         return filtered;
     }
 
+    // break keyMerge into parts
+    var keysDown = keySignal(function getKeyCodes(v) {
+        return v.keyCodes;
+    });
+    var alt = keySignal(function getKeyCodes(v) {
+        return v.alt;
+    });
+    var meta = keySignal(function getKeyCodes(v) {
+        return v.meta;
+    });
+
     function dir(up, down, left, right) {
-        function f(ks) {
+        function toDirections(state) {
+            var keyCodes = state.keyCodes;
             var x = 0, y = 0;
-            while (ks.ctor === "::") {
-                switch (ks._0) {
+            while (keyCodes.ctor === "::") {
+                switch (keyCodes._0) {
                 case left : --x; break;
                 case right: ++x; break;
                 case up   : ++y; break;
                 case down : --y; break;
                 }
-                ks = ks._1;
+                keyCodes = keyCodes._1;
             }
             return { _:{}, x:x, y:y };
         }
-        return keySignal(f);
+        return keySignal(toDirections);
     }
 
-    function is(key) { return keySignal(NList.member(key)); }
+    function is(key) {
+        return keySignal(function(v) {
+            return A2( NList.member, key, v.keyCodes );
+        });
+    }
 
     var lastPressed = downEvents;
 
     return elm.Native.Keyboard.values = {
         isDown:is,
+        alt: alt,
+        meta: meta,
         directions:F4(dir),
         keysDown:keysDown,
         lastPressed:lastPressed
