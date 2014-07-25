f11d75ed404677aafac3b0626c903ac9cf5cd64c
Andrew Shulaev <a-shulayev@ya.ru>
2014-07-25 15:00:43 +0200

    Started to work on printing out documentation for TLDs

--- compiler/Elm/Internal/Documentation.hs
+++ compiler/Elm/Internal/Documentation.hs
@@ -1,41 +1,82 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# OPTIONS -W -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Elm.Internal.Documentation where
 
-import Control.Applicative ((<$>),(<*>))
-import Control.Monad
 import Data.Aeson
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Text (Text)
+import GHC.Generics
 
-data Document = Doc
-    { moduleName :: String
-    , structure :: String
-    , entries :: [Entry]
-    } deriving (Show)
+import qualified AST.Type as T
+import qualified AST.Variable as Var
+import qualified AST.Module as M
 
-data Entry = Entry
+type Type = T.CanonicalType
+
+data ModuleDocument = ModuleDocument
     { name :: String
-    , comment :: String
+    , document :: String
+    , values :: [Document]
+    , aliases :: [Document]
+    , datatypes :: [Document]
+    } deriving (Generic)
+
+instance ToJSON ModuleDocument
+
+data Document = Document
+    { docName :: String
     , raw :: String
-    , assocPrec :: Maybe (String,Int)
+    , comment :: String
+    , body :: Map Text Value
     } deriving (Show)
 
-instance FromJSON Document where
-    parseJSON (Object v) =
-        Doc <$> v .: "name"
-            <*> v .: "document"
-            <*> (concat <$> sequence [ v .: "aliases", v .: "datatypes", v .: "values" ])
+instance ToJSON Document where
+  toJSON doc =
+    let processPair (x, y) = x .= y in
+    object $ [ "name" .= docName doc
+             , "raw" .= raw doc
+             , "comment" .= comment doc
+             ] ++ map processPair (Map.toList $ body doc)
 
-    parseJSON _ = fail "Conversion to Document was expecting an object"
+instance ToJSON var => ToJSON (T.Type var) where
+  toJSON tipe =
+    object $
+    case tipe of
+      T.Lambda _ _ ->
+        let tipes = T.collectLambdas tipe in
+        [ "tag" .= ("function" :: Text)
+        , "args" .= toJSON (init tipes)
+        , "result" .= toJSON (last tipes)
+        ]
 
-instance FromJSON Entry where
-    parseJSON (Object v) =
-        Entry <$> v .: "name"
-              <*> v .: "comment"
-              <*> v .: "raw"
-              <*> (liftM2 (,) <$> v .:? "associativity"
-                              <*> v .:? "precedence")
+      T.Var x ->
+        [ "tag" .= ("var" :: Text)
+        , "name" .= toJSON x
+        ]
 
-    parseJSON _ = fail "Conversion to Entry was expecting an object"
+      T.App n ts ->
+        [ "tag" .= ("adt" :: Text)
+        , "name" .= toJSON n
+        , "args" .= map toJSON ts
+        ]
 
-data Content = Markdown String | Value String
-               deriving Show
+      T.Record fields ext ->
+        [ "tag" .= ("record" :: Text)
+        , "extension" .= toJSON ext
+        ]
+
+      T.Type var ->
+        [ "tag" .= ("type" :: Text) -- ???
+        , "name" .= toJSON var
+        ]
+
+      T.Aliased _ typ ->
+        [ "tag" .= ("aliased" :: Text) -- ???
+        , "type" .= toJSON typ
+        ]
+
+instance ToJSON Var.Canonical where
+  toJSON var = toJSON $ Var.name var
 
