481a7301ee66c9f016a8f8a2b1c7c2eac1d8121e
Christian Widera <xash@posteo.de>
2014-08-08 18:01:02 +0200

    Fix getSlot: mixed up search direction if bit magic failed

--- libraries/Native/Array.js
+++ libraries/Native/Array.js
@@ -35,10 +35,8 @@ Elm.Native.Array.make = function(elm) {
     function unsafeGet(i, array) {
       for (var x = array.height; x > 0; x--) {
         var slot = i >> (x * 5);
-        if (slot > 0) {
-          while (array.lengths[slot - 1] > i) { slot--; }
-          i -= array.lengths[slot - 1];
-        }
+        while (array.lengths[slot] <= i) slot++;
+        if (slot > 0)  i -= array.lengths[slot - 1];
         array = array.table[slot];
       }
       return array.table[i];
@@ -558,7 +556,7 @@ Elm.Native.Array.make = function(elm) {
     // find the exact slot via forward searching in  "lengths". Returns the index.
     function getSlot(i, a) {
       var slot = i >> (5 * a.height);
-      while (a.lengths[slot - 1] > i) { slot--; }
+      while (a.lengths[slot] <= i) slot++;
       return slot;
     }
 
