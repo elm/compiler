9cbd9ba4658ab8645592b65bbcf7a86f4d031abd
Evan Czaplicki <info@elm-lang.org>
2014-09-13 11:26:04 -0700

    style: follow the new Haskell style guide better

--- compiler/Generate/Cases.hs
+++ compiler/Generate/Cases.hs
@@ -3,28 +3,31 @@ module Generate.Cases (toMatch, Match (..), Clause (..), matchSubst, newVar) whe
 
 import Control.Applicative ((<$>))
 import Control.Arrow (first)
+import Control.Monad.State (State)
 import qualified Control.Monad.State as State
 import Data.List (groupBy,sortBy)
 import Data.Maybe (fromMaybe)
 
 import qualified AST.Annotation as A
 import qualified AST.Expression.General as Expr
-import AST.Expression.Canonical as Canonical
+import qualified AST.Expression.Canonical as Canonical
 import qualified AST.Literal as Literal
 import qualified AST.Pattern as P
 import qualified AST.Variable as Var
 import Transform.Substitute (subst)
 
-toMatch :: [(P.CanonicalPattern, Canonical.Expr)] -> State.State Int (String, Match)
+toMatch :: [(P.CanonicalPattern, Canonical.Expr)] -> State Int (String, Match)
 toMatch patterns = do
   v <- newVar
   (,) v <$> match [v] (map (first (:[])) patterns) Fail
 
-newVar :: State.State Int String
+
+newVar :: State Int String
 newVar =
- do n <- State.get
-    State.modify (+1)
-    return $ "_v" ++ show n
+  do  n <- State.get
+      State.modify (+1)
+      return $ "_v" ++ show n
+
 
 data Match
     = Match String [Clause] Match
@@ -34,37 +37,51 @@ data Match
     | Seq [Match]
       deriving Show
 
+
 data Clause =
     Clause (Either Var.Canonical Literal.Literal) [String] Match
     deriving Show
+
+
+type Case =
+    ([P.CanonicalPattern], Canonical.Expr)
+
 
 matchSubst :: [(String,String)] -> Match -> Match
 matchSubst pairs match =
     case match of
       Break -> Break
-      Fail  -> Fail
-      Seq ms -> Seq (map (matchSubst pairs) ms)
+
+      Fail -> Fail
+      
+      Seq ms ->
+          Seq (map (matchSubst pairs) ms)
+      
       Other (A.A a e) ->
           Other . A.A a $ foldr ($) e $ map (\(x,y) -> subst x (Expr.localVar y)) pairs
+      
       Match n cs m ->
           Match (varSubst n) (map clauseSubst cs) (matchSubst pairs m)
-          where
-            varSubst v = fromMaybe v (lookup v pairs)
-            clauseSubst (Clause c vs m) =
-                Clause c (map varSubst vs) (matchSubst pairs m)
+        where
+          varSubst v = fromMaybe v (lookup v pairs)
+          clauseSubst (Clause c vs m) =
+              Clause c (map varSubst vs) (matchSubst pairs m)
+
 
 isCon :: ([P.Pattern var], expr) -> Bool
 isCon ([] , _) = noMatch "isCon"
 isCon (p:_, _) =
-  case p of
-    P.Data _ _  -> True
-    P.Literal _ -> True
-    _           -> False
+    case p of
+      P.Data _ _  -> True
+      P.Literal _ -> True
+      _           -> False
+
 
 isVar :: ([P.Pattern var], expr) -> Bool
 isVar p = not (isCon p)
 
-match :: [String] -> [([P.CanonicalPattern], Canonical.Expr)] -> Match -> State.State Int Match
+
+match :: [String] -> [Case] -> Match -> State Int Match
 match variables cases match =
     case (variables, cases, match) of
       ([], [], _) ->
@@ -87,61 +104,84 @@ match variables cases match =
             cases' = map (dealias var) cases
 
 
-dealias :: String -> ([P.CanonicalPattern], Canonical.Expr) -> ([P.CanonicalPattern], Canonical.Expr)
+dealias :: String -> Case -> Case
 dealias _ ([], _) = noMatch "dealias"
 dealias v c@(p:ps, A.A a e) =
     case p of
       P.Alias x pattern -> (pattern:ps, A.A a $ subst x (Expr.localVar v) e)
       _ -> c
 
-matchVar :: [String] -> [([P.CanonicalPattern], Canonical.Expr)] -> Match -> State.State Int Match
+
+matchVar :: [String] -> [Case] -> Match -> State Int Match
 matchVar [] _ _ = noMatch "matchVar"
-matchVar (v:vs) cs def = match vs (map subVar cs) def
+matchVar (v:vs) cs def =
+    match vs (map subVar cs) def
   where
     subVar ([], _) = noMatch "matchVar.subVar"
-    subVar (p:ps, (A.A a e)) = (ps, A.A a $ subOnePattern p e)
-        where
-          subOnePattern pattern e =
+    subVar (p:ps, (A.A a expr)) =
+        (ps, A.A a $ subOnePattern p expr)
+      where
+        subOnePattern pattern expr =
             case pattern of
-              P.Var x     -> subst x (Expr.localVar v) e
-              P.Anything  -> e
+              P.Anything -> expr
+
+              P.Var x ->
+                  subst x (Expr.localVar v) expr
+
               P.Record fs ->
-                 foldr (\x -> subst x (Expr.Access (A.A a (Expr.localVar v)) x)) e fs
+                 foldr (\x -> subst x (Expr.Access (A.A a (Expr.localVar v)) x)) expr fs
+
               _ -> noMatch "matchVar.subVar"
 
-matchCon :: [String] -> [([P.CanonicalPattern], Canonical.Expr)] -> Match -> State.State Int Match
-matchCon [] _ _ = noMatch "matchCon"
-matchCon (v:vs) cs def = (flip (Match v) def) <$> mapM toClause css
-    where
-      css = groupBy eq (sortBy cmp cs)
 
-      cmp (p1:_,_) (p2:_,_) =
-        case (p1,p2) of
-          (P.Data n1 _, P.Data n2 _) -> compare n1 n2
-          _ -> compare p1 p2
-      cmp _ _ = noMatch "matchCon.cmp"
+matchCon :: [String] -> [Case] -> Match -> State Int Match
+matchCon variables cases match =
+    case variables of
+      [] -> noMatch "matchCon"
 
-      eq (p1:_,_) (p2:_,_) =
-        case (p1,p2) of
-          (P.Data n1 _, P.Data n2 _) -> n1 == n2
-          _ -> p1 == p2
-      eq _ _ = noMatch "matchCon.eq"
+      var : _vars ->
+          flip (Match var) match <$> mapM toClause caseGroups
+        where
+          caseGroups :: [[Case]]
+          caseGroups =
+              groupBy eq (sortBy cmp cases)
 
-      toClause cs =
-        case head cs of
-          (P.Data name _ : _, _) -> matchClause (Left name) (v:vs) cs Break
-          (P.Literal lit : _, _) -> matchClause (Right lit) (v:vs) cs Break
-          _ -> noMatch "matchCon"
+          eq :: Case -> Case -> Bool
+          eq (patterns1, _) (patterns2, _) =
+              case (patterns1, patterns2) of
+                (pattern1:_, pattern2:_) ->
+                    case (pattern1, pattern2) of
+                      (P.Data name1 _, P.Data name2 _) -> name1 == name2
+                      (_, _) -> pattern1 == pattern2
+                (_, _) -> noMatch "matchCon.eq"
 
-matchClause :: Either Var.Canonical Literal.Literal
-            -> [String]
-            -> [([P.CanonicalPattern], Canonical.Expr)]
-            -> Match
-            -> State.State Int Clause
+          cmp :: Case -> Case -> Ordering
+          cmp (patterns1, _) (patterns2, _) =
+              case (patterns1, patterns2) of
+                (pattern1:_, pattern2:_) ->
+                    case (pattern1, pattern2) of
+                      (P.Data name1 _, P.Data name2 _) -> compare name1 name2
+                      (_, _) -> compare pattern1 pattern2
+                (_, _) -> noMatch "matchCon.cmp"
+
+          toClause :: [Case] -> State Int Clause
+          toClause cases =
+              case head cases of
+                (P.Data name _ : _, _) ->
+                    matchClause (Left name) variables cases Break
+
+                (P.Literal lit : _, _) ->
+                    matchClause (Right lit) variables cases Break
+
+                _ -> noMatch "matchCon"
+
+
+matchClause :: Either Var.Canonical Literal.Literal -> [String] -> [Case] -> Match
+            -> State Int Clause
 matchClause _ [] _ _ = noMatch "matchClause"
-matchClause c (_:vs) cs def =
+matchClause c (_:vs) cs mtch =
     do vs' <- getVars
-       Clause c vs' <$> match (vs' ++ vs) (map flatten cs) def
+       Clause c vs' <$> match (vs' ++ vs) (map flatten cs) mtch
     where
       flatten ([], _) = noMatch "matchClause.flatten"
       flatten (p:ps, e) =
@@ -150,17 +190,20 @@ matchClause c (_:vs) cs def =
             P.Literal _  -> (ps, e)
             _ -> noMatch "matchClause.flatten"
 
+      getVars :: State Int [String]
       getVars =
           case head cs of
             (P.Data _ ps : _, _) -> State.forM ps (const newVar)
             (P.Literal _ : _, _) -> return []
             _ -> noMatch "matchClause.getVars"
 
-matchMix :: [String] -> [([P.CanonicalPattern], Canonical.Expr)] -> Match -> State.State Int Match
+
+matchMix :: [String] -> [Case] -> Match -> State Int Match
 matchMix vs cs def =
     State.foldM (flip $ match vs) def (reverse css)
   where
     css = groupBy (\p1 p2 -> isCon p1 == isCon p2) cs
+
 
 noMatch :: String -> a
 noMatch name =
