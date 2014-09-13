034d52e14792eb9817abbb655ce3becf945dd03b
Evan Czaplicki <info@elm-lang.org>
2014-09-13 11:36:56 -0700

    style: follow new Haskell style guide

--- compiler/Transform/Substitute.hs
+++ compiler/Transform/Substitute.hs
@@ -10,42 +10,77 @@ import qualified AST.Expression.Canonical as Canonical
 import qualified AST.Pattern as Pattern
 import qualified AST.Variable as V
 
+
 subst :: String -> Canonical.Expr' -> Canonical.Expr' -> Canonical.Expr'
-subst old new expr =
+subst old new expression =
     let f (A a e) = A a (subst old new e) in
-    case expr of
-      Range e1 e2 -> Range (f e1) (f e2)
-      ExplicitList es -> ExplicitList (map f es)
-      Binop op e1 e2 -> Binop op (f e1) (f e2)
-      Lambda p e
-          | Set.member old (Pattern.boundVars p) -> expr
-          | otherwise -> Lambda p (f e)
-      App e1 e2 -> App (f e1) (f e2)
-      MultiIf ps -> MultiIf (map (f *** f) ps)
+    case expression of
+      Range e1 e2 ->
+          Range (f e1) (f e2)
+
+      ExplicitList exprs ->
+          ExplicitList (map f exprs)
+
+      Binop op e1 e2 ->
+          Binop op (f e1) (f e2)
+
+      Lambda pattern body
+          | Set.member old (Pattern.boundVars pattern) -> expression
+          | otherwise -> Lambda pattern (f body)
+
+      App e1 e2 ->
+          App (f e1) (f e2)
+
+      MultiIf branches ->
+          MultiIf (map (f *** f) branches)
 
       Let defs body
-          | anyShadow -> expr
+          | anyShadow -> expression
           | otherwise -> Let (map substDef defs) (f body)
         where
-          substDef (Canonical.Definition p e t) = Canonical.Definition p (f e) t
+          substDef (Canonical.Definition p e t) =
+              Canonical.Definition p (f e) t
+
           anyShadow =
-              any (Set.member old . Pattern.boundVars) [ p | Canonical.Definition p _ _ <- defs ]
+              any (Set.member old . Pattern.boundVars)
+                  [ pattern | Canonical.Definition pattern _ _ <- defs ]
 
       Var (V.Canonical home x) ->
           case home of
-            V.Module _ -> expr
-            V.BuiltIn -> expr
-            V.Local -> if x == old then new else expr
+            V.Module _ -> expression
+            V.BuiltIn -> expression
+            V.Local -> if x == old then new else expression
 
-      Case e cases -> Case (f e) $ map (second f) cases
-      Data name es -> Data name (map f es)
-      Access e x -> Access (f e) x
-      Remove e x -> Remove (f e) x
-      Insert e x v -> Insert (f e) x (f v)
-      Modify r fs -> Modify (f r) (map (second f) fs)
-      Record fs -> Record (map (second f) fs)
-      Literal _ -> expr
-      Markdown uid md es -> Markdown uid md (map f es)
-      GLShader _ _ _ -> expr
-      PortIn name st -> PortIn name st
-      PortOut name st signal -> PortOut name st (f signal)
+      Case e cases ->
+          Case (f e) $ map (second f) cases
+
+      Data tag values ->
+          Data tag (map f values)
+
+      Access record field ->
+          Access (f record) field
+
+      Remove record field ->
+          Remove (f record) field
+
+      Insert record field value ->
+          Insert (f record) field (f value)
+
+      Modify record fields ->
+          Modify (f record) (map (second f) fields)
+
+      Record fields ->
+          Record (map (second f) fields)
+
+      Literal _ -> expression
+
+      Markdown uid md exprs ->
+          Markdown uid md (map f exprs)
+
+      GLShader _ _ _ -> expression
+
+      PortIn name st ->
+          PortIn name st
+
+      PortOut name st signal ->
+          PortOut name st (f signal)
