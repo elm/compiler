ff7917f55b7cf1bbc67a219a38d2e936a0b529c1
Evan Czaplicki <info@elm-lang.org>
2014-10-14 17:41:52 -0700

    update Paths_Elm to Paths_elm_compiler

--- compiler/Elm/Compiler/Version.hs
+++ compiler/Elm/Compiler/Version.hs
@@ -2,7 +2,7 @@
 module Elm.Compiler.Version (version) where
 
 import qualified Data.Version as Version
-import qualified Paths_Elm as This
+import qualified Paths_elm_compiler as This
 
 
 version :: String
