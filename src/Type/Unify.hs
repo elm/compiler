e4ce75997fa0990e72207be4efa73803d829fbca
Evan Czaplicki <info@elm-lang.org>
2014-11-19 22:16:22 -0800

    Improve the missing field error message a bit
    Ideal world, we’d have a string distance algorithm, so we could
    actually suggest fields that are close to the same name.

--- src/Type/Unify.hs
+++ src/Type/Unify.hs
@@ -305,7 +305,9 @@ fieldMismatchError :: Map.Map String a -> String
 fieldMismatchError missingFields =
     case Map.keys missingFields of
       [] -> ""
-      [key] -> "Looks like one record is missing field " ++ key
+      [key] ->
+        "Looks like a record is missing the field '" ++ key ++ "'.\n" ++
+        "Maybe there is a misspelling in a record access or record update?"
       keys ->
         "Looks like one record is missing fields "
         ++ List.intercalate ", " (init keys) ++ ", and " ++ last keys
