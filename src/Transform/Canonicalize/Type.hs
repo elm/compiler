b651262b6517135d452113862897e344d92bfbd3
Evan Czaplicki <info@elm-lang.org>
2014-11-23 21:34:27 -0600

    style: conform to modern style guide

--- src/Transform/Canonicalize/Type.hs
+++ src/Transform/Canonicalize/Type.hs
@@ -13,43 +13,68 @@ import qualified AST.Variable as Var
 import Transform.Canonicalize.Environment
 import qualified Transform.Canonicalize.Variable as Canonicalize
 
-tipe :: Environment -> T.RawType -> Canonicalizer String T.CanonicalType
+
+tipe
+    :: Environment
+    -> T.RawType
+    -> Canonicalizer String T.CanonicalType
 tipe env typ =
     let go = tipe env in
     case typ of
-      T.Var x    -> return (T.Var x)
-      T.Type _   -> canonicalizeApp env typ []
-      T.App t ts -> canonicalizeApp env t ts
+      T.Var x ->
+          return (T.Var x)
 
-      T.Lambda a b     -> T.Lambda <$> go a <*> go b
-      T.Aliased name t -> T.Aliased name <$> go t
+      T.Type _ ->
+          canonicalizeApp env typ []
+
+      T.App t ts ->
+          canonicalizeApp env t ts
+
+      T.Lambda a b ->
+          T.Lambda <$> go a <*> go b
+
+      T.Aliased name t ->
+          T.Aliased name <$> go t
 
       T.Record fields ext ->
           let go' (f,t) = (,) f <$> go t
-          in  T.Record <$> mapM go' fields <*> traverse go ext
+          in
+              T.Record <$> mapM go' fields <*> traverse go ext
 
-canonicalizeApp :: Environment -> T.RawType -> [T.RawType]
-                -> Canonicalizer String T.CanonicalType
+
+canonicalizeApp
+    :: Environment
+    -> T.RawType
+    -> [T.RawType]
+    -> Canonicalizer String T.CanonicalType
 canonicalizeApp env f args =
   case f of
     T.Type (Var.Raw rawName) ->
-        do answer <- Canonicalize.tvar env rawName
-           case answer of
-             Right alias -> canonicalizeAlias env alias args
-             Left name -> case args of
-                            []  -> return (T.Type name)
-                            _:_ -> T.App (T.Type name) <$> mapM (tipe env) args
+      do  answer <- Canonicalize.tvar env rawName
+          case answer of
+            Right alias ->
+                canonicalizeAlias env alias args
 
-    _ -> T.App <$> tipe env f <*> mapM (tipe env) args
+            Left name ->
+                case args of
+                  [] -> return (T.Type name)
+                  _ ->
+                    T.App (T.Type name) <$> mapM (tipe env) args
 
-canonicalizeAlias :: Environment -> (Var.Canonical, [String], T.CanonicalType)
-                  -> [T.RawType]
-                  -> Canonicalizer String T.CanonicalType
+    _ ->
+      T.App <$> tipe env f <*> mapM (tipe env) args
+
+
+canonicalizeAlias
+    :: Environment
+    -> (Var.Canonical, [String], T.CanonicalType)
+    -> [T.RawType]
+    -> Canonicalizer String T.CanonicalType
 canonicalizeAlias env (name, tvars, dealiasedTipe) tipes =
-  do when (tipesLen /= tvarsLen) (throwError msg)
-     tipes' <- mapM (tipe env) tipes
-     let tipe' = replace (Map.fromList (zip tvars tipes')) dealiasedTipe
-     return $ T.Aliased name tipe'
+  do  when (tipesLen /= tvarsLen) (throwError msg)
+      tipes' <- mapM (tipe env) tipes
+      let tipe' = replace (Map.fromList (zip tvars tipes')) dealiasedTipe
+      return $ T.Aliased name tipe'
   where
     tipesLen = length tipes
     tvarsLen = length tvars
