a3f5817f6e70db06195958b7193be5dcda3cf37e
Evan Czaplicki <info@elm-lang.org>
2014-11-28 21:39:12 -0800

    Get tests working again

--- tests/compiler/Tests/Compiler.hs
+++ tests/compiler/Tests/Compiler.hs
@@ -1,62 +1,73 @@
 {-# OPTIONS_GHC -W #-}
 module Tests.Compiler (compilerTests) where
 
+import qualified Data.Map as Map
 import Data.Traversable (traverse)
 
-import System.Exit
 import System.FilePath ((</>))
 import System.FilePath.Find (find, (==?), extension)
-import System.Process (readProcessWithExitCode)
 
 import Test.Framework
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit (Assertion, assertFailure, assertBool)
+
+import qualified Elm.Compiler as Compiler
+import qualified Elm.Compiler.Module as Module
+
 
 compilerTests :: Test
 compilerTests =
   buildTest $ do
     goods <- testIf isSuccess =<< getElms "good"
     bads  <- testIf isFailure =<< getElms "bad"
-    return $ testGroup "Compile Tests"
-             [ testGroup "Good Tests" goods
-             , testGroup "Bad Tests"  bads
-             ]
+    return $
+        testGroup "Compile Tests"
+          [ testGroup "Good Tests" goods
+          , testGroup "Bad Tests"  bads
+          ]
 
--- COLLECT FILES
+
+-- GATHER ELM FILES
 
 getElms :: FilePath -> IO [FilePath]
 getElms filePath =
-    find (return True) (extension ==? ".elm") (testsDir </> filePath)
+    find
+      (return True)
+      (extension ==? ".elm")
+      (testsDir </> filePath)
+
 
 testsDir :: FilePath
-testsDir = "tests" </> "compiler" </> "test-files"
+testsDir =
+    "tests" </> "compiler" </> "test-files"
 
 
 -- RUN COMPILER
 
-testIf :: (ExitCode -> String -> Assertion) -> [FilePath] -> IO [Test]
-testIf handler filePaths =
+testIf
+    :: (Either String (Module.Interface, String) -> Assertion)
+    -> [FilePath]
+    -> IO [Test]
+testIf handleResult filePaths =
     traverse setupTest filePaths
   where
-    setupTest filePath = do
-      let args = [ "--make", "--src-dir=" ++ testsDir, filePath ]
-      (exitCode, stdout, stderr) <- readProcessWithExitCode compiler args ""
-      return (testCase filePath (handler exitCode (stdout ++ stderr)))
-
-compiler :: FilePath
-compiler = "dist" </> "build" </> "elm" </> "elm"
+    setupTest filePath =
+      do  source <- readFile filePath
+          let result = Compiler.compile "elm-lang" "core" source Map.empty
+          return (testCase filePath (handleResult result))
 
 
 -- CHECK RESULTS
 
-isSuccess :: ExitCode -> String -> Assertion
-isSuccess exitCode stderr =
-    case exitCode of
-      ExitSuccess -> assertBool "" True
-      ExitFailure _ -> assertFailure stderr
+isSuccess :: Either String a -> Assertion
+isSuccess result =
+    case result of
+      Right _ -> assertBool "" True
+      Left msg -> assertFailure msg
 
-isFailure :: ExitCode -> String -> Assertion
-isFailure exitCode _stderr =
-    case exitCode of
-      ExitSuccess -> assertFailure "Compilation succeeded but should have failed"
-      ExitFailure _ -> assertBool "" True
+
+isFailure :: Either a b -> Assertion
+isFailure result =
+    case result of
+      Left _ -> assertFailure "Compilation succeeded but should have failed"
+      Right _ -> assertBool "" True
