f5366947d61acb97595eee77c5ebb55c5f68fd90
Evan Czaplicki <info@elm-lang.org>
2015-03-07 16:55:03 -0600

    Improve indentation in Transform.Definition

--- src/Transform/Definition.hs
+++ src/Transform/Definition.hs
@@ -8,28 +8,33 @@ import qualified AST.Expression.Valid as Valid
 import qualified Transform.Expression as Expr
 
 combineAnnotations :: [Source.Def] -> Either String [Valid.Def]
-combineAnnotations = go
-    where
-      msg x = "Syntax Error: The type annotation for '" ++ x ++
-              "' must be directly above its definition."
+combineAnnotations =
+    go
+  where
+    errorMessage x =
+        "Syntax Error: The type annotation for '" ++ x ++
+        "' must be directly above its definition."
 
-      exprCombineAnnotations = Expr.crawlLet combineAnnotations
+    exprCombineAnnotations =
+        Expr.crawlLet combineAnnotations
 
-      go defs =
-          case defs of
-            Source.TypeAnnotation name tipe : rest ->
-                case rest of
-                  Source.Definition pat@(P.Var name') expr : rest'
-                      | name == name' ->
-                          do expr' <- exprCombineAnnotations expr
-                             let def = Valid.Definition pat expr' (Just tipe)
-                             (:) def <$> go rest'
+    go defs =
+        case defs of
+          Source.TypeAnnotation name tipe : rest ->
+              case rest of
+                Source.Definition pat@(P.Var name') expr : rest'
+                    | name == name' ->
+                        do  expr' <- exprCombineAnnotations expr
+                            let def = Valid.Definition pat expr' (Just tipe)
+                            (:) def <$> go rest'
 
-                  _ -> Left (msg name)
+                _ ->
+                    Left (errorMessage name)
 
-            Source.Definition pat expr : rest ->
-                do expr' <- exprCombineAnnotations expr
-                   let def = Valid.Definition pat expr' Nothing
-                   (:) def <$> go rest
+          Source.Definition pat expr : rest ->
+              do  expr' <- exprCombineAnnotations expr
+                  let def = Valid.Definition pat expr' Nothing
+                  (:) def <$> go rest
 
-            [] -> return []
+          [] ->
+              return []
