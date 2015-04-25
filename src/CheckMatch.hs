d583bfe6ad651bf4d5b7d6c0ba7c553c5d13488a
Izaak Meckler <ihmeckler@gmail.com>
2015-04-25 13:15:39 -0500

    changed it back since the types were wrong

--- src/CheckMatch.hs
+++ src/CheckMatch.hs
@@ -51,7 +51,7 @@ fromPattern p = case p of
 data ErrorType = ConstructorNotFound Var.Canonical
 
 checkModule :: [CanonicalAdt] -> Module exports CanonicalBody -> [(Either ErrorType MatchResult, Annotation.Region)]
-checkModule (Module {body, datatypes}) = checkBody body where
+checkModule adts (Module {body}) = checkBody body where
     checkBody :: CanonicalBody -> [(Either ErrorType MatchResult, Annotation.Region)]
     checkBody  (CanonicalBody {program}) = checkExpr program
 
@@ -86,7 +86,7 @@ checkModule (Module {body, datatypes}) = checkBody body where
         m =
             Map.fromList $ concatMap (\adt@(_adtName, (_tyVars, ctors)) ->
                 map (\(ctor,_args) -> (ctor, adt)) ctors)
-                datatypes
+                adts
 
     ctorToAdtExn :: Var.Canonical -> Either ErrorType CanonicalAdt
     ctorToAdtExn c = case ctorToAdt c of
