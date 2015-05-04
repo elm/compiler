504b04ca54eda6e06ff4c55fa7bfc26c20a70ee0
Izaak Meckler <ihmeckler@gmail.com>
2015-05-04 18:17:50 -0500

    more style

--- src/CheckMatch.hs
+++ src/CheckMatch.hs
@@ -26,89 +26,96 @@ import qualified AST.Variable as Var
 import qualified Reporting.Error.CheckMatch as Error
 import Reporting.Warning.CheckMatch
 
-type M = Either (Annotation.Located Error.Error)
+type M =
+    Either (Annotation.Located Error.Error)
 
 
 fromPattern :: CanonicalPattern -> CanonicalPat
-fromPattern (Annotation.A _region p) = case p of
-    Pattern.Data ctor ps ->
-        Data ctor (map fromPattern ps)
+fromPattern (Annotation.A _region p) =
+    case p of
+      Pattern.Data ctor ps ->
+          Data ctor (map fromPattern ps)
 
-    Pattern.Record fs ->
-        Record fs
+      Pattern.Record fs ->
+          Record fs
 
-    Pattern.Alias s p ->
-        Alias s (fromPattern p)
+      Pattern.Alias s p ->
+          Alias s (fromPattern p)
 
-    Pattern.Var s ->
-        Var s
+      Pattern.Var s ->
+          Var s
 
-    Pattern.Anything ->
-        Anything
+      Pattern.Anything ->
+          Anything
 
-    Pattern.Literal l ->
-        Literal l
+      Pattern.Literal l ->
+          Literal l
 
 
 checkBody
   :: Module.Interfaces
   -> Module.CanonicalBody
   -> M [Annotation.Located Warning]
-checkBody interfaces = checkExpr . Module.program where
+checkBody interfaces =
+    checkExpr . Module.program
+    where
     checkExpr :: Canonical.Expr -> M [Annotation.Located Warning]
-    checkExpr (Annotation.A r e) = case e of
-        General.ExplicitList es  ->
-          concatMapM checkExpr es
-
-        General.Binop _ e e' ->
-            (++) <$> checkExpr e <*> checkExpr e'
-
-        General.Lambda p e ->
-            maybeCons <$> checkMatch r [p] <*> checkExpr e
-
-        General.App e e' ->
-            (++) <$> checkExpr e <*> checkExpr e'
-
-        General.MultiIf branches ->
-            concatMapM (\(b,e) -> (++) <$> checkExpr b <*> checkExpr e) branches
-
-        General.Let defs e ->
-            concat <$> sequence [ps, rhssResults, checkExpr e]
-            where
-            ps          = mapMaybeM (\(Canonical.Definition p _ _) -> checkMatch r [p]) defs
-            rhssResults = concatMapM (\(Canonical.Definition _ e _) -> checkExpr e) defs
-
-        General.Case e branches ->
-            do  eResults      <- checkExpr e
-                result        <- checkMatch r ps
-                branchResults <- concatMapM (checkExpr . snd) branches
-                return (eResults ++ maybeCons result branchResults)
-                where ps = map fst branches
-
-        General.Data _ctor es ->
+    checkExpr (Annotation.A r e) =
+        case e of
+          General.ExplicitList es  ->
             concatMapM checkExpr es
 
-        General.Access e _f ->
-            checkExpr e
+          General.Binop _ e e' ->
+              (++) <$> checkExpr e <*> checkExpr e'
 
-        General.Remove e _f ->
-            checkExpr e
+          General.Lambda p e ->
+              maybeCons <$> checkMatch r [p] <*> checkExpr e
 
-        General.Insert e _f e' ->
-            (++) <$> checkExpr e <*> checkExpr e'
+          General.App e e' ->
+              (++) <$> checkExpr e <*> checkExpr e'
 
-        General.Modify e _ ->
-            checkExpr e
+          General.MultiIf branches ->
+              concatMapM (\(b,e) -> (++) <$> checkExpr b <*> checkExpr e) branches
 
-        General.Record fields ->
-            concatMapM (checkExpr . snd) fields
+          General.Let defs e ->
+              concat <$> sequence [ps, rhssResults, checkExpr e]
+              where
+              ps          = mapMaybeM (\(Canonical.Definition p _ _) -> checkMatch r [p]) defs
+              rhssResults = concatMapM (\(Canonical.Definition _ e _) -> checkExpr e) defs
 
-        _ ->
-            return []
+          General.Case e branches ->
+              do  eResults      <- checkExpr e
+                  result        <- checkMatch r ps
+                  branchResults <- concatMapM (checkExpr . snd) branches
+                  return (eResults ++ maybeCons result branchResults)
+                  where ps = map fst branches
+
+          General.Data _ctor es ->
+              concatMapM checkExpr es
+
+          General.Access e _f ->
+              checkExpr e
+
+          General.Remove e _f ->
+              checkExpr e
+
+          General.Insert e _f e' ->
+              (++) <$> checkExpr e <*> checkExpr e'
+
+          General.Modify e _ ->
+              checkExpr e
+
+          General.Record fields ->
+              concatMapM (checkExpr . snd) fields
+
+          _ ->
+              return []
 
 
     ctorToAdt :: Var.Canonical -> Maybe (Module.AdtInfo String)
-    ctorToAdt = \(Var.Canonical {Var.home,Var.name}) -> Map.lookup name =<< Map.lookup home m where
+    ctorToAdt =
+        \(Var.Canonical {Var.home,Var.name}) -> Map.lookup name =<< Map.lookup home m
+        where
         -- Maps home to ctor name to the adt to which that ctor belongs
         m :: Map.Map Var.Home (Map.Map String (Module.AdtInfo String))
         m = builtins `Map.union` fromModules where
@@ -119,46 +126,59 @@ checkBody interfaces = checkExpr . Module.program where
 
 
     ctorToAdtErr :: Region.Region -> Var.Canonical -> M (Module.AdtInfo String)
-    ctorToAdtErr region c = case ctorToAdt c of
-        Nothing ->
-          throwError (Annotation.A region (Error.ConstructorTypeNotFound c))
+    ctorToAdtErr region c =
+        case ctorToAdt c of
+          Nothing ->
+              throwError (Annotation.A region (Error.ConstructorTypeNotFound c))
 
-        Just adt ->
-          return adt
+          Just adt ->
+              return adt
 
 
     complement :: Region.Region -> CanonicalPat -> M [CanonicalPat]
-    complement region p = case p of
-        Data ctor ps ->
-            do  let ctorName = Var.name ctor
-                    ctorHome = Var.home ctor
-                (_tyVars, ctors) <- ctorToAdtErr region ctor
-                -- A pattern is in the complement of this one if it uses one of the
-                -- other constructors of this type...
-                let otherCtorPat (ctor', args) =
-                        Data (Var.Canonical {Var.name = ctor', Var.home = ctorHome})
-                          (replicate (length args) Anything)
+    complement region p =
+        case p of
+          Data ctor ps ->
+              do  let ctorName = Var.name ctor
+                      ctorHome = Var.home ctor
+                  (_tyVars, ctors) <- ctorToAdtErr region ctor
+                  -- A pattern is in the complement of this one if it uses one of the
+                  -- other constructors of this type...
+                  let otherCtorPat (ctor', args) =
+                          Data (Var.Canonical {Var.name = ctor', Var.home = ctorHome})
+                            (replicate (length args) Anything)
 
-                    otherCtorPatterns =
-                        filter (\(ctor', _) -> ctor' /= ctorName) ctors
-                        |> map otherCtorPat
+                      otherCtorPatterns =
+                          filter (\(ctor', _) -> ctor' /= ctorName) ctors
+                          |> map otherCtorPat
 
-                    -- or if it uses this constructor and one of its argument patterns is
-                    -- in the complement of an argument pattern here.
-                    numArgs        = length ps
-                    sandwich pat i = replicate i Anything ++ pat : replicate (numArgs - i - 1) Anything
+                      -- or if it uses this constructor and one of its argument patterns is
+                      -- in the complement of an argument pattern here.
+                      numArgs        = length ps
+                      sandwich pat i = replicate i Anything ++ pat : replicate (numArgs - i - 1) Anything
 
-                argComplement <- fmap concat . forM (zip [0..] ps) $ \(i, argP) ->
-                    map (\argP' -> Data ctor (sandwich argP' i)) <$> (complement region argP)
+                  argComplement <- fmap concat . forM (zip [0..] ps) $ \(i, argP) ->
+                      map (\argP' -> Data ctor (sandwich argP' i)) <$> (complement region argP)
 
-                return (otherCtorPatterns ++ argComplement)
+                  return (otherCtorPatterns ++ argComplement)
 
-        Record _fs     -> return []
-        Alias _s p     -> complement region p
-        Var _s         -> return []
-        Anything       -> return []
-        Literal l      -> return [AnythingBut (Set.singleton l)]
-        AnythingBut ls -> return (map Literal (Set.toList ls))
+          Record _fs ->
+              return []
+
+          Alias _s p ->
+              complement region p
+
+          Var _s ->
+              return []
+
+          Anything ->
+              return []
+
+          Literal l ->
+              return [AnythingBut (Set.singleton l)]
+
+          AnythingBut ls ->
+              return (map Literal (Set.toList ls))
 
     intersection :: CanonicalPat -> CanonicalPat -> Maybe CanonicalPat
     intersection p1 p2 =
@@ -205,7 +225,9 @@ checkBody interfaces = checkExpr . Module.program where
           _ -> Nothing
 
     checkMatch :: Region.Region -> [CanonicalPattern] -> M (Maybe (Annotation.Located Warning))
-    checkMatch region = go [Anything] where
+    checkMatch region =
+        go [Anything]
+        where
         go :: [CanonicalPat] -> [CanonicalPattern] -> M (Maybe (Annotation.Located Warning))
         go unhandled ps =
             case (unhandled, ps) of
@@ -227,13 +249,16 @@ checkBody interfaces = checkExpr . Module.program where
 
 -- Util
 concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
-concatMapM f = liftM concat . mapM f
+concatMapM f =
+    liftM concat . mapM f
 
 
 mapMaybeM :: Monad m => (a -> m (Maybe b)) -> [a] -> m [b]
-mapMaybeM f = liftM catMaybes . mapM f
+mapMaybeM f =
+    liftM catMaybes . mapM f
 
 
 maybeCons :: Maybe a -> [a] -> [a]
-maybeCons = maybe id (:)
+maybeCons =
+    maybe id (:)
 
