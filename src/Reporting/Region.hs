9b2fc656a0c774bb1c7dd0c61d781f506f5e80f5
Evan Czaplicki <info@elm-lang.org>
2015-05-05 11:15:48 -0700

    Extract region from source code with line numbers

--- src/Reporting/Region.hs
+++ src/Reporting/Region.hs
@@ -5,6 +5,8 @@ import Data.Aeson ((.=))
 import qualified Data.Aeson as Json
 import qualified Text.Parsec.Pos as Parsec
 
+import Elm.Utils ((|>))
+
 
 data Region = Region
     { start :: Position
@@ -53,6 +55,62 @@ toPrettyString (Region start end) =
     True ->
         "on line " ++ show (line end) ++ ", column "
         ++ show (column start) ++ " to " ++ show (column end)
+
+
+-- EXTRACT REGION from SOURCE with LINE NUMBERS
+
+select :: Region -> String -> String
+select (Region start end) source =
+  let
+    relevantLines =
+        lines source
+          |> drop (line start)
+          |> take (line end - line start)
+  in
+  case relevantLines of
+    [] ->
+        error "something has gone badly wrong with reporting source locations"
+
+    [theLine] ->
+        let
+          lineNumber = show (line start) ++ "|"
+        in
+            lineNumber
+            ++ theLine
+            ++ "\n"
+            ++ replicate (column start + length lineNumber) ' '
+            ++ replicate (column end - column start) '^'
+            ++ "\n"
+
+    firstLine : rest ->
+        let
+          filteredFirstLine =
+              replicate (column start) ' ' ++ drop (column start) firstLine
+
+          filteredLastLine =
+              take (column end) (last rest)
+
+          focusedRelevantLines =
+              filteredFirstLine : init rest ++ [filteredLastLine]
+
+          lineNumbersWidth =
+              length (show (line end))
+
+          numberedLines =
+              zipWith
+                (addLineNumber lineNumbersWidth)
+                [line start .. line end]
+                focusedRelevantLines
+        in
+            unlines numberedLines
+
+
+addLineNumber :: Int -> Int -> String -> String
+addLineNumber width n line =
+  let
+    number = show n
+  in
+    replicate (width - length number) ' ' ++ number ++ "|" ++ line
 
 
 -- JSON
