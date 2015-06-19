a5d8eeca101c68687ab0559de467f6987914fe30
Evan Czaplicki <info@elm-lang.org>
2015-06-19 11:21:39 +0200

    For union type docs, filter out tags which are not exported

--- src/Docs/Check.hs
+++ src/Docs/Check.hs
@@ -1,6 +1,7 @@
 module Docs.Check (check) where
 
 import Control.Applicative ((<$>),(<*>))
+import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Data.Traversable as T
@@ -20,9 +21,12 @@ import qualified Reporting.Region as R
 import qualified Reporting.Result as R
 
 
+type Result w a = R.Result w Error.Error a
+
+
 -- CHECK DOCUMENTATION
 
-check :: [Var.Value] -> A.Located (Maybe Docs.Centralized) -> R.Result w Error.Error Docs.Checked
+check :: [Var.Value] -> A.Located (Maybe Docs.Centralized) -> Result w Docs.Checked
 check exports (A.A region maybeDocs) =
   case maybeDocs of
     Nothing ->
@@ -32,7 +36,7 @@ check exports (A.A region maybeDocs) =
         checkHelp region exports docs
 
 
-checkHelp :: R.Region -> [Var.Value] -> Docs.Centralized -> R.Result w Error.Error Docs.Checked
+checkHelp :: R.Region -> [Var.Value] -> Docs.Centralized -> Result w Docs.Checked
 checkHelp region exports (Docs.Docs comment aliases types values) =
   let
     docNames =
@@ -44,24 +48,18 @@ checkHelp region exports (Docs.Docs comment aliases types values) =
         |> Map.mapWithKey checkValue
         |> T.traverse id
 
-    checkValue name value =
-      const
-        <$> hasType name value
-        <*> hasComment Docs.valueComment name value
+    exportedUnions =
+      map (\(tipe, Var.Listing tags _) -> (tipe, tags)) (Var.getUnions exports)
   in
     (\() -> Docs.Docs comment)
-      <$> checkComment region exports docNames
-      <*> checkCategory (hasComment Docs.aliasComment) aliases
-      <*> checkCategory (hasComment Docs.unionComment) types
+      <$> checkModuleComment region exports docNames
+      <*> checkCategory (checkComment Docs.aliasComment) aliases
+      <*> checkCategory (checkUnion exportedUnions) types
       <*> checkCategory checkValue values
 
 
-hasComment
-    :: (a -> Maybe String)
-    -> String
-    -> A.Located a
-    -> R.Result w Error.Error (A.Located a)
-hasComment getComment name (A.A region value) =
+checkComment :: (a -> Maybe String) -> String -> A.Located a -> Result w (A.Located a)
+checkComment getComment name (A.A region value) =
   case getComment value of
     Nothing ->
         R.throw region (Error.NoComment name)
@@ -70,10 +68,35 @@ hasComment getComment name (A.A region value) =
         R.ok (A.A region value)
 
 
-hasType
-    :: String
-    -> A.Located (Docs.Value (Maybe Type.Type))
-    -> R.Result w Error.Error (A.Located (Docs.Value Type.Type))
+checkUnion :: [(String, [String])] -> String -> A.Located Docs.Union -> Result w (A.Located Docs.Union)
+checkUnion exportedUnions name value =
+  const
+    <$> filterUnionTags exportedUnions name value
+    <*> checkComment Docs.unionComment name value
+
+
+filterUnionTags :: [(String, [String])] -> String -> A.Located Docs.Union -> Result w (A.Located Docs.Union)
+filterUnionTags exportedUnions name (A.A region union@(Docs.Union _ _ ctors)) =
+  case List.lookup name exportedUnions of
+    Nothing ->
+      R.throw region (Error.OnlyInExports name)
+
+    Just tags ->
+      R.ok $ A.A region $ union
+        { Docs.unionCases =
+            filter (\(tag, _) -> elem tag tags) ctors
+        }
+
+
+
+checkValue :: String -> A.Located (Docs.Value (Maybe Type.Type)) -> Result w (A.Located (Docs.Value Type.Type))
+checkValue name value =
+  const
+    <$> hasType name value
+    <*> checkComment Docs.valueComment name value
+
+
+hasType :: String -> A.Located (Docs.Value (Maybe Type.Type)) -> Result w (A.Located (Docs.Value Type.Type))
 hasType name (A.A region value) =
   case Docs.valueType value of
     Just tipe ->
@@ -85,8 +108,8 @@ hasType name (A.A region value) =
 
 -- CHECK MODULE COMMENT
 
-checkComment :: R.Region -> [Var.Value] -> Set.Set String -> R.Result w Error.Error ()
-checkComment region exports docNames =
+checkModuleComment :: R.Region -> [Var.Value] -> Set.Set String -> Result w ()
+checkModuleComment region exports docNames =
   let
     exportNames =
       Set.fromList (map valueName exports)
