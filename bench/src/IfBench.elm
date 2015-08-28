a9658ccb44cf3358991671b7abe02c4d87a31151
Evan Czaplicki <info@elm-lang.org>
2015-08-27 20:27:59 -0700

    Get rid of multi-way if syntax

--- bench/src/IfBench.elm
+++ bench/src/IfBench.elm
@@ -1,81 +1,104 @@
 module IfBench where
 
+
 tailRecursiveIfFn : Int -> Float
-tailRecursiveIfFn i = 
-  if 
-    | i < 0 ->
+tailRecursiveIfFn i =
+  if i < 0 then
       10.0
-    | i == 0 ->
+
+  else if i == 0 then
       20.0
-    | i > 0 && i < 10 ->
+
+  else if i > 0 && i < 10 then
       30.0
-    | otherwise ->
-      tailRecursiveIfFn (i - (1 + (i % 5 ) ) )
-    
-    
+
+  else
+      tailRecursiveIfFn (i - (1 + (i % 5)))
+
+
 manyConditionFunction i =
-  if
-    | i <= 0 ->
+  if i <= 0 then
       30.0 + (toFloat i)
-    | i > 100 && (i * i) % 20 == 7 ->
+
+  else if i > 100 && (i * i) % 20 == 7 then
       24.3 + (toFloat i)
-    | i > 100 && (i % 3 == 1) ->
+
+  else if i > 100 && (i % 3 == 1) then
       3.33 + (toFloat i)
-    | (i == 152) ->
+
+  else if i == 152 then
       29.0 + (toFloat i)
-    | (i + (i*i) > 200 && (i + (i*i) < 300)) -> 
+
+  else if i + (i*i) > 200 && (i + (i*i) < 300) then
       32.9 + (toFloat i)
-    | i == 99 ->
+
+  else if i == 99 then
       93.0 + (toFloat i)
-    | (i*i % 17 == 13 ) ->
+
+  else if i*i % 17 == 13 then
       9.0 + (toFloat i)
-    | (i == 101) ->
+
+  else if i == 101 then
       39.0 + (toFloat i)
-    | (i == 102) ->
+
+  else if i == 102 then
       38.0 + (toFloat i)
-    | (i == 103) ->
+
+  else if i == 103 then
       49.0 + (toFloat i)
-    | (i == 104) ->
+
+  else if i == 104 then
       59.0 + (toFloat i)
-    | (i == 105) ->
+
+  else if i == 105 then
       69.0 + (toFloat i)
-    | (i == 106) ->
+
+  else if i == 106 then
       39.0 + (toFloat i)
-    | (i == 107) ->
+
+  else if i == 107 then
       38.0 + (toFloat i)
-    | (i == 108) ->
+
+  else if i == 108 then
       49.0 + (toFloat i)
-    | (i == 109) ->
+
+  else if i == 109 then
       59.0 + (toFloat i)
-    | (i == 110) ->
+
+  else if i == 110 then
       69.0 + (toFloat i)
-    | (i == 111) ->
+
+  else if i == 111 then
       39.0 + (toFloat i)
-    | (i == 115) ->
+
+  else if i == 115 then
       69.0 + (toFloat i)
-    | otherwise ->
+
+  else if otherwise then
       toFloat i
-      
-      
+
+
 fewConditionFunction i =
-  if
-    | i <= 0 ->
+  if i <= 0 then
       30.0 + (toFloat i)
-    | (i == 104) ->
+
+  else if i == 104 then
       59.0 + (toFloat i)
-    | (i == 105) ->
+
+  else if i == 105 then
       69.0 + (toFloat i)
-    | otherwise ->
+
+  else
       toFloat i
-  
-  
-tailRecSum intList = 
-  List.foldr (\i sumSoFar -> sumSoFar + tailRecursiveIfFn i ) 0.0 intList  
-      
-manyConditionalSum intList = 
+
+
+tailRecSum intList =
+  List.foldr (\i sumSoFar -> sumSoFar + tailRecursiveIfFn i ) 0.0 intList
+
+
+manyConditionalSum intList =
   List.foldr (\i sumSoFar -> sumSoFar + manyConditionFunction i ) 0.0 intList
-    
-    
-fewConditionalSum intList = 
+
+
+fewConditionalSum intList =
   List.foldr (\i sumSoFar -> sumSoFar + fewConditionFunction i ) 0.0 intList
-    
