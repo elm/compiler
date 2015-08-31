bb7eb96a1953b7dcbead353ef0b30c53aa2b9694
Laszlo Pandy <laszlo.pandy@prezi.com>
2015-08-31 13:31:58 +0200

    Compiler tests compare current output (with nothing yet).

--- tests/Test/Compiler.hs
+++ tests/Test/Compiler.hs
@@ -1,15 +1,18 @@
 {-# OPTIONS_GHC -W #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Test.Compiler (compilerTests) where
+
+import Control.Exception (try, IOException)
 
 import qualified Data.Map as Map
 import Data.Traversable (traverse)
 
-import System.FilePath ((</>))
+import System.FilePath ((</>), joinPath, splitDirectories)
 import System.FilePath.Find (find, (==?), extension)
 
 import Test.Framework
 import Test.Framework.Providers.HUnit (testCase)
-import Test.HUnit (Assertion, assertFailure, assertBool)
+import Test.HUnit (Assertion, assertFailure, assertBool, assertEqual)
 
 import qualified Elm.Compiler as Compiler
 import qualified Elm.Package as Package
@@ -19,10 +22,12 @@ compilerTests =
   buildTest $ do
     goods <- testIf isSuccess =<< getElms "good"
     bads  <- testIf isFailure =<< getElms "bad"
+    expectedJsTests <- testMatchesExpected "good" "good-expected-js"
     return $
         testGroup "Compile Tests"
           [ testGroup "Good Tests" goods
           , testGroup "Bad Tests"  bads
+          , testGroup "Expected JS Tests" expectedJsTests
           ]
 
 
@@ -43,6 +48,18 @@ testsDir =
 
 -- RUN COMPILER
 
+compileString :: FilePath -> String -> Either String Compiler.Result
+compileString filePath source =
+  let context =
+        Compiler.Context Package.coreName True False []
+      (dealiaser, _warnings, result) =
+        Compiler.compile context source Map.empty
+      formatErrors errors =
+        concatMap (Compiler.errorToString dealiaser filePath source) errors
+  in
+      either (Left . formatErrors) Right result
+
+
 testIf
     :: (Either String Compiler.Result -> Assertion)
     -> [FilePath]
@@ -52,17 +69,32 @@ testIf handleResult filePaths =
   where
     setupTest filePath = do
       source <- readFile filePath
-
-      let context =
-            Compiler.Context Package.coreName True False []
-      let (dealiaser, _warnings, result) =
-            Compiler.compile context source Map.empty
-      let formatErrors errors =
-            concatMap (Compiler.errorToString dealiaser filePath source) errors
-      let formattedResult =
-            either (Left . formatErrors) Right result
+      let formattedResult = compileString filePath source
 
       return $ testCase filePath (handleResult formattedResult)
+
+
+testMatchesExpected :: String -> String -> IO [Test]
+testMatchesExpected elmDir expectedJsDir =
+    traverse doTest =<< getElms elmDir
+  where
+    readFileOrErrorStr filePath = do
+      strOrExc <- try $ readFile filePath
+      case strOrExc of
+        Right s -> return s
+        Left (e :: IOException) -> return (show e)
+
+    doTest filePath = do
+      source <- readFile filePath
+
+      let expectedFilePath =
+            testsDir </> expectedJsDir </> joinPath (drop 3 (splitDirectories filePath)) ++ ".js"
+      expectedJs <- readFileOrErrorStr expectedFilePath
+
+      let formattedResult = compileString filePath source
+          assertion = matchesExpected expectedJs formattedResult
+
+      return $ testCase filePath assertion
 
 
 -- CHECK RESULTS
@@ -86,3 +118,19 @@ isFailure result =
       Left _ ->
           assertBool "" True
 
+
+matchesExpected :: String -> Either String Compiler.Result -> Assertion
+matchesExpected expectedJs result =
+    case result of
+      Right (Compiler.Result _ _ js) ->
+          assertEqual matchFailureMessage expectedJs js
+
+      _ ->
+          assertFailure "compile failed; could not match expected output"
+
+
+matchFailureMessage :: String
+matchFailureMessage =
+  "Compiled JS did not match expected JS." ++
+  "\n  If the change is intentional, rerun " ++
+  "the tests with environment variable: ELM_WRITE_NEW_EXPECTED=1"
