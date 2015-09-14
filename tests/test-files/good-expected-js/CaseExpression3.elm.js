44519e8564739c4b4cf392b1fbd78ad215b0feca
Evan Czaplicki <info@elm-lang.org>
2015-09-13 21:36:21 -0700

    Update tests

--- tests/test-files/good-expected-js/CaseExpression3.elm.js
+++ tests/test-files/good-expected-js/CaseExpression3.elm.js
@@ -9,15 +9,19 @@ list2) {
    var _p0 = {ctor: "_Tuple2"
              ,_0: list1
              ,_1: list2};
-   switch (_p0._0.ctor)
-   {case "Nil": return elm_lang$core$Main$Nil;
-      default: switch (_p0._1.ctor)
-        {case "Nil": return elm_lang$core$Main$Nil;
-           default: var x = _p0._0._0;
-             var xs = _p0._0._1;
-             var y = _p0._1._0;
-             var ys = _p0._1._1;
-             return A2(elm_lang$core$Main$Cons,
-             {ctor: "_Tuple2",_0: x,_1: y},
-             A2(elm_lang$core$Main$zip,xs,ys));}}
+   if (_p0._0.ctor === "Nil") {
+         return elm_lang$core$Main$Nil;
+      } else {
+         if (_p0._1.ctor === "Nil") {
+               return elm_lang$core$Main$Nil;
+            } else {
+               var x = _p0._0._0,
+               xs = _p0._0._1,
+               y = _p0._1._0,
+               ys = _p0._1._1;
+               return A2(elm_lang$core$Main$Cons,
+               {ctor: "_Tuple2",_0: x,_1: y},
+               A2(elm_lang$core$Main$zip,xs,ys));
+            }
+      }
 });
