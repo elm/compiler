e29deed407ccba5a76100859e2b7e3c707a73a0e
Evan Czaplicki <info@elm-lang.org>
2015-10-07 17:24:13 -0700

    Change Benchmark API to allow arbitrary nesting of suites

--- bench/src/Benchmark.elm
+++ bench/src/Benchmark.elm
@@ -1,212 +1,100 @@
 module Benchmark
-    ( Never
-    , Benchmark
-    , Suite (..)
-    , BenchStats
-    , run
-    , runWithProgress
+    ( Suite
     , bench
-    , bench1
-    , bench2
-    , bench3
-    , bench4
-    , bench5
-    , bench6
-    , bench7
-    , bench8
-    , bench9
+    , suite
+    , run
+    , Statistics
+    , statisticsToString
     )where
-{-| A wrapper around benchmark.js that allows benchmarking
-of pure functions to be evaluated.
+{-| A wrapper around benchmark.js that allows benchmarking of pure functions.
 
-# Error and Benchmark types
-@docs Never, Benchmark, Suite, BenchStats
+# Creating Benchmarks
+@docs Benchmark, bench, suite
 
-# Running benchmarks
-@docs run, runWithProgress
-
-# Creating benchmarks
-@docs bench, bench1, bench2, bench3, bench4, bench5, bench6, bench7, bench8, bench9
+# Run benchmarks
+@docs run, Statistics
 -}
 
 
-import Task
-import String
 import Native.Benchmark
 import Native.BenchmarkJS
-import Signal
-import Time
+import String
+import Task
 
 
-{-|
-Our error return type: can never be instantiated
-because running benchmarks should not fail
-|-}
-type Never = 
-    Never Never
+
+-- SET UP BENCHMARK SUITE
 
 
-{-|
-Opaque type representing a function to be timed
-|-}
-type Benchmark = 
-    Benchmark
-
-
-{-|
-The results of running a benchmark
-|-}
-type BenchStats = 
-  BenchStats 
-  { name : String
-  , hz : Float
-  , marginOfError : Float
-  , moePercent : Float
---  , numRunsSampled : Int
-  }
-
-
-{-|
-A single or collection of benchmarks
-that can be run, generating output
-|-}
-type Suite = 
-      SingleBenchmark Benchmark
+{-| A set of benchmarks. -}
+type Benchmark
+    = Test String Thunk
     | Suite String (List Benchmark)
 
 
-{-|
-Run a benchmark, generating a list of results for each benchmark.
-Results contain a pretty-printed string summary, and a list
-of detailed data for each benchmark.
-|-}
-run 
-  :  Suite 
-  -> Task.Task Never (String, List BenchStats)
-run = runWithProgress Nothing
+type Thunk = Thunk
 
 
-{-|
-Run a benchmark, generating a list of results for each benchmark
-and updating a String signal with progress as the benchmarks run
-|-}
-runWithProgress 
-  :  Maybe (Signal.Mailbox String) 
-  -> Suite 
-  -> Task.Task Never (String, List BenchStats)
-runWithProgress maybeMailbox suite = 
-  let
-    ourTask = 
-      case maybeMailbox of
-        Nothing -> \_ -> Task.sleep 0
-        Just mailbox -> 
-          \s ->
-            (Signal.send mailbox.address s
-            `Task.andThen` \_ -> Task.sleep (5*Time.second))
-          -- `Task.andThen` \_ -> Task.keepGoing
-  in
-    Native.Benchmark.runWithProgress ourTask suite 
-
-{-|
-Create a benchmark with the given name
-running an arbitrary thunk
-|-}
+{-| An individual benchmark with a name. It measures how long it takes to
+evaluate the given function when it is passed a `()` value.
+-}
 bench : String -> (() -> result) -> Benchmark
-bench = Native.Benchmark.makeBenchmark 
+bench name thunk =
+  Test name (Native.Benchmark.makeThunk thunk)
 
 
-{-|
-Functions for creating named benchmarks with 1 to 9 arguments
-|-}
-bench1 : String -> (a -> result) -> a -> Benchmark
-bench1 name f a = 
-    Native.Benchmark.makeBenchmark name (\_ -> f a)
+{-| A collection of benchmarks to run together.
+-}
+suite : String -> List Benchmark -> Benchmark
+suite =
+  Suite
 
 
-{-||-}
-bench2 : String -> (a -> b -> result) -> a -> b -> Benchmark
-bench2 name f a b = 
-    Native.Benchmark.makeBenchmark name (\_ -> f a b)
+
+-- RUN THE BENCHMARKS
 
 
-{-||-}
-bench3 : String -> (a -> b -> c -> result) -> a -> b -> c -> Benchmark
-bench3 name f a b c = 
-    Native.Benchmark.makeBenchmark name (\_ -> f a b c)
+{-| Statistics -}
+type Statistics
+    = StatSuite String (List Statistics)
+    | Result String
+        { mean : Float
+        }
 
 
-{-||-}
-bench4 : String -> (a -> b -> c -> d -> result) -> a -> b -> c -> d -> Benchmark
-bench4 name f a b c d = 
-    Native.Benchmark.makeBenchmark name (\_ -> f a b c d)
+{-| Run a benchmark.
+-}
+run : Benchmark -> Task.Task x Statistics
+run =
+  Native.Benchmark.run
 
 
-{-||-}
-bench5 : String -> (a -> b -> c -> d -> e -> result) -> a -> b -> c -> d -> e -> Benchmark
-bench5 name f a b c d e = 
-    Native.Benchmark.makeBenchmark name (\_ -> f a b c d e)
+
+-- TO STRING
 
 
-{-||-}
-bench6 : 
-  String 
-  -> (a -> b -> c -> d -> e -> f -> result) 
-  -> a 
-  -> b 
-  -> c 
-  -> d 
-  -> e 
-  -> f 
-  -> Benchmark
-bench6 name fn a b c d e f = 
-    Native.Benchmark.makeBenchmark name (\_ -> fn a b c d e f)
+statisticsToString : Statistics -> String
+statisticsToString statistics =
+  List.join "\n" (statisticsToStringHelp statistics)
 
 
-{-||-}
-bench7 :
-  String
-  -> (a -> b -> c -> d -> e -> f -> g -> result)
-  -> a 
-  -> b 
-  -> c 
-  -> d 
-  -> e 
-  -> f 
-  -> g
-  -> Benchmark
-bench7 name fn a b c d e f g = 
-    Native.Benchmark.makeBenchmark name (\_ -> fn a b c d e f g)
+statisticsToStringHelp : Statistics -> (List String, Float)
+statisticsToStringHelp statistics =
+  case statistics of
+    Result name {mean} ->
+        ( [ name ++ " - " ++ toString mean ]
+        , mean
+        )
 
+    StatSuite name subStats ->
+        let
+            (strings, subMeans) =
+                List.unzip (List.map statisticsToStringHelp subStats)
 
-{-||-}
-bench8 : 
-  String 
-  -> (a -> b -> c -> d -> e -> f -> g -> h -> result)
-  -> a 
-  -> b 
-  -> c 
-  -> d 
-  -> e 
-  -> f 
-  -> g
-  -> h
-  -> Benchmark
-bench8 name fn a b c d e f g h = 
-    Native.Benchmark.makeBenchmark name  (\_ -> fn a b c d e f g h)
-
-{-||-}
-bench9 :
-  String 
-  -> (a -> b -> c -> d -> e -> f -> g -> h -> i -> result)
-  -> a 
-  -> b 
-  -> c 
-  -> d 
-  -> e 
-  -> f 
-  -> g
-  -> h
-  -> i
-  -> Benchmark
-bench9 name fn a b c d e f g h i = 
-    Native.Benchmark.makeBenchmark name (\_ -> fn a b c d e f g h i)
+            total =
+                List.sum means
+        in
+            ( name ++ " - " ++ toString total
+              :: List.map (String.padLeft 4 ' ') (List.concat strings)
+            , total
+            )
--- bench/src/Native/Benchmark.js
+++ bench/src/Native/Benchmark.js
@@ -8,95 +8,68 @@ Elm.Native.Benchmark.make = function(localRuntime) {
 		return localRuntime.Native.Benchmark.values;
 	}
 
-	var List = Elm.Native.List.make(localRuntime);
+	var List = Elm.List.make(localRuntime);
 	var Task = Elm.Native.Task.make(localRuntime);
-        var Signal = Elm.Signal.make(localRuntime);
-        var Utils = Elm.Native.Utils.make(localRuntime);
 
-        //Nothing special happens here, but we need to use it as a Native function
-        //So that we can accept thunks of any type
-        function makeBenchmark(name, thunk)
-        {
-            return {name : name, thunk : thunk};
-        }
 
-        //Generate the task for running a benchmark suite
-        //Possibly updating a given mailbox with a string describing
-        //Our progress of running the benchmarks so far
-	function runWithProgress(maybeTaskFn, inSuite)
+	// RUN SOME BENCHMARKS
+
+	function run(benchmark)
 	{
-            
-	    return Task.asyncFunction(function(callback) {
-                var bjsSuite = new Benchmark.Suite;
-                var benchArray;
-                var retData = [];
-                var finalString = "";
-                var numCompleted = 0;
-                var numToRun;
-                
-                switch (inSuite.ctor)
-                {
-                    case "Suite":
-                      benchArray = List.toArray(inSuite._1);
-                      break;
-                    case "SingleBenchmark":
-                      benchArray = [inSuite._0 ];
-                      break;
-                }
+		return Task.asyncFunction(function(callback)
+		{
+			var overallSuite = new Benchmark.Suite;
+			var statistics = addTests(overallSuite, benchmark);
+			overallSuite.on('complete', function(event) {
+				callback(Task.succeed(statistics));
+			});
+			overallSuite.run({ async: true });
+		});
+	}
 
-                numToRun = benchArray.length;
-                Task.perform(maybeTaskFn("Running benchmark 1 of " + numToRun));
 
-                //Initialize each benchmark in the suite
-                for (i = 0; i < benchArray.length; i++)
-                {
-                    var ourThunk = function (){
-                        //Run the thing we're timing, then mark the asynch benchmark as done
-                        benchArray[i].thunk();
-                        deferred.resolve();
-                        }
-                    bjsSuite.add(benchArray[i].name, benchArray[i].thunk );
-                }
+	// ADD TESTS TO THE JS BENCHMARK SUITE
 
-                //Every time a benchmark finishes, we store its results
-                //and update our progress string
-                bjsSuite.on('cycle', function(event) {
-                   numCompleted += 1;
-                   retData.push(
-                       { name : event.target.options.name
-                       , hz : event.target.hz
-                       , marginOfError : event.target.stats.moe
-                       , moePercent : event.target.stats.rme
-                       }
-                       );
-                   finalString += String(event.target) + "\n";
-                   var intermedString = 
-                        "Running benchmark " 
-                        + (numCompleted + 1) 
-                        + " of " + numToRun 
-                        + "\nLast result: " + String(event.target);
-                   Task.perform(maybeTaskFn(intermedString));
-                   //retString += String(event.target) + "\n";
-                });
-                
-                //When the last benchmark finishes, we show all results collected
-                bjsSuite.on('complete', function(event) {
-                   Task.perform(maybeTaskFn("Final results:\n\n" + finalString) );
-                   return callback(Task.succeed(Utils.Tuple2(finalString, List.fromArray(retData))));
-                });
+	function addTests(overallSuite, benchmark)
+	{
+		if (benchmark.ctor === 'Suite')
+		{
+			return {
+				ctor: 'StatSuite',
+				_0: benchmark._0,
+				_1: A2(List.map, curriedAddTests(overallSuite), benchmark._1)
+			};
+		}
 
-                //Finally: actually run the suite
-                Task.perform(
-                  Task.asyncFunction(function(otherCallback){
-                      bjsSuite.run({ 'async': true });
-                  }));
-        });
-             }
+		// WE MUST BE LOOKING AT A TEST
+		var name = benchmark._0;
+		var thunk = benchmark._1;
+
+		var statistics = {
+			ctor: 'Result',
+			_0: name
+			_1: null
+		};
+
+		var jsBenchmark = new Benchmark(name, thunk, {
+			onComplete: function() {
+				var info = {
+					mean: jsBenchmark.stats.mean
+				};
+				statistics._1 = info;
+			}
+		});
+		overallSuite.add(jsBenchmark);
+
+		return statistics;
+	}
+
+	var curriedAddTests = F2(addTests);
+
+
 
 	return localRuntime.Native.Benchmark.values = {
-		makeBenchmark: F2(makeBenchmark),
-		runWithProgress: F2(runWithProgress)
+		makeThunk: function(x) { return x; },
+		run: run
 	};
-                
-   
 };
