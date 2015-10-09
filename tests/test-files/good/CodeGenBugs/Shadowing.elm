05a0c164c93e080e0172e8c27eb05a48d5c7964e
Evan Czaplicki <info@elm-lang.org>
2015-10-09 16:18:44 -0700

    Add some more explanation to @laszlopandy's addition from #1086

--- tests/test-files/good/CodeGenBugs/Shadowing.elm
+++ tests/test-files/good/CodeGenBugs/Shadowing.elm
@@ -1,18 +1,22 @@
+-- From: https://github.com/elm-lang/elm-compiler/issues/1086
+--
+-- Problem is that the `a` in (Second a b) shadowed the argument to `toName`,
+-- actually overwriting the value.
 
-{- From: https://github.com/elm-lang/elm-compiler/issues/1086 -}
 
 type AType
     = First
     | Second String String
 
+
 toName : AType -> String
 toName a =
-    let name =
-            case a of
-                First ->
-                    "True"
+  let name =
+        case a of
+          First ->
+              "True"
 
-                Second a b ->
-                    a
-    in
-        name
+          Second a b ->
+              a
+  in
+    name
