d7c2354b35f099853241100baa2c2352a4569560
Laszlo Pandy <laszlo.pandy@prezi.com>
2015-10-26 16:22:48 +0100

    Expected output tests for Debug.crash.

---
+++ tests/test-files/good-expected-js/Crash/CrashIf.elm.js
@@ -0,0 +1,15 @@
+Elm.Main = Elm.Main || {};
+Elm.Main.make = function (_elm) {
+   "use strict";
+   _elm.Main = _elm.Main || {};
+   if (_elm.Main.values) return _elm.Main.values;
+   var _U = Elm.Native.Utils.make(_elm),
+   $Debug = Elm.Debug.make(_elm);
+   var _op = {};
+   var test = function (x) {
+      return x ? 2 : _U.crash("Main",
+      {start: {line: 8,column: 9}
+      ,end: {line: 8,column: 20}})("unexpected value");
+   };
+   return _elm.Main.values = {_op: _op,test: test};
+};
