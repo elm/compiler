09e0b442259e82effb71107e53667095085a63b7
Evan Czaplicki <info@elm-lang.org>
2016-02-01 10:16:50 -0800

    cosmetic: modernize whitespace

--- src/AST/Type.hs
+++ src/AST/Type.hs
@@ -63,9 +63,11 @@ getPortType portType =
 
 tuple :: R.Region -> [Raw] -> Raw
 tuple region types =
-  let name = Var.Raw ("_Tuple" ++ show (length types))
+  let
+    name =
+      Var.Raw ("_Tuple" ++ show (length types))
   in
-      A.A region (RApp (A.A region (RType name)) types)
+    A.A region (RApp (A.A region (RType name)) types)
 
 
 
@@ -76,65 +78,68 @@ deepDealias :: Canonical -> Canonical
 deepDealias tipe =
   case tipe of
     Lambda a b ->
-          Lambda (deepDealias a) (deepDealias b)
+      Lambda (deepDealias a) (deepDealias b)
 
     Var _ ->
-        tipe
+      tipe
 
     Record fields ext ->
-        Record (map (second deepDealias) fields) (fmap deepDealias ext)
+      Record (map (second deepDealias) fields) (fmap deepDealias ext)
 
     Aliased _name args tipe' ->
-        deepDealias (dealias args tipe')
+      deepDealias (dealias args tipe')
 
     Type _ ->
-        tipe
+      tipe
 
     App f args ->
-        App (deepDealias f) (map deepDealias args)
+      App (deepDealias f) (map deepDealias args)
 
 
 iteratedDealias :: Canonical -> Canonical
 iteratedDealias tipe =
   case tipe of
     Aliased _ args realType ->
-        iteratedDealias (dealias args realType)
+      iteratedDealias (dealias args realType)
 
     _ ->
-        tipe
+      tipe
 
 
 dealias :: [(String, Canonical)] -> Aliased Canonical -> Canonical
 dealias args aliasType =
   case aliasType of
     Holey tipe ->
-        dealiasHelp (Map.fromList args) tipe
+      dealiasHelp (Map.fromList args) tipe
 
     Filled tipe ->
-        tipe
+      tipe
 
 
 dealiasHelp :: Map.Map String Canonical -> Canonical -> Canonical
 dealiasHelp typeTable tipe =
-    let go = dealiasHelp typeTable in
-    case tipe of
-      Lambda a b ->
-          Lambda (go a) (go b)
+  let
+    go =
+      dealiasHelp typeTable
+  in
+  case tipe of
+    Lambda a b ->
+      Lambda (go a) (go b)
 
-      Var x ->
-          Map.findWithDefault tipe x typeTable
+    Var x ->
+      Map.findWithDefault tipe x typeTable
 
-      Record fields ext ->
-          Record (map (second go) fields) (fmap go ext)
+    Record fields ext ->
+      Record (map (second go) fields) (fmap go ext)
 
-      Aliased original args t' ->
-          Aliased original (map (second go) args) t'
+    Aliased original args t' ->
+      Aliased original (map (second go) args) t'
 
-      Type _ ->
-          tipe
+    Type _ ->
+      tipe
 
-      App f args ->
-          App (go f) (map go args)
+    App f args ->
+      App (go f) (map go args)
 
 
 
@@ -145,10 +150,10 @@ collectLambdas :: Canonical -> [Canonical]
 collectLambdas tipe =
   case tipe of
     Lambda arg result ->
-        arg : collectLambdas result
+      arg : collectLambdas result
 
     _ ->
-        [tipe]
+      [tipe]
 
 
 
@@ -157,49 +162,49 @@ collectLambdas tipe =
 
 instance Binary Canonical where
   put tipe =
-      case tipe of
-        Lambda t1 t2 ->
-            putWord8 0 >> put t1 >> put t2
+    case tipe of
+      Lambda t1 t2 ->
+        putWord8 0 >> put t1 >> put t2
 
-        Var x ->
-            putWord8 1 >> put x
+      Var x ->
+        putWord8 1 >> put x
 
-        Type name ->
-            putWord8 2 >> put name
+      Type name ->
+        putWord8 2 >> put name
 
-        App t1 t2 ->
-            putWord8 3 >> put t1 >> put t2
+      App t1 t2 ->
+        putWord8 3 >> put t1 >> put t2
 
-        Record fs ext ->
-            putWord8 4 >> put fs >> put ext
+      Record fs ext ->
+        putWord8 4 >> put fs >> put ext
 
-        Aliased var args t ->
-            putWord8 5 >> put var >> put args >> put t
+      Aliased var args t ->
+        putWord8 5 >> put var >> put args >> put t
 
-  get = do
-      n <- getWord8
-      case n of
-        0 -> Lambda <$> get <*> get
-        1 -> Var <$> get
-        2 -> Type <$> get
-        3 -> App <$> get <*> get
-        4 -> Record <$> get <*> get
-        5 -> Aliased <$> get <*> get <*> get
-        _ -> error "Error reading a valid type from serialized string"
+  get =
+    do  n <- getWord8
+        case n of
+          0 -> Lambda <$> get <*> get
+          1 -> Var <$> get
+          2 -> Type <$> get
+          3 -> App <$> get <*> get
+          4 -> Record <$> get <*> get
+          5 -> Aliased <$> get <*> get <*> get
+          _ -> error "Error reading a valid type from serialized string"
 
 
 instance Binary t => Binary (Aliased t) where
   put aliasType =
-      case aliasType of
-        Holey tipe ->
-            putWord8 0 >> put tipe
+    case aliasType of
+      Holey tipe ->
+        putWord8 0 >> put tipe
 
-        Filled tipe ->
-            putWord8 1 >> put tipe
+      Filled tipe ->
+        putWord8 1 >> put tipe
 
-  get = do
-      n <- getWord8
-      case n of
-        0 -> Holey <$> get
-        1 -> Filled <$> get
-        _ -> error "Error reading a valid type from serialized string"
+  get =
+    do  n <- getWord8
+        case n of
+          0 -> Holey <$> get
+          1 -> Filled <$> get
+          _ -> error "Error reading a valid type from serialized string"
