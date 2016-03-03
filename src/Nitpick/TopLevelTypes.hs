4b1ea5c461bc93d6070f0673ec8dd18159dcb568
Evan Czaplicki <info@elm-lang.org>
2016-03-03 15:00:33 -0800

    Simplify code that checks `main` type a little bit

--- src/Nitpick/TopLevelTypes.hs
+++ src/Nitpick/TopLevelTypes.hs
@@ -1,10 +1,10 @@
 {-# OPTIONS_GHC -Wall #-}
 module Nitpick.TopLevelTypes (topLevelTypes) where
 
+import Control.Monad (foldM, when)
 import Data.Map ((!))
-import Prelude hiding (maybe)
-import qualified Data.Foldable as F
 import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
 
 import qualified AST.Expression.Valid as Valid
 import qualified AST.Declaration as Decl
@@ -15,18 +15,22 @@ import qualified AST.Variable as Var
 import qualified Elm.Package as Pkg
 import qualified Reporting.Annotation as A
 import qualified Reporting.Error.Type as Error
+import qualified Reporting.Region as R
 import qualified Reporting.Result as Result
 import qualified Reporting.Warning as Warning
 
 
 
+-- CHECK TOP LEVEL TYPES
+
+
 topLevelTypes
-    :: Map.Map String Type.Canonical
-    -> [Decl.Valid]
-    -> Result.Result Warning.Warning Error.Error ()
+  :: Map.Map String Type.Canonical
+  -> [Decl.Valid]
+  -> Result.Result Warning.Warning Error.Error ()
 topLevelTypes typeEnv validDecls =
-  do  F.traverse_ (warnMissingAnnotation typeEnv) validDecls
-      checkMainType typeEnv validDecls
+  do  maybeMainRegion <- foldM (warnMissingAnnotation typeEnv) Nothing validDecls
+      maybe (return ()) (checkMainType typeEnv) maybeMainRegion
 
 
 
@@ -34,59 +38,50 @@ topLevelTypes typeEnv validDecls =
 
 
 warnMissingAnnotation
-    :: Map.Map String Type.Canonical
-    -> Decl.Valid
-    -> Result.Result Warning.Warning Error.Error ()
-warnMissingAnnotation typeEnv (A.A (region,_) decl) =
+  :: Map.Map String Type.Canonical
+  -> Maybe R.Region
+  -> Decl.Valid
+  -> Result.Result Warning.Warning Error.Error (Maybe R.Region)
+warnMissingAnnotation typeEnv maybeMainRegion (A.A (region,_) decl) =
   case decl of
-    Decl.Def (Valid.Definition (A.A _ (P.Var name)) _ Nothing) ->
-      Result.warn region (Warning.MissingTypeAnnotation name (typeEnv ! name))
+    Decl.Def (Valid.Definition (A.A _ (P.Var name)) _ maybeType) ->
+      do  when (Maybe.isNothing maybeType) $
+            Result.warn region (Warning.MissingTypeAnnotation name (typeEnv ! name))
+
+          return (if name == "main" then Just region else maybeMainRegion)
 
     _ ->
-      return ()
+      return maybeMainRegion
 
 
 
--- MAIN TYPE
+-- CHECK MAIN TYPE
 
 
-checkMainType
-    :: Map.Map String Type.Canonical
-    -> [Decl.Valid]
-    -> Result.Result w Error.Error ()
-checkMainType typeEnv decls =
-    case decls of
-      A.A (region,_) (Decl.Def (Valid.Definition (A.A _ (P.Var "main")) _ _)) : _ ->
-          case Map.lookup "main" typeEnv of
-            Nothing ->
-              return ()
+checkMainType :: Map.Map String Type.Canonical -> R.Region -> Result.Result w Error.Error ()
+checkMainType typeEnv region =
+  let
+    tipe =
+      typeEnv ! "main"
+  in
+    case Type.deepDealias tipe of
+      Type.App name [_] | name `elem` [program, vdomNode] ->
+        return ()
 
-            Just tipe ->
-              case Type.deepDealias tipe of
-                Type.App (Type.Type name) [_] | name == virtualDomNode ->
-                  return ()
-
-                Type.App (Type.Type name) [_] | name == program ->
-                  return ()
-
-                _ ->
-                  Result.throw region (Error.BadMain tipe)
-
-      _ : remainingDecls ->
-          checkMainType typeEnv remainingDecls
-
-      [] ->
-          return ()
+      _ ->
+        Result.throw region (Error.BadMain tipe)
 
 
-virtualDomNode :: Var.Canonical
-virtualDomNode =
-  Var.fromModule
-    (ModuleName.Canonical (Pkg.Name "evancz" "virtual-dom") ["VirtualDom"])
-    "Node"
-
-
-program :: Var.Canonical
+program :: Type.Canonical
 program =
-  Var.fromModule (ModuleName.inCore ["Platform"]) "Program"
+  Type.Type (Var.inCore ["Platform"] "Program")
+
+
+vdomNode :: Type.Canonical
+vdomNode =
+  let
+    vdom =
+      ModuleName.Canonical (Pkg.Name "evancz" "virtual-dom") ["VirtualDom"]
+  in
+    Type.Type (Var.fromModule vdom "Node")
 
