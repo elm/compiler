a8161f6a85d0a2a14718c3a4a3d28adaf8a135b9
jvoigtlaender <jvoigtlaender@users.noreply.github.com>
2016-04-19 08:52:15 +0200

    adjust expected output to new code gen

--- tests/test-files/good-expected-js/CodeGenBugs/Shadowing3.elm.js
+++ tests/test-files/good-expected-js/CodeGenBugs/Shadowing3.elm.js
@@ -1,16 +1,18 @@
-Elm.Main = Elm.Main || {};
-Elm.Main.make = function (_elm) {
-   "use strict";
-   _elm.Main = _elm.Main || {};
-   if (_elm.Main.values) return _elm.Main.values;
-   var _U = Elm.Native.Utils.make(_elm);
-   var _op = {};
-   var bad = function (arg) {
-      return function (x) {
-         return x;
-      }(function () {    var _p0 = arg;if (_p0.ctor === "Just") {    return true;} else {    var _p1 = true;return _p1 ? false : _p1;}}());
-   };
-   var Nothing = {ctor: "Nothing"};
-   var Just = function (a) {    return {ctor: "Just",_0: a};};
-   return _elm.Main.values = {_op: _op,Just: Just,Nothing: Nothing,bad: bad};
+var _elm_lang$core$Main$bad = function (arg) {
+	return function (x) {
+		return x;
+	}(
+		function () {
+			var _p0 = arg;
+			if (_p0.ctor === 'Just') {
+				return true;
+			} else {
+				var _p1 = true;
+				return _p1 ? false : _p1;
+			}
+		}());
+};
+var _elm_lang$core$Main$Nothing = {ctor: 'Nothing'};
+var _elm_lang$core$Main$Just = function (a) {
+	return {ctor: 'Just', _0: a};
 };
