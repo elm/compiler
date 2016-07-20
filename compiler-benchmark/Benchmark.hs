ead696e1b6be29fa5b2e89ef7ed9770756afd9d2
Justin Manley <manleyjster@gmail.com>
2016-07-19 21:52:38 -0700

    Reorganize and tidy compiler benchmark code.

--- compiler-benchmark/Benchmark.hs
+++ compiler-benchmark/Benchmark.hs
@@ -8,7 +8,7 @@ import qualified Data.Text.IO as Text
 import Data.Text as Text (Text, pack, unpack)
 import Data.Foldable (find)
 import Data.Map (Map)
-import Data.Maybe (isJust)
+import Data.Maybe (isJust, maybe)
 import Text.Tabular (Table(Table), Header(Header, Group), Properties(NoLine, SingleLine, DoubleLine))
 import qualified Text.Tabular.AsciiArt as Table
 import qualified Data.Map as Map
@@ -67,6 +67,7 @@ dev =
 costCentreNames :: [Text]
 costCentreNames = map Text.pack
     [ "parsing"
+    , "completeness"
     ]
 
 main :: IO ()
@@ -77,8 +78,11 @@ main = do
         createDirectoryIfMissing True "benchmark-projects"
         inDirectory "benchmark-projects" $ do
             todoMvcExists <- doesDirectoryExist "elm-todomvc"
-            unless todoMvcExists . void $
-                git ["clone", "https://github.com/evancz/elm-todomvc", "elm-todomvc"]
+            unless todoMvcExists . void $ git
+                [ "clone"
+                , "https://github.com/evancz/elm-todomvc"
+                , "elm-todomvc"
+                ]
             
         -- Source 
         createDirectoryIfMissing True "benchmark-src"
@@ -98,8 +102,10 @@ main = do
         inDirectory "benchmark-src/elm-make" $ do
             -- git [ "checkout", "0.17", "--quiet" ]
             cabal ["sandbox", "init"]
-            cabal ["sandbox", "add-source", root </> "benchmark-src" </> "elm-compiler"]
-            cabal ["sandbox", "add-source", root </> "benchmark-src" </> "elm-package"]
+            cabal ["sandbox", "add-source",
+                root </> "benchmark-src" </> "elm-compiler"]
+            cabal ["sandbox", "add-source",
+                root </> "benchmark-src" </> "elm-package"]
             cabal ["install"]
 
         createDirectoryIfMissing True "benchmark-results"
@@ -112,12 +118,32 @@ main = do
         -- branch of the repo in which benchmarking is being run.
         inDirectory (root </> "benchmark-src/elm-compiler") $ do
             hasDevBranch <- successful <$> git ["remote", "show", "dev"]
-            unless hasDevBranch (void $ git ["remote", "add", "dev", root </> ".."])
+            unless hasDevBranch (void $ git ["remote", "add", "dev",
+                root </> ".."])
             git ["pull", "dev", "master"]
 
         forM_ projects (compile Test)
 
         reportResults
+
+-- SETUP
+
+-- Fetch and prepare a repository containing some component of the Elm Platform.
+makeSrcRepo :: String -> IO ()
+makeSrcRepo projectName = do
+    root <- getCurrentDirectory
+
+    projectExists <- doesDirectoryExist $ "benchmark-src" </> projectName
+    unless projectExists $ do
+        git [ "clone"
+            , "https://github.com/elm-lang/" ++ projectName ++ ".git"
+            , "benchmark-src" </> projectName
+            ]
+        inDirectory ("benchmark-src" </> projectName) $ do
+            copyFile (root </> "cabal.config.example") "cabal.config"
+
+
+-- COMPILATION
 
 -- Compile an Elm project using the Elm compiler.
 compile :: Version -> FilePath -> IO ()
@@ -132,14 +158,28 @@ compile version project = do
     when elmStuffExists (removeDirectoryRecursive "elm-stuff/build-artifacts")
 
     -- The -p flag turns profiling on.
-    let elmMake = rawSystem (root </> "benchmark-src/elm-make/.cabal-sandbox/bin/elm-make")
+    let elmMake = rawSystem $ root </> "benchmark-src/elm-make/.cabal-sandbox/bin/elm-make"
     elmMake ["+RTS", "-p"] 
 
     -- Profiling should produce a file called elm-make.prof.
-    copyFile "elm-make.prof" (root </> "benchmark-results" </> (project ++ "." ++ show version ++ ".prof"))
+    copyFile "elm-make.prof" (root 
+        </> "benchmark-results" 
+        </> (project ++ "." ++ show version ++ ".prof"))
 
     -- reset
     setCurrentDirectory root
+    
+
+-- REPORTING
+
+-- CostCentreDiff is used to group the cost centre results from runs of
+-- different compilers.
+data CostCentreDiff = CostCentreDiff
+    { gold :: Maybe CostCentre
+    -- ^ Profiling results for the gold version of the compiler
+    , dev :: Maybe CostCentre
+    -- ^ Profiling results for the dev version of the compiler
+    }
 
 reportResults :: IO ()
 reportResults = do
@@ -151,67 +191,65 @@ reportResults = do
 
     case parseOnly timeAllocProfile goldResults of
         Right goldProfile -> do 
-            goldResults <- reportMissingCostCentres . extractCostCentres $ goldProfile 
+            let goldResults = extractCostCentres goldProfile 
 
             case parseOnly timeAllocProfile devResults of
                 Right devProfile -> do
-                    devResults <- reportMissingCostCentres . extractCostCentres $ devProfile
-                    putStrLn . Table.render unpack id show . reportDiffs $ Map.intersectionWith CostCentreDiff goldResults devResults
-                Left error -> putStrLn $ "Error parsing profiling results from " ++ devFilename ++ ": " ++ error
+                    let devResults = extractCostCentres devProfile
+                    
+                    -- Intersection should have no effect here, since the gold
+                    -- and dev maps are both constructed using costCentreNames.
+                    reportDiffs $ Map.intersectionWith CostCentreDiff
+                        goldResults
+                        devResults
+                Left error -> reportParseError devFilename error 
 
-        Left error -> putStrLn $ "Error parsing profiling results from " ++ goldFilename ++ ": " ++ error
+        Left error -> reportParseError goldFilename error 
 
-data CostCentreDiff = CostCentreDiff
-    { gold :: CostCentre
-    -- ^ Profiling results for the gold version of the compiler
-    , dev :: CostCentre
-    -- ^ Profiling results for the dev version of the compiler
-    }
-
--- Adds a header
---reportDiffs :: Map Text CostCentreDiff -> Table (Header 
-reportDiffs results = Table
-        (Group SingleLine [ Group NoLine $ map Header (Map.keys results) ])
-        (Group DoubleLine [ Group NoLine 
-            [ Header "%time in gold"
-            , Header "%time in dev"
-            ]])
-        (map reportDiff $ Map.elems results)
     where
-        reportDiff :: CostCentreDiff -> [Double]
+        reportParseError :: FilePath -> String -> IO ()
+        reportParseError f errorMsg = putStrLn 
+            $ "Error parsing profiling results from "
+            ++ f
+            ++ ": "
+            ++ errorMsg
+
+-- Report the results of profiling in a nice table.
+reportDiffs :: Map Text CostCentreDiff -> IO ()
+reportDiffs results =
+        putStrLn . Table.render unpack id renderMaybe $ resultsTable
+    where
+        resultsTable :: Table Text String (Maybe Double)
+        resultsTable = Table
+            (Group SingleLine [ Group NoLine $ map Header (Map.keys results) ])
+            (Group DoubleLine [ Group NoLine 
+                [ Header "%time in gold"
+                , Header "%time in dev"
+                ]])
+            (map reportDiff $ Map.elems results)
+
+        renderMaybe :: Show a => Maybe a -> String
+        renderMaybe = maybe "-" show
+
+        reportDiff :: CostCentreDiff -> [Maybe Double]
         reportDiff costCentre = 
-            [ costCentreIndTime . gold $ costCentre
-            , costCentreIndTime . dev $ costCentre
+            [ costCentreIndTime <$> gold costCentre
+            , costCentreIndTime <$> dev costCentre
             ]
 
-reportMissingCostCentres :: Map Text (Maybe CostCentre) -> IO (Map Text CostCentre)
-reportMissingCostCentres results = do
-    let (present, missing) = partition (isJust . snd) $ Map.toList results
-    mapM_ reportMissingCostCentre missing    
-    return $ (Map.mapMaybe id . Map.fromList) present
-    where
-        reportMissingCostCentre :: (Text, Maybe CostCentre) -> IO ()
-        reportMissingCostCentre (name, _) = putStrLn $ "Missing cost centre " ++ unpack name
-    
-
+-- Extract cost centres from the given profiling report.
 extractCostCentres :: TimeAllocProfile -> Map Text (Maybe CostCentre)
-extractCostCentres tree = 
-        Map.fromList $ map (\name -> (name, findCostCentre name)) costCentreNames
+extractCostCentres tree = foldr insertCostCentre Map.empty costCentreNames
     where 
+        insertCostCentre :: Text 
+            -> Map Text (Maybe CostCentre)
+            -> Map Text (Maybe CostCentre)
+        insertCostCentre name = Map.insert name (findCostCentre name)
+
         findCostCentre :: Text -> Maybe CostCentre
         findCostCentre name = find ((== name) . costCentreName)
             $ costCentreNodes (profileCostCentreTree tree)
 
--- Fetch and prepare a repository containing some component of the Elm Platform.
-makeSrcRepo :: String -> IO ()
-makeSrcRepo projectName = do
-    root <- getCurrentDirectory
-
-    projectExists <- doesDirectoryExist $ "benchmark-src" </> projectName
-    unless projectExists $ do
-        git [ "clone", "https://github.com/elm-lang/" ++ projectName ++ ".git", "benchmark-src" </> projectName]
-        inDirectory ("benchmark-src" </> projectName) $ do
-            copyFile (root </> "cabal.config.example") "cabal.config"
 
 -- HELPER FUNCTIONS
 
