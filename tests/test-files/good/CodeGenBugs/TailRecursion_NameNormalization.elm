9f4d479b48a4a4de2690cb7a2f519260d5f06170
Evan Czaplicki <evancz@users.noreply.github.com>
2016-12-07 15:23:37 -0600

    Update tests: primes are no longer allowed in variable names

--- tests/test-files/good/CodeGenBugs/TailRecursion_NameNormalization.elm
+++
@@ -1,8 +0,0 @@
--- From https://github.com/elm-lang/elm-compiler/issues/1226
---
--- When code can be tail call optimized, one of the arguments turns into
--- the loop variable. Its name must be properly normalized, for example,
--- to avoid primes.
-
-go n' = if n' then go n' else False
-
