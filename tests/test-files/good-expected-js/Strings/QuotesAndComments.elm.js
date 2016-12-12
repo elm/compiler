001eff77654f2c50be6486ea0dfc70e5e91ef8a5
Evan Czaplicki <evancz@users.noreply.github.com>
2016-12-12 12:18:22 -0600

    Update tests: no longer testing for primes

--- tests/test-files/good-expected-js/Strings/QuotesAndComments.elm.js
+++ tests/test-files/good-expected-js/Strings/QuotesAndComments.elm.js
@@ -1,7 +1,3 @@
 var _elm_lang$core$Main$commentStart = '{-';
 var _elm_lang$core$Main$singleQuote = _elm_lang$core$Native_Utils.chr('\'');
 var _elm_lang$core$Main$doubleQuote = _elm_lang$core$Native_Utils.chr('\"');
-var _elm_lang$core$Main$variablesWithPrimes = function (x) {
-	var x$ = x;
-	return x$;
-};
