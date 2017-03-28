dd4b58827cb38dc555e82ef0f4a9d54cb3359b85
Evan Czaplicki <evancz@users.noreply.github.com>
2017-03-27 18:44:12 -0700

    Add a binary instance for AST.Literal

--- src/AST/Literal.hs
+++ src/AST/Literal.hs
@@ -1,7 +1,14 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
-module AST.Literal where
+module AST.Literal
+  ( Literal(..)
+  , toBuilder
+  , GLType(..), glTypeToText, Shader(..)
+  )
+  where
 
+
+import Data.Binary
 import qualified Data.Map as Map
 import Data.Monoid ((<>))
 import Data.Text (Text)
@@ -37,6 +44,15 @@ toBuilder literal =
 -- WebGL TYPES
 
 
+data Shader =
+  Shader
+    { attribute :: Map.Map Text GLType
+    , uniform :: Map.Map Text GLType
+    , varying :: Map.Map Text GLType
+    }
+    deriving (Eq)
+
+
 data GLType
   = Int
   | Float
@@ -60,10 +76,38 @@ glTypeToText glTipe =
     Texture -> "WebGL.Texture"
 
 
-data Shader =
-  Shader
-    { attribute :: Map.Map Text GLType
-    , uniform :: Map.Map Text GLType
-    , varying :: Map.Map Text GLType
-    }
-    deriving (Eq)
+
+-- BINARY
+
+
+instance Binary Literal where
+  put literal =
+    case literal of
+      Chr chr ->
+        putWord8 0 >> put chr
+
+      Str str ->
+        putWord8 1 >> put str
+
+      IntNum n ->
+        putWord8 2 >> put n
+
+      FloatNum n ->
+        putWord8 3 >> put n
+
+      Boolean True ->
+        putWord8 4
+
+      Boolean False ->
+        putWord8 5
+
+  get =
+    do  word <- getWord8
+        case word of
+          0 -> Chr <$> get
+          1 -> Str <$> get
+          2 -> IntNum <$> get
+          3 -> FloatNum <$> get
+          4 -> pure $ Boolean True
+          5 -> pure $ Boolean False
+          _ -> error "bad binary for AST.Literal"
