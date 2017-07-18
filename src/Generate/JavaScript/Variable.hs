8060c4ec4e3b71063ac350f36f9c75a09dbd8994
Evan Czaplicki <evancz@users.noreply.github.com>
2017-07-18 16:02:06 -0700

    Add intToAscii which converts ints to valid JS field names
    It converts an int to ASCII by checking if it is in progressively
    larger “block sizes” to make sure we get every possible name.
    
    We start with a block size of 53. This contains [a-zA-Z_]. If that is
    too small, we move on to [a-zA-Z_$][a-zA-Z_$0-9] with a block size of
    (54 * 64). From there we can just keep getting bigger indefinitely (54
    * 64^n) so we never run out of names.
    
    This code also checks for collisions with reserved words in JS,
    skipping over them. The logic about block sizes is a bit fancier on
    account of this.

--- src/Generate/JavaScript/Variable.hs
+++ src/Generate/JavaScript/Variable.hs
@@ -9,15 +9,20 @@ module Generate.JavaScript.Variable
   , global
   , globalName
   , globalToName
+  , intToAscii
   )
   where
 
 
 import qualified Control.Monad.State.Strict as State
+import qualified Data.ByteString as BS
 import Data.Monoid ((<>))
+import qualified Data.Map as Map
 import qualified Data.Set as Set
-import qualified Data.Text as Text
 import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text (encodeUtf8)
+import Data.Word (Word8)
 
 import qualified AST.Helpers as Help
 import qualified AST.Module.Name as ModuleName
@@ -61,28 +66,41 @@ local name =
 
 safe :: Text -> Text
 safe name =
-  if Set.member name reservedWords then
+  if Set.member name reservedNames then
     "_" <> name
   else
     name
 
 
-reservedWords :: Set.Set Text
-reservedWords =
+
+-- RESERVED NAMES
+
+
+reservedNames :: Set.Set Text
+reservedNames =
+  Set.union jsReservedWords elmReservedWords
+
+
+jsReservedWords :: Set.Set Text
+jsReservedWords =
   Set.fromList
-    -- reserved by JavaScript
-    [ "null", "undefined", "NaN", "Infinity", "true", "false", "eval"
-    , "arguments", "int", "byte", "char", "goto", "long", "final", "float"
-    , "short", "double", "native", "throws", "boolean", "abstract", "volatile"
-    , "transient", "synchronized", "function", "break", "case", "catch"
-    , "continue", "debugger", "default", "delete", "do", "else", "finally"
-    , "for", "function", "if", "in", "instanceof", "new", "return", "switch"
-    , "this", "throw", "try", "typeof", "var", "void", "while", "with", "class"
-    , "const", "enum", "export", "extends", "import", "super", "implements"
-    , "interface", "let", "package", "private", "protected", "public"
-    , "static", "yield"
-    -- reserved by Elm
-    , "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9"
+    [ "do", "if", "in"
+    , "NaN", "int", "for", "new", "try", "var", "let"
+    , "null", "true", "eval", "byte", "char", "goto", "long", "case", "else", "this", "void", "with", "enum"
+    , "false", "final", "float", "short", "break", "catch", "throw", "while", "class", "const", "super", "yield"
+    , "double", "native", "throws", "delete", "return", "switch", "typeof", "export", "import", "public", "static"
+    , "boolean", "default", "finally", "extends", "package", "private"
+    , "Infinity", "abstract", "volatile", "function", "continue", "debugger", "function"
+    , "undefined", "arguments", "transient", "interface", "protected"
+    , "instanceof", "implements"
+    , "synchronized"
+    ]
+
+
+elmReservedWords :: Set.Set Text
+elmReservedWords =
+  Set.fromList
+    [ "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9"
     , "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9"
     ]
 
@@ -119,3 +137,113 @@ globalToName (ModuleName.Canonical (Pkg.Name user project) moduleName) name =
       <> "$" <> Text.replace "." "_" moduleName
       <> "$" <> revisedName
 
+
+
+-- INT TO ASCII
+
+
+intToAscii :: Int -> BS.ByteString
+intToAscii n =
+  if n < 53 then -- skip $ as a standalone name
+    BS.singleton (toByte n)
+
+  else
+    intToAsciiHelp (numStartBytes * numInnerBytes) allBadFields (n - 53)
+
+
+intToAsciiHelp :: Int -> [BadFields] -> Int -> BS.ByteString
+intToAsciiHelp blockSize badFields n =
+  case badFields of
+    [] ->
+      if n < blockSize then
+        unsafeIntToAscii n
+      else
+        intToAsciiHelp (blockSize * numInnerBytes) [] (n - blockSize)
+
+    BadFields renamings : biggerBadFields ->
+      if n < blockSize - Map.size renamings then
+        let name = unsafeIntToAscii n in
+        Map.findWithDefault name name renamings
+      else
+        intToAsciiHelp (blockSize * numInnerBytes) biggerBadFields (n - blockSize)
+
+
+
+-- UNSAFE INT TO ASCII
+
+
+unsafeIntToAscii :: Int -> BS.ByteString
+unsafeIntToAscii n =
+  let
+    (quotient, remainder) =
+      quotRem n numStartBytes
+  in
+    BS.pack $ toByte remainder : unsafeIntToAsciiHelp quotient
+
+
+unsafeIntToAsciiHelp :: Int -> [Word8]
+unsafeIntToAsciiHelp n =
+  if n == 0 then
+    []
+  else
+    let (quotient, remainder) = quotRem n numInnerBytes in
+    toByte remainder : unsafeIntToAsciiHelp quotient
+
+
+
+-- ASCII BYTES
+
+
+numStartBytes :: Int
+numStartBytes =
+  54
+
+
+numInnerBytes :: Int
+numInnerBytes =
+  64
+
+
+toByte :: Int -> Word8
+toByte n
+  | n < 26  = fromIntegral (97 + n     ) {- lower -}
+  | n < 52  = fromIntegral (65 + n - 26) {- upper -}
+  | n == 53 = 95 {- _ -}
+  | n == 54 = 36 {- $ -}
+  | n < 64  = fromIntegral (48 + n - 54) {- digit -}
+  | True    = error $ "cannot convert int " ++ show n ++ " to ASCII"
+
+
+
+-- BAD FIELDS
+
+
+newtype BadFields =
+  BadFields { _renamings :: Renamings }
+
+
+type Renamings =
+  Map.Map BS.ByteString BS.ByteString
+
+
+allBadFields :: [BadFields]
+allBadFields =
+  let
+    add keyword dict =
+      Map.alter (Just . addRenaming keyword) (Text.length keyword) dict
+  in
+      Map.elems $ Set.foldr add Map.empty jsReservedWords
+
+
+addRenaming :: Text -> Maybe BadFields -> BadFields
+addRenaming keyword maybeBadFields =
+  let
+    maxName =
+      numStartBytes * numInnerBytes ^ (Text.length keyword - 1) - 1
+  in
+  case maybeBadFields of
+    Nothing ->
+      BadFields $ Map.singleton (Text.encodeUtf8 keyword) (unsafeIntToAscii maxName)
+
+    Just (BadFields renamings) ->
+      BadFields $ Map.insert (Text.encodeUtf8 keyword) (unsafeIntToAscii (maxName - Map.size renamings)) renamings
