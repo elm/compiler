c49ac3a40d4e12b12752858e45a5b45fd8d6bb50
Evan Czaplicki <evancz@users.noreply.github.com>
2017-11-02 15:34:29 -0700

    Update type constraint generation for effects

--- src/Type/Constrain/Effects.hs
+++ src/Type/Constrain/Effects.hs
@@ -1,112 +1,135 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Type.Constrain.Effects (constrain) where
+module Type.Constrain.Effects
+  ( constrain
+  )
+  where
 
+
+import Control.Monad (replicateM)
 import qualified Data.Map.Strict as Map
 
-import qualified AST.Effects as Effects
+import qualified AST.Expression.Canonical as Can
 import qualified AST.Module.Name as ModuleName
-import qualified AST.Variable as Var
-import qualified Reporting.Annotation as A
+import qualified AST.Type as T
+import qualified Elm.Name as N
 import qualified Reporting.Error.Type as Error
+import qualified Reporting.Region as R
 import Type.Constraint (Constraint(..), Scheme(Scheme))
-import Type.Type (Variable, Type(AppN,VarN), (==>), mkFlexVar)
+import Type.Type (Type(..), Variable, (==>), fromFlexSrcType, mkFlexVar)
 
 
 
--- CONSTRAIN EFFECTS
+-- CONSTRAIN
 
 
-constrain :: ModuleName.Canonical -> Effects.Canonical -> IO Constraint
-constrain moduleName effects =
+constrain :: ModuleName.Canonical -> Can.Effects -> IO ( Map.Map N.Name Type, Constraint )
+constrain home effects =
   case effects of
-    Effects.None ->
-      return CSaveEnv
+    Can.NoEffects ->
+      return (Map.empty, CSaveEnv)
 
-    Effects.Port _ ->
-      return CSaveEnv
+    Can.Ports ports ->
+      (,)
+        <$> traverse fromSrcType (Map.map deport ports)
+        <*> return CSaveEnv
 
-    Effects.Manager _ info ->
-      constrainHelp moduleName info
+    Can.Manager r0 r1 r2 manager ->
+      case manager of
+        Can.Cmd cmdName ->
+          do  cmd <- command home cmdName
+              con <- constrainEffects r0 r1 r2 $ \msg result ->
+                effectList home cmdName msg ==> result
+              return (Map.singleton "command" cmd, con)
+
+        Can.Sub subName ->
+          do  sub <- subscription home subName
+              con <- constrainEffects r0 r1 r2 $ \msg result ->
+                effectList home subName msg ==> result
+              return (Map.singleton "subscription" sub, con)
+
+        Can.Fx cmdName subName ->
+          do  cmd <- command home cmdName
+              sub <- subscription home subName
+              con <- constrainEffects r0 r1 r2 $ \msg result ->
+                effectList home cmdName msg ==> effectList home subName msg ==> result
+              return (Map.fromList [("command", cmd),("subscription", sub)], con)
 
 
-never :: Type
-never =
-  AppN Var.never []
+
+-- HELPERS
 
 
-constrainHelp :: ModuleName.Canonical -> Effects.Info -> IO Constraint
-constrainHelp moduleName (Effects.Info tagRegion r0 r1 r2 managerType) =
+deport :: Can.Port -> T.Canonical
+deport port_ =
+  case port_ of
+    Can.Incoming tipe ->
+      tipe
+
+    Can.Outgoing tipe ->
+      tipe
+
+
+fromSrcType :: T.Canonical -> IO Type
+fromSrcType tipe =
+  fst <$> fromFlexSrcType tipe
+
+
+command :: ModuleName.Canonical -> N.Name -> IO Type
+command home typeName =
+  fromSrcType $
+    T.Lambda
+      (T.Type home typeName [T.Var "msg"])
+      (T.Type ModuleName.cmd N.cmd [T.Var "msg"])
+
+
+subscription :: ModuleName.Canonical -> N.Name -> IO Type
+subscription home typeName =
+  fromSrcType $
+    T.Lambda
+      (T.Type home typeName [T.Var "msg"])
+      (T.Type ModuleName.sub N.sub [T.Var "msg"])
+
+
+
+-- CONSTRAINTS FOR EFFECT MANAGER STUFF
+
+
+constrainEffects :: R.Region -> R.Region -> R.Region -> (Variable -> Type -> Type) -> IO Constraint
+constrainEffects r0 r1 r2 toFinalArgs =
   let
     task t =
-      AppN Var.task [ never, VarN t ]
+      AppN ModuleName.platform N.task [ never, VarN t ]
 
     router msg selfMsg =
-      AppN Var.router [ VarN msg, VarN selfMsg ]
+      AppN ModuleName.platform N.router [ VarN msg, VarN selfMsg ]
   in
-    do  v0 <- mkFlexVar
-        v1 <- mkFlexVar
-        v2 <- mkFlexVar
+  do  vars@[v0,v1,v2,s0,s1,s2,msg1,msg2,selfMsg1,selfMsg2] <- replicateM 10 mkFlexVar
 
-        state0 <- mkFlexVar
-        state1 <- mkFlexVar
-        state2 <- mkFlexVar
+      let
+        onEffectsType = router msg1 selfMsg1 ==> toFinalArgs msg1 (VarN s1 ==> task s1)
+        onSelfMsgType = router msg2 selfMsg2 ==> VarN selfMsg2 ==> VarN s2 ==> task s2
+        constrains =
+          [ CInstance r0 "init" (VarN v0)
+          , CInstance r1 "onEffects" (VarN v1)
+          , CInstance r2 "onSelfMsg" (VarN v2)
+          , CEqual (Error.Manager "init") r0 (VarN v0) (task s0)
+          , CEqual (Error.Manager "onEffects") r1 (VarN v1) onEffectsType
+          , CEqual (Error.Manager "onSelfMsg") r2 (VarN v2) onSelfMsgType
+          , CEqual (Error.State "onEffects") r1 (VarN s0) (VarN s1)
+          , CEqual (Error.State "onSelfMsg") r2 (VarN s0) (VarN s2)
+          , CEqual Error.SelfMsg r2 (VarN selfMsg1) (VarN selfMsg2)
+          ]
 
-        msg1 <- mkFlexVar
-        msg2 <- mkFlexVar
-
-        selfMsg1 <- mkFlexVar
-        selfMsg2 <- mkFlexVar
-
-        let
-          vars =
-            [v0, v1, v2, state0, state1, state2, msg1, msg2, selfMsg1, selfMsg2]
-
-          onEffectsType =
-            router msg1 selfMsg1
-            ==> addEffectArgs moduleName managerType msg1
-                  (VarN state1 ==> task state1)
-
-          onSelfMsgType =
-            router msg2 selfMsg2 ==> VarN selfMsg2 ==> VarN state2 ==> task state2
-
-          constrains =
-            [ CInstance tagRegion "init" (VarN v0)
-            , CInstance tagRegion "onEffects" (VarN v1)
-            , CInstance tagRegion "onSelfMsg" (VarN v2)
-            , CEqual (Error.Manager "init") r0 (VarN v0) (task state0)
-            , CEqual (Error.Manager "onEffects") r1 (VarN v1) onEffectsType
-            , CEqual (Error.Manager "onSelfMsg") r2 (VarN v2) onSelfMsgType
-            , CEqual (Error.State "onEffects") r1 (VarN state0) (VarN state1)
-            , CEqual (Error.State "onSelfMsg") r2 (VarN state0) (VarN state2)
-            , CEqual Error.SelfMsg r2 (VarN selfMsg1) (VarN selfMsg2)
-            ]
-
-        return $
-          CLet [Scheme [] vars (CAnd constrains) Map.empty] CSaveEnv
+      return $ CLet [ Scheme [] vars (CAnd constrains) Map.empty ] CSaveEnv
 
 
-addEffectArgs
-  :: ModuleName.Canonical
-  -> Effects.RawManagerType
-  -> Variable
-  -> Type
-  -> Type
-addEffectArgs moduleName managerType msg result =
-  let
-    toVar (A.A _ name) =
-      Var.fromModule moduleName name
+{-# NOINLINE never #-}
+never :: Type
+never =
+  AppN ModuleName.basics "Never" []
 
-    effectList fxName =
-      AppN Var.list [AppN (toVar fxName) [VarN msg]]
-  in
-    case managerType of
-      Effects.CmdManager cmd ->
-        effectList cmd ==> result
 
-      Effects.SubManager sub ->
-        effectList sub ==> result
-
-      Effects.FxManager cmd sub ->
-        effectList cmd ==> effectList sub ==> result
-
+effectList :: ModuleName.Canonical -> N.Name -> Variable -> Type
+effectList home name msg =
+  AppN ModuleName.list N.list [AppN home name [VarN msg]]
