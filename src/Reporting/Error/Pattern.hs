077322c887c1ba7242853cdfc70029a17741a22f
Evan Czaplicki <evancz@users.noreply.github.com>
2018-01-15 16:00:39 -0800

    Rethink the error messages for exhaustiveness checks
    For example, when there is a pattern match in a function arg, but the
    type has other options it is called an “UNSAFE PATTERN” rather than a
    “PARTIAL PATTERN”
    
    I think the overall sum of changes like this is pretty nice.

--- src/Reporting/Error/Pattern.hs
+++ src/Reporting/Error/Pattern.hs
@@ -1,99 +1,194 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Reporting.Error.Pattern where
+module Reporting.Error.Pattern
+  ( P.Error(..)
+  , toReport
+  )
+  where
 
-import Data.Text (Text)
-import qualified Data.Text as Text
+import qualified Data.List as List
 
-import qualified Nitpick.Pattern as Pattern
-import qualified Reporting.Render.Type as RenderType
-import qualified Reporting.Region as Region
+import qualified Nitpick.PatternMatches as P
 import qualified Reporting.Report as Report
-import qualified Reporting.Helpers as Help
-import Reporting.Helpers ((<>), text)
-
-
-
-data Error
-    = Incomplete Origin [Pattern.Pattern]
-    | Redundant Region.Region Int
-
-
-data Origin
-    = Arg
-    | LetBound
-    | Case
+import qualified Reporting.Render.Code as Code
+import qualified Reporting.Helpers as H
+import Reporting.Helpers ((<>))
 
 
 
 -- TO REPORT
 
 
-toReport :: RenderType.Localizer -> Error -> Report.Report
-toReport _localizer err =
+toReport :: Code.Source -> P.Error -> Report.Report
+toReport source err =
   case err of
-    Incomplete Arg unhandled ->
-        Report.report
-          "PARTIAL PATTERN"
-          Nothing
-          "This pattern does not cover all possible inputs."
-          (text (unhandledError unhandled toCaseMessage))
+    P.Redundant caseRegion patternRegion index ->
+      Report.Report "REDUNDANT PATTERN" patternRegion [] $
+        Report.toCodeSnippet source caseRegion (Just patternRegion)
+          (
+            H.reflow $
+              "The " <> H.ordinalize index <> " pattern is redundant:"
+          ,
+            H.reflow $
+              "Any value with this shape will be handled by a previous\
+              \ pattern, so it should be removed."
+          )
 
-    Incomplete LetBound unhandled ->
-        Report.report
-          "PARTIAL PATTERN"
-          Nothing
-          "The pattern used here does not cover all possible values."
-          (text (unhandledError unhandled toCaseMessage))
+    P.Incomplete region context unhandled ->
+      case context of
+        P.BadArg ->
+          Report.Report "UNSAFE PATTERN" region [] $
+            Report.toCodeSnippet source region Nothing
+              (
+                "This pattern does not cover all possiblities:"
+              ,
+                H.stack
+                  [ "Other possibilities include:"
+                  , unhandledPatternsToDocBlock unhandled
+                  , H.reflow $
+                      "I would have to crash if I saw one of those! So rather than\
+                      \ pattern matching in function arguments, put a `case` in\
+                      \ the function body to account for all possibilities."
+                  ]
+              )
 
-    Incomplete Case unhandled ->
-        Report.report
-          "MISSING PATTERNS"
-          Nothing
-          "This `case` does not have branches for all possibilities."
-          (text (unhandledError unhandled (missingBranchesMessage (length unhandled))))
+        P.BadDestruct ->
+          Report.Report "UNSAFE PATTERN" region [] $
+            Report.toCodeSnippet source region Nothing
+              (
+                "This pattern does not cover all possible values:"
+              ,
+                H.stack
+                  [ "Other possibilities include:"
+                  , unhandledPatternsToDocBlock unhandled
+                  , H.reflow $
+                      "I would have to crash if I saw one of those! You can use\
+                      \ `let` to deconstruct values only if there is ONE possiblity.\
+                      \ Switch to a `case` expression to account for all possibilities."
+                  , H.toSimpleHint $
+                      "Are you calling a function that definitely returns values\
+                      \ with a very specific shape? Try making the return type of\
+                      \ that function more specific!"
+                  ]
+              )
 
-    Redundant region index ->
-        Report.report
-          "REDUNDANT PATTERN"
-          (Just region)
-          ("The " <> Help.ordinalize index <> " pattern is redundant. Remove it.")
-          (text "Any value with this shape will be handled by a previous pattern.")
-
-
-unhandledError :: [Pattern.Pattern] -> Text -> Text
-unhandledError unhandledPatterns relevantMessage =
-  let
-    (visiblePatterns, rest) =
-      splitAt 4 unhandledPatterns
-
-    patternList =
-        map Pattern.toText visiblePatterns
-        ++ if null rest then [] else ["..."]
-
-    noun =
-      if length unhandledPatterns == 1 then "pattern" else "patterns"
-  in
-    "You need to account for the following " <> noun <> ":\n"
-    <> Text.concat (map ("\n    " <>) patternList) <> "\n"
-    <> "\n"
-    <> relevantMessage <> "\n"
-    <> "\n"
-    <> "If you are seeing this error for the first time, check out these hints:\n"
-    <> Help.hintLink "missing-patterns" <> "\n"
-    <> "The recommendations about wildcard patterns and `Debug.crash` are important!"
-
-
-toCaseMessage :: Text
-toCaseMessage =
-  "Switch to a `case` expression to handle all possible patterns."
+        P.BadCase ->
+          Report.Report "MISSING PATTERNS" region [] $
+            Report.toCodeSnippet source region Nothing
+              (
+                "This `case` does not have branches for all possibilities:"
+              ,
+                H.stack
+                  [ "Missing possibilities include:"
+                  , unhandledPatternsToDocBlock unhandled
+                  , H.reflow $
+                      "I would have to crash if I saw one of those. Add branches for them!"
+                  , H.link "Hint"
+                      "If you want to write the code for each branch later, use `Debug.crash` as a placeholder. Read"
+                      "missing-patterns"
+                      "for more guidance on this workflow."
+                  ]
+              )
 
 
-missingBranchesMessage :: Int -> Text
-missingBranchesMessage numUnhandled =
-  if numUnhandled == 1 then
-    "Add a branch to cover this pattern!"
 
-  else
-    "Add branches to cover each of these patterns!"
+-- PATTERN TO DOC
 
+
+unhandledPatternsToDocBlock :: [P.Pattern] -> H.Doc
+unhandledPatternsToDocBlock unhandledPatterns =
+  H.indent 4 $ H.dullyellow $ H.vcat $
+    map (patternToDoc Unambiguous) unhandledPatterns
+
+
+data Context
+  = Arg
+  | Head
+  | Unambiguous
+  deriving (Eq)
+
+
+patternToDoc :: Context -> P.Pattern -> H.Doc
+patternToDoc context pattern =
+  case delist pattern [] of
+    NonList P.Anything ->
+      "_"
+
+    NonList (P.Literal literal) ->
+      case literal of
+        P.Chr chr ->
+          H.textToDoc ("'" <> chr <> "'")
+
+        P.Str str ->
+          H.textToDoc ("\"" <> str <> "\"")
+
+        P.Int int ->
+          H.text (show int)
+
+    NonList (P.Ctor _ "#0" []) ->
+      "()"
+
+    NonList (P.Ctor _ "#2" [a,b]) ->
+      "( " <> patternToDoc Unambiguous a <>
+      ", " <> patternToDoc Unambiguous b <>
+      " )"
+
+    NonList (P.Ctor _ "#3" [a,b,c]) ->
+      "( " <> patternToDoc Unambiguous a <>
+      ", " <> patternToDoc Unambiguous b <>
+      ", " <> patternToDoc Unambiguous c <>
+      " )"
+
+    NonList (P.Ctor _ name args) ->
+      let
+        ctorDoc =
+          H.hsep (H.nameToDoc name : map (patternToDoc Arg) args)
+      in
+      if context == Arg && length args > 0 then
+        "(" <> ctorDoc <> ")"
+      else
+        ctorDoc
+
+    FiniteList [] ->
+      "[]"
+
+    FiniteList entries ->
+      let entryDocs = map (patternToDoc Unambiguous) entries in
+      "[" <> H.hcat (List.intersperse "," entryDocs) <> "]"
+
+    Conses conses finalPattern ->
+      let
+        consDoc =
+          foldr
+            (\hd tl -> patternToDoc Head hd <> " :: " <> tl)
+            (patternToDoc Unambiguous finalPattern)
+            conses
+      in
+      if context == Unambiguous then
+        consDoc
+      else
+        "(" <> consDoc <> ")"
+
+
+data Structure
+  = FiniteList [P.Pattern]
+  | Conses [P.Pattern] P.Pattern
+  | NonList P.Pattern
+
+
+delist :: P.Pattern -> [P.Pattern] -> Structure
+delist pattern revEntries =
+  case pattern of
+    P.Ctor _ "[]" [] ->
+      FiniteList revEntries
+
+    P.Ctor _ "::" [hd,tl] ->
+      delist tl (hd:revEntries)
+
+    _ ->
+      case revEntries of
+        [] ->
+          NonList pattern
+
+        _ ->
+          Conses (reverse revEntries) pattern
