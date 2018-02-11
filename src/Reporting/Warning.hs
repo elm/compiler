7ae440051122930a022a8365ffea20f8078c20cc
Evan Czaplicki <evancz@users.noreply.github.com>
2018-02-10 18:34:07 -0800

    Update warnings
    Drop JSON format for now. Can rebuild later on.

--- src/Reporting/Warning.hs
+++ src/Reporting/Warning.hs
@@ -4,22 +4,20 @@ module Reporting.Warning
   ( Warning(..)
   , Context(..)
   , toReport
-  , toJson
   )
   where
 
-import Data.Aeson ((.=))
-import qualified Data.Aeson as Json
-import Data.Text (Text)
+
+import Data.Monoid ((<>))
 
 import qualified AST.Canonical as Can
+import qualified AST.Utils.Type as Type
 import qualified Elm.Name as N
-import qualified Reporting.Annotation as A
 import qualified Reporting.Region as R
 import qualified Reporting.Report as Report
-import qualified Reporting.Render.Type as RenderType
-import qualified Reporting.Helpers as Help
-import Reporting.Helpers ((<>), text)
+import qualified Reporting.Render.Code as Code
+import qualified Reporting.Render.Type as RT
+import qualified Reporting.Helpers as H
 
 
 
@@ -27,9 +25,9 @@ import Reporting.Helpers ((<>), text)
 
 
 data Warning
-  = UnusedImport N.Name
+  = UnusedImport R.Region N.Name
   | UnusedVariable R.Region Context N.Name
-  | MissingTypeAnnotation Text Can.Type
+  | MissingTypeAnnotation R.Region N.Name Can.Type
 
 
 data Context = Def | Pattern
@@ -39,46 +37,62 @@ data Context = Def | Pattern
 -- TO REPORT
 
 
-toReport :: RenderType.Localizer -> Warning -> Report.Report
-toReport localizer warning =
+toReport :: Code.Source -> Warning -> Report.Report
+toReport source warning =
   case warning of
-    UnusedImport moduleName ->
-      Report.report
-        "unused import"
-        Nothing
-        ("Nothing from the `" <> moduleName <> "` module is used in this file.")
-        (text "I recommend removing unused imports.")
+    UnusedImport region moduleName ->
+      Report.Report "unused import" region [] $
+        Report.toCodeSnippet source region Nothing
+          (
+            H.reflow $
+              "Nothing from the `" <> N.toString moduleName <> "` module is used in this file."
+          ,
+            "I recommend removing unused imports."
+          )
 
-    UnusedVariable _ context name ->
-      Report.report
-        (defOrPat context "unused definition" "unused variable")
-        Nothing
-        ("You are not using `" <> name <> "` anywhere.")
-        ( Help.stack
-            [ Help.reflowParagraph $
-                "Is there a typo? Maybe you intended to use `" <> name
-                <> "` but typed a similar name instead?"
-            , Help.reflowParagraph $
-                defOrPat context
-                  ( "If you are sure there is no typo, remove the definition.\
-                    \ This way future readers will not have to wonder!"
-                  )
-                  ( "If you are sure there is no typo, replace `" <> name
-                    <> "` with _ so future readers will not have to wonder!"
-                  )
-            ]
-        )
-
-    MissingTypeAnnotation name inferredType ->
-        Report.report
-          "missing type annotation"
-          Nothing
-          ("Top-level value " <> Help.functionName name <> " does not have a type annotation.")
-          ( Help.stack
-              [ text "I inferred the type annotation so you can copy it into your code:"
-              , RenderType.annotation localizer name inferredType
+    UnusedVariable region context name ->
+      let title = defOrPat context "unused definition" "unused variable" in
+      Report.Report title region [] $
+        Report.toCodeSnippet source region Nothing
+          (
+            H.reflow $
+              "You are not using `" <> N.toString name <> "` anywhere."
+          ,
+            H.stack
+              [ H.reflow $
+                  "Is there a typo? Maybe you intended to use `" <> N.toString name
+                  <> "` somewhere but typed another name instead?"
+              , H.reflow $
+                  defOrPat context
+                    ( "If you are sure there is no typo, remove the definition.\
+                      \ This way future readers will not have to wonder why it is there!"
+                    )
+                    ( "If you are sure there is no typo, replace `" <> N.toString name
+                      <> "` with _ so future readers will not have to wonder why it is there!"
+                    )
               ]
           )
+
+    MissingTypeAnnotation region name inferredType ->
+        Report.Report "missing type annotation" region [] $
+          Report.toCodeSnippet source region Nothing
+            (
+              H.reflow $
+                case Type.deepDealias inferredType of
+                  Can.TLambda _ _ ->
+                    "The `" <> N.toString name <> "` function has no type annotation."
+
+                  _ ->
+                    "The `" <> N.toString name <> "` definition has no type annotation."
+            ,
+              H.stack
+                [ "I inferred the type annotation myself though! You can copy it into your code:"
+                , H.green $ H.hang 4 $ H.sep $
+                    [ H.nameToDoc name <> " :"
+                    , RT.canToDoc RT.None inferredType
+                    ]
+                ]
+            )
 
 
 defOrPat :: Context -> a -> a -> a
@@ -87,20 +101,3 @@ defOrPat context def pat =
     Def -> def
     Pattern -> pat
 
-
-
--- TO JSON
-
-
-toJson :: RenderType.Localizer -> FilePath -> A.Located Warning -> Json.Value
-toJson localizer filePath (A.At region warning) =
-  let
-    (maybeRegion, additionalFields) =
-        Report.toJson [] (toReport localizer warning)
-  in
-      Json.object $
-        [ "file" .= filePath
-        , "region" .= maybe region id maybeRegion
-        , "type" .= ("warning" :: Text)
-        ]
-        ++ additionalFields
