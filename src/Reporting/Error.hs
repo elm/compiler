e63ef232ece35920f2940869e2f4ed08982739d7
Evan Czaplicki <evancz@users.noreply.github.com>
2018-02-10 18:34:47 -0800

    Update error manager
    Drop JSON stuff for now. Can be added back, possibly in a better way.

--- src/Reporting/Error.hs
+++ src/Reporting/Error.hs
@@ -1,17 +1,20 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Reporting.Error where
+module Reporting.Error
+  ( Error(..)
+  , toReports
+  )
+  where
 
-import Data.Aeson ((.=))
-import qualified Data.Aeson as Json
 
-import qualified Reporting.Annotation as A
+import qualified Type.Error
 import qualified Reporting.Error.Canonicalize as Canonicalize
 import qualified Reporting.Error.Docs as Docs
+import qualified Reporting.Error.Main as Main
 import qualified Reporting.Error.Pattern as Pattern
 import qualified Reporting.Error.Syntax as Syntax
 import qualified Reporting.Error.Type as Type
-import qualified Reporting.Render.Type as RenderType
+import qualified Reporting.Render.Code as Code
 import qualified Reporting.Report as Report
 
 
@@ -20,70 +23,35 @@ import qualified Reporting.Report as Report
 
 
 data Error
-    = Syntax Syntax.Error
-    | Canonicalize Canonicalize.Error
-    | Type Type.Error
-    | Pattern Pattern.Error
-    | Docs Docs.Error
+  = Syntax Syntax.Error
+  | Canonicalize Canonicalize.Error
+  | Type [Type.Error]
+  | Main Main.Error
+  | Pattern [Pattern.Error]
+  | Docs Docs.Error
 
 
 
 -- TO REPORT
 
 
-toReport :: RenderType.Localizer -> Error -> Report.Report
-toReport localizer err =
+toReports :: Code.Source -> Type.Error.Localizer -> Error -> [Report.Report]
+toReports source localizer err =
   case err of
     Syntax syntaxError ->
-        Syntax.toReport localizer syntaxError
+        [Syntax.toReport source syntaxError]
 
     Canonicalize canonicalizeError ->
-        Canonicalize.toReport localizer canonicalizeError
+        [Canonicalize.toReport source canonicalizeError]
 
-    Type typeError ->
-        Type.toReport localizer typeError
+    Type typeErrors ->
+        map (Type.toReport source localizer) typeErrors
 
-    Pattern patternError ->
-        Pattern.toReport localizer patternError
+    Main mainError ->
+        [Main.toReport source mainError]
+
+    Pattern patternErrors ->
+        map (Pattern.toReport source) patternErrors
 
     Docs docsError ->
-        Docs.toReport docsError
-
-
-
--- TO JSON
-
-
-toJson :: RenderType.Localizer -> FilePath -> A.Located Error -> Json.Value
-toJson localizer filePath (A.A region err) =
-  let
-    (maybeRegion, additionalFields) =
-        case err of
-          Syntax syntaxError ->
-              Report.toJson [] (Syntax.toReport localizer syntaxError)
-
-          Canonicalize canonicalizeError ->
-              let
-                suggestions =
-                  maybe []
-                      (\s -> ["suggestions" .= s])
-                      (Canonicalize.extractSuggestions canonicalizeError)
-              in
-                Report.toJson suggestions (Canonicalize.toReport localizer canonicalizeError)
-
-          Type typeError ->
-              Report.toJson [] (Type.toReport localizer typeError)
-
-          Pattern patternError ->
-              Report.toJson [] (Pattern.toReport localizer patternError)
-
-          Docs docsError ->
-              Report.toJson [] (Docs.toReport docsError)
-  in
-      Json.object $
-        [ "file" .= filePath
-        , "region" .= region
-        , "subregion" .= maybeRegion
-        , "type" .= ("error" :: String)
-        ]
-        ++ additionalFields
+        [Docs.toReport source docsError]
