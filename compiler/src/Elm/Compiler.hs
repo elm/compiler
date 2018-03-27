f9540d6ef0c4b1a7986efcce0a654c3b4c78def3
Evan Czaplicki <evancz@users.noreply.github.com>
2018-03-27 18:02:50 +0200

    Provide way to turn errors to JSON

--- compiler/src/Elm/Compiler.hs
+++ compiler/src/Elm/Compiler.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Elm.Compiler
   ( version
   , Context(..)
@@ -6,6 +6,7 @@ module Elm.Compiler
   , compile
   , Error.Error
   , errorsToDoc
+  , errorsToJson
   , Warning.Warning
   )
   where
@@ -14,14 +15,17 @@ module Elm.Compiler
 import qualified Data.ByteString as BS
 import qualified Data.Map as Map
 import qualified Data.Text as Text
+import qualified Text.PrettyPrint.ANSI.Leijen as P
 
 import qualified Compile
 import qualified Elm.Compiler.Module as M
 import qualified Elm.Compiler.Version
 import qualified Elm.Package as Pkg
+import qualified Json.Encode as Encode
 import qualified Reporting.Error as Error
 import qualified Reporting.Helpers as H
 import qualified Reporting.Render.Code as Code
+import qualified Reporting.Region as Region
 import qualified Reporting.Report as Report
 import qualified Reporting.Result as Result
 import qualified Reporting.Warning as Warning
@@ -59,12 +63,42 @@ compile (Context pkg exposed importDict interfaces) source =
 
 
 
--- ERRORS
+-- ERRORS TO DOC
 
 
 errorsToDoc :: FilePath -> Text.Text -> [Error.Error] -> H.Doc
 errorsToDoc filePath source errors =
-  H.vcat $
-    map
-      (Report.toDoc filePath)
-      (concatMap (Error.toReports (Code.toSource source) Map.empty) errors)
+  let
+    reports =
+      concatMap (Error.toReports (Code.toSource source) Map.empty) errors
+  in
+  H.vcat $ map (Report.toDoc filePath) reports
+
+
+
+-- ERRORS TO JSON
+
+
+errorsToJson :: FilePath -> Text.Text -> [Error.Error] -> Encode.Value
+errorsToJson filePath source errors =
+  let
+    reports =
+      concatMap (Error.toReports (Code.toSource source) Map.empty) errors
+  in
+  Encode.object
+    [ ("path", Encode.text (Text.pack filePath))
+    , ("errors", Encode.array (map reportToJson reports))
+    ]
+
+
+reportToJson :: Report.Report -> Encode.Value
+reportToJson (Report.Report title region _sgstns message) =
+  let
+    messageString =
+      P.displayS (P.renderPretty 1 80 message) ""
+  in
+  Encode.object
+    [ ("title", Encode.text (Text.pack title))
+    , ("region", Region.encode region)
+    , ("message", Encode.text (Text.pack messageString))
+    ]
