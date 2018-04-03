6a96e212017297c082e68a3de6748c007aeaef37
Evan Czaplicki <evancz@users.noreply.github.com>
2018-04-03 18:54:37 +0200

    Clarify that --docs is best for packages
    It now triggers the --optimize checks about Debug, so it will be even
    harder to use with application code.

--- ui/terminal/src/Main.hs
+++ ui/terminal/src/Main.hs
@@ -176,7 +176,7 @@ make =
         |-- onOff "optimize" "Turn on optimizations to make code smaller and faster. For example, the compiler renames record fields to be as short as possible and unboxes values to reduce allocation."
         |-- flag "output" Output.output "Specify the name of the resulting JS file. For example --output=assets/elm.js to generate the JS at assets/elm.js or --output=/dev/null to generate no output at all!"
         |-- flag "report" Make.reportType "You can say --report=json to get error messages as JSON. This is only really useful if you are an editor plugin. Humans should avoid it!"
-        |-- flag "docs" Make.docsFile "Generate a JSON file of documentation for your project. Eventually it will be possible to preview docs with `reactor` because it is quite hard to deal with these JSON files directly."
+        |-- flag "docs" Make.docsFile "Generate a JSON file of documentation for a package. Eventually it will be possible to preview docs with `reactor` because it is quite hard to deal with these JSON files directly."
   in
   Interface "make" (Common summary) details example (zeroOrMore elmFile) makeFlags Make.run
 
