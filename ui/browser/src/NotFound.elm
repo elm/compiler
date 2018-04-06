9faeae1fba12a5973c170be614a4f870645641e6
Evan Czaplicki <evancz@users.noreply.github.com>
2018-04-06 16:50:01 +0200

    Get the 404 page working as expected

--- ui/browser/src/NotFound.elm
+++ ui/browser/src/NotFound.elm
@@ -1,14 +1,29 @@
 module NotFound exposing (main)
 
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
 
-main : Html msg
+main : Program () () ()
 main =
-  div [ class "not-found" ]
-    [ div [ style "font-size" "12em" ] [ text "404" ]
-    , div [ style "font-size" "3em" ] [ text "Page not found" ]
-    ]
+  Browser.fullscreen
+    { init = \_ -> ((), Cmd.none)
+    , update = \_ _ -> ((), Cmd.none)
+    , subscriptions = \_ -> Sub.none
+    , onNavigation = Nothing
+    , view = \_ -> page
+    }
+
+
+page =
+  { title = "Page not found"
+  , body =
+      [ div [ class "not-found" ]
+          [ div [ style "font-size" "12em" ] [ text "404" ]
+          , div [ style "font-size" "3em" ] [ text "Page not found" ]
+          ]
+      ]
+  }
