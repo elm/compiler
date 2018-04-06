16d6dc008c49e73cf77891adb4226c4eb7e48499
Evan Czaplicki <evancz@users.noreply.github.com>
2018-04-06 16:26:11 +0200

    Make the sandwich generation simpler

--- builder/src/Generate/Html.hs
+++ builder/src/Generate/Html.hs
@@ -19,43 +19,35 @@ import qualified Elm.Name as N
 
 sandwich :: N.Name -> B.Builder -> B.Builder
 sandwich moduleName javascript =
-  header <> javascript <> footer (N.toBuilder moduleName)
-
-
-header :: B.Builder
-header = [r|
-<!DOCTYPE HTML>
+  let name = N.toBuilder moduleName in
+  [r|<!DOCTYPE HTML>
 <html>
 <head>
   <meta charset="UTF-8">
+  <title>|] <> name <> [r|</title>
 </head>
 
 <body>
 <div id="embed"></div>
 <script>
-|]
+|] <> javascript <> [r|
 
-
-footer :: B.Builder -> B.Builder
-footer name =
-  "</script>\n\
-  \<script>\n\
-  \// It is too complicated to figure out which kind of Program you created\n\
-  \// based on only the code. So here we check for different possibilities.\n\
-  \//\n\
-  \// When you embed compiled Elm into HTML yourself, you should not copy/paste\n\
-  \// from here. Do just the part that makes sense for your application!\n\
-  \//\n\
-  \var app =\n\
-  \    Elm." <> name <> ".fullscreen\n\
-  \        ? Elm." <> name <> ".fullscreen()\n\
-  \        :\n\
-  \    Elm." <> name <> ".embed\n\
-  \        ? Elm." <> name <> ".embed(document.getElementById('embed'))\n\
-  \        :\n\
-  \    Elm." <> name <> ".worker\n\
-  \        ? (document.getElementById('embed').innerText = 'This is a headless program, meaning there is nothing to show here. I started the program anyway though, and you can access it as `app` in the developer console.', Elm." <> name <> ".worker())\n\
-  \        : (document.getElementById('embed').innerText = 'I cannot figure out what kind of program this is supposed to be. Perhaps someone on Slack can help?', undefined);\n\
-  \</script>\n\
-  \</body>\n\
-  \</html>"
+// It is too complicated to figure out which kind of Program you created
+// based on only the code. So here we check for different possibilities.
+//
+// When you embed compiled Elm into HTML yourself, you should not copy/paste
+// from here. Do just the part that makes sense for your application!
+//
+var app =
+    Elm.|] <> name <> [r|.fullscreen
+        ? Elm.|] <> name <> [r|.fullscreen()
+        :
+    Elm.|] <> name <> [r|.embed
+        ? Elm.|] <> name <> [r|.embed(document.getElementById('embed'))
+        :
+    Elm.|] <> name <> [r|.worker
+        ? (document.getElementById('embed').innerText = 'This is a headless program, meaning there is nothing to show here. I started the program anyway though, and you can access it as `app` in the developer console.', Elm.|] <> name <> [r|.worker())
+        : (document.getElementById('embed').innerText = 'I cannot figure out what kind of program this is supposed to be. Perhaps someone on Slack can help?', undefined);
+</script>
+</body>
+</html>|]
