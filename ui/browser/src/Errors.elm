0ca686481b3b915ae977b02c6f4b012f6f9a3078
Evan Czaplicki <evancz@users.noreply.github.com>
2018-04-08 14:24:45 +0200

    Take first pass at displaying error messages with colors
    Should look just like the terminal. Need to fix the colors though!

--- ui/browser/src/Errors.elm
+++ ui/browser/src/Errors.elm
@@ -7,16 +7,16 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import String
 import Json.Decode as D
+import Elm.Error as Error
 
 
 
 -- MAIN
 
 
-main : Program D.Value Model ()
 main =
   Browser.fullscreen
-    { init = \env -> (D.decodeValue decodeExit env.flags, Cmd.none)
+    { init = \env -> (D.decodeValue Error.decoder env.flags, Cmd.none)
     , update = \_ exit -> (exit, Cmd.none)
     , view = view
     , subscriptions = \_ -> Sub.none
@@ -25,26 +25,10 @@ main =
 
 
 
--- MODEL
-
-
-type alias Model =
-  Result D.Error Exit
-
-
-type alias Exit = ()
-
-
-decodeExit : D.Decoder Exit
-decodeExit =
-  D.null ()
-
-
-
 -- VIEW
 
 
-view : Model -> Browser.View msg
+view : Result D.Error Error.Error -> Browser.View msg
 view result =
   { title = "Problem!"
   , body =
@@ -52,13 +36,13 @@ view result =
         Err err ->
           [ text (D.errorToString err) ]
 
-        Ok exit ->
-          [ viewExit exit ]
+        Ok error ->
+          [ viewError error ]
   }
 
 
-viewExit : Exit -> Html msg
-viewExit exit =
+viewError : Error.Error -> Html msg
+viewError error =
   div
     [ style "width" "100%"
     , style "min-height" "100%"
@@ -71,10 +55,114 @@ viewExit exit =
     ]
     [ div
         [ style "display" "block"
-        , style "white-space" "pre"
+        , style "white-space" "pre-wrap"
         , style "background-color" "rgb(39, 40, 34)"
         , style "padding" "2em"
         ]
-        [ text "TODO"
-        ]
+        (viewErrorHelp error)
     ]
+
+
+viewErrorHelp : Error.Error -> List (Html msg)
+viewErrorHelp error =
+  case error of
+    Error.GeneralProblem { path, title, message } ->
+      [ text title ]
+
+    Error.ModuleProblems badModules ->
+      List.map viewBadModule badModules
+
+
+viewBadModule : Error.BadModule -> Html msg
+viewBadModule { path, name, problems } =
+  span [] (List.map (viewProblem path) problems)
+
+
+viewProblem : String -> Error.Problem -> Html msg
+viewProblem filePath problem =
+  let
+    leftover =
+      76 - String.length problem.title
+
+    header =
+      "-- " ++ problem.title ++ " " ++ String.padLeft leftover '-' (" " ++ filePath) ++ "\n\n"
+  in
+  span []
+    ( span [ style "color" "cyan" ] [ text header ]
+      :: viewMessage problem.message
+    )
+
+
+
+-- VIEW MESSAGE
+
+
+viewMessage : List Error.Chunk -> List (Html msg)
+viewMessage chunks =
+  case chunks of
+    [] ->
+      []
+
+    chunk :: others ->
+      let
+        htmlChunk =
+          case chunk of
+            Error.Unstyled string ->
+              text string
+
+            Error.Styled style string ->
+              span (styleToAttrs style) [ text string ]
+      in
+      htmlChunk :: viewMessage others
+
+
+styleToAttrs : Error.Style -> List (Attribute msg)
+styleToAttrs { bold, underline, color } =
+  addBold bold <| addUnderline underline <| addColor color []
+
+
+addBold : Bool -> List (Attribute msg) -> List (Attribute msg)
+addBold bool attrs =
+  if bool then
+    style "font-weight" "bold" :: attrs
+  else
+    attrs
+
+
+addUnderline : Bool -> List (Attribute msg) -> List (Attribute msg)
+addUnderline bool attrs =
+  if bool then
+    style "text-decoration" "underline" :: attrs
+  else
+    attrs
+
+
+addColor : Maybe Error.Color -> List (Attribute msg) -> List (Attribute msg)
+addColor maybeColor attrs =
+  case maybeColor of
+    Nothing ->
+      attrs
+
+    Just color ->
+      style "color" (colorToCss color) :: attrs
+
+
+colorToCss : Error.Color -> String
+colorToCss color =
+  case color of
+    Error.Red -> "red"
+    Error.RED -> "red"
+    Error.Magenta -> "magenta"
+    Error.MAGENTA -> "magenta"
+    Error.Yellow -> "yellow"
+    Error.YELLOW -> "yellow"
+    Error.Green -> "green"
+    Error.GREEN -> "green"
+    Error.Cyan -> "cyan"
+    Error.CYAN -> "cyan"
+    Error.Blue -> "blue"
+    Error.BLUE -> "blue"
+    Error.White -> "white"
+    Error.WHITE -> "white"
+    Error.Black -> "black"
+    Error.BLACK -> "black"
