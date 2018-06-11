5ee7773572cc1f0389dd2f6ee62e19ad179363bf
Evan Czaplicki <evancz@users.noreply.github.com>
2018-06-10 21:20:25 -0500

    Make the error about bad deps more generic / talk about reactor

--- builder/src/Reporting/Exit/Deps.hs
+++ builder/src/Reporting/Exit/Deps.hs
@@ -99,15 +99,16 @@ toReport exit =
         ]
 
     BadDeps ->
-      Help.report "CLASHING PACKAGE DEPENDENCIES" (Just "elm.json")
+      Help.report "INVALID PACKAGE DEPENDENCIES" (Just "elm.json")
         "The dependencies in your elm.json are not compatible."
-        [ D.reflow $
-            "Did you change them by hand? Try to change it back! It is much\
-            \ better to add dependencies with commands like this:"
-        , D.indent 4 $ D.dullyellow "elm install elm/http"
+        [ D.fillSep
+            ["Did","you","change","them","by","hand?","Try","to","change","it","back!"
+            ,"It","is","much","better","to","add","dependencies","with",D.green "elm install"
+            ,"or","the","dependency","management","tool","in",D.green "elm reactor" <> "."
+            ]
         , D.reflow $
             "Please ask for help on the Elm slack <http://elmlang.herokuapp.com/> if\
-            \ you are running into something that seems trickier than this."
+            \ you try those paths and still cannot figure it out!"
         ]
 
     BuildFailure elmHome pkg vsn ->
