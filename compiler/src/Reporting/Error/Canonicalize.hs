b0d8c114d81d3a8436c8725bc3632830a576fc27
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-01-04 09:35:55 +1100

    Fix typo in error message: multilpe -> multiple

--- compiler/src/Reporting/Error/Canonicalize.hs
+++ compiler/src/Reporting/Error/Canonicalize.hs
@@ -243,11 +243,11 @@ toReport source err =
 
     DuplicateAliasArg typeName name r1 r2 ->
       nameClash source r1 r2 $
-        "The `" <> N.toString typeName <> "` type alias has multilpe `" <> N.toString name <> "` type variables."
+        "The `" <> N.toString typeName <> "` type alias has multiple `" <> N.toString name <> "` type variables."
 
     DuplicateUnionArg typeName name r1 r2 ->
       nameClash source r1 r2 $
-        "The `" <> N.toString typeName <> "` type has multilpe `" <> N.toString name <> "` type variables."
+        "The `" <> N.toString typeName <> "` type has multiple `" <> N.toString name <> "` type variables."
 
     DuplicatePattern context name r1 r2 ->
       nameClash source r1 r2 $
