d973fc902c34bdd8a06f2db608762fa4181af547
Dave Tapley <dukedave@gmail.com>
2019-01-28 16:51:51 -0700

    Fix help example for elm diff in application
    The package (elm/html) was updated in a44fcf164e1858d812d91fd9323ce5a6df1f2947,
    but no such versions (5.1.1 nor 6.0.0) exist for it.
    
    Switched to elm/json because it has a minor version available.

--- builder/src/Reporting/Exit/Diff.hs
+++ builder/src/Reporting/Exit/Diff.hs
@@ -35,7 +35,7 @@ toReport exit =
       Help.report "CANNOT DIFF APPLICATIONS" Nothing
         "I cannot perform diffs on applications, only packages! If you are\
         \ just curious to see a diff, try running this command:"
-        [ D.indent 4 $ D.green $ "elm diff elm/html 5.1.1 6.0.0"
+        [ D.indent 4 $ D.green $ "elm diff elm/json 1.0.0 1.1.2"
         ]
 
     Unpublished ->
