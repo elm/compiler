58a25d59221aab4f9ce5491c0c007ca40659f417
Evan Czaplicki <evancz@users.noreply.github.com>
2019-02-12 18:38:13 -0500

    Have the Task type do less work / use CPS implementation
    Stop creating an Http.Manager for every single run. That is a
    relatively significant cost on an incremental `elm make` and we'll get
    a decent speed boost in that context if it can be skipped.

--- builder/src/Reporting/Task.hs
+++ builder/src/Reporting/Task.hs
@@ -1,38 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, Rank2Types #-}
 module Reporting.Task
-  ( Task, Task_
-  , try
+  ( Task
   , run
   , throw
-  , mapError
-  , Env
-  , getPackageCacheDir
-  , getPackageCacheDirFor
-  , report
-  , getReporter
-  , getApproval
-  , silently
-  , getSilentRunner
-  , workerChan
-  , runHttp
+  -- progress
+  , notify
+  , ask
+  -- io
+  , io
+  , eio
+  , withKey
   )
   where
 
-import Control.Concurrent (forkIO)
-import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
-import Control.Monad (forever, join, replicateM_)
-import Control.Monad.Except (ExceptT, runExceptT, throwError, withExceptT)
-import qualified Control.Monad.Reader as R
-import Control.Monad.Trans (liftIO)
-import qualified Network.HTTP.Client as Http
-import qualified Network.HTTP.Client.TLS as Http
-import qualified System.Directory as Dir
-import qualified System.Exit as Exit
-import System.FilePath ((</>))
 
-import qualified Elm.Package as Pkg
-import qualified Elm.PerUserCache as PerUserCache
-import qualified Elm.Version as V
+import qualified System.Exit as Exit
+
 import qualified Reporting.Doc as D
 import qualified Reporting.Exit as Exit
 import qualified Reporting.Progress as Progress
@@ -42,148 +25,126 @@ import qualified Reporting.Progress as Progress
 -- TASKS
 
 
-type Task =
-  Task_ Exit.Exit
-
-
-type Task_ e =
-  ExceptT e (R.ReaderT Env IO)
+newtype Task a =
+  Task
+  (
+    forall result.
+      Env
+      -> (a -> IO result)
+      -> (Exit.Exit -> IO result)
+      -> IO result
+  )
 
 
 data Env =
   Env
-    { _cacheDir :: FilePath
-    , _worker :: IO () -> IO ()
-    , _httpManager :: Http.Manager
-    , _tell :: Progress.Progress -> IO ()
-    , _ask :: D.Doc -> IO Bool
+    { _tell :: Progress.Progress -> IO ()
+    , _ask  :: D.Doc -> IO Bool
     }
 
 
-try :: Progress.Reporter -> Task a -> IO (Maybe a)
-try (Progress.Reporter tell ask end) task =
-  do  root <- PerUserCache.getPackageRoot
-      pool <- initPool 4
-      httpManager <- Http.newManager Http.tlsManagerSettings
-      let env = Env root pool httpManager tell ask
-      result <- R.runReaderT (runExceptT task) env
-      case result of
-        Left err ->
-          do  end (Just err)
-              return Nothing
-
-        Right answer ->
-          do  end Nothing
-              return (Just answer)
-
-
 run :: Progress.Reporter -> Task a -> IO ()
-run reporter task =
-  do  maybeAnswer <- try reporter task
-      case maybeAnswer of
-        Nothing -> Exit.exitFailure
-        Just _ -> return ()
+run (Progress.Reporter pTell pAsk pEnd) (Task task) =
+  task (Env pTell pAsk)
+    (\_ ->
+        do  pEnd Nothing
+            return ()
+    )
+    (\exit ->
+        do  pEnd (Just exit)
+            Exit.exitFailure
+    )
 
 
-throw :: e -> Task_ e a
-throw =
-  throwError
-
-
-mapError :: (x -> y) -> Task_ x a -> Task_ y a
-mapError =
-  withExceptT
-
-
-
--- CACHING
-
-
-getPackageCacheDir :: Task_ e FilePath
-getPackageCacheDir =
-  R.asks _cacheDir
-
-
-getPackageCacheDirFor :: Pkg.Name -> V.Version -> Task_ e FilePath
-getPackageCacheDirFor name version =
-  do  cacheDir <- getPackageCacheDir
-      let dir = cacheDir </> Pkg.toFilePath name </> V.toChars version
-      liftIO (Dir.createDirectoryIfMissing True dir)
-      return dir
+throw :: Exit.Exit -> Task a
+throw exit =
+  Task $ \_ _ err -> err exit
 
 
 
--- REPORTER
+-- PROGRESS
 
 
-report :: Progress.Progress -> Task_ e ()
-report progress =
-  do  tell <- R.asks _tell
-      liftIO (tell progress)
+notify :: Progress.Progress -> Task ()
+notify progress =
+  Task $ \(Env pTell _) ok _ -> pTell progress >>= ok
 
 
-getReporter :: Task (Progress.Progress -> IO ())
-getReporter =
-  R.asks _tell
-
-
-
--- APPROVAL
-
-
-getApproval :: D.Doc -> Task_ e Bool
-getApproval doc =
-  do  ask <- R.asks _ask
-      liftIO (ask doc)
+ask :: D.Doc -> Task Bool
+ask question =
+  Task $ \(Env _ pAsk) ok _ -> pAsk question >>= ok
 
 
 
--- RUNNER
+-- IO
 
 
-silently :: Task_ e a -> Task_ e a
-silently task =
-  do  runner <- getSilentRunner
-      result <- liftIO (runner task)
-      either throw return result
+{-# INLINE io #-}
+io :: IO a -> Task a
+io work =
+  Task $ \_ ok _ -> work >>= ok
 
 
-getSilentRunner :: Task_ x (Task_ e a -> IO (Either e a))
-getSilentRunner =
-  do  env <- R.ask
-      let silentEnv = env { _tell = \_ -> return () }
-      return $ \task -> R.runReaderT (runExceptT task) silentEnv
+eio :: (x -> Exit.Exit) -> IO (Either x a) -> Task a
+eio func work =
+  Task $ \_ ok err ->
+    do  result <- work
+        case result of
+          Right a -> ok a
+          Left e -> err (func e)
 
 
-
--- THREAD POOL
-
-
-workerChan :: Chan (Either e a) -> Task_ e a -> Task_ x ()
-workerChan chan task =
-  do  env <- R.ask
-
-      liftIO $ _worker env $
-        do  result <- R.runReaderT (runExceptT task) env
-            writeChan chan result
-
-
-initPool :: Int -> IO (IO () -> IO ())
-initPool size =
-  do  chan <- newChan
-
-      replicateM_ size $ forkIO $ forever $
-        join (readChan chan)
-
-      return $ writeChan chan
+withKey :: (Progress.Key -> IO (Either Exit.Exit a)) -> Task a
+withKey callback =
+  Task $ \(Env pTell _) ok err ->
+    do  result <- callback (Progress.Key pTell)
+        case result of
+          Right a -> ok a
+          Left e -> err e
 
 
 
--- HTTP
+-- INSTANCES
 
 
-runHttp :: (Http.Manager -> (Progress.Progress -> IO ()) -> IO (Either Exit.Exit a)) -> Task a
-runHttp fetcher =
-  do  (Env _ _ manager tell _) <- R.ask
-      result <- liftIO $ fetcher manager tell
-      either throwError return result
+instance Functor Task where
+  {-# INLINE fmap #-}
+  fmap func (Task taskA) =
+    Task $ \env ok err ->
+      let
+        okA arg = ok (func arg)
+      in
+      taskA env okA err
+
+
+instance Applicative Task where
+  {-# INLINE pure #-}
+  pure a =
+    Task $ \_ ok _ -> ok a
+
+  {-# INLINE (<*>) #-}
+  (<*>) (Task taskFunc) (Task taskArg) =
+    Task $ \env ok err ->
+      let
+        okFunc func =
+          let
+            okArg arg = ok (func arg)
+          in
+          taskArg env okArg err
+      in
+      taskFunc env okFunc err
+
+
+instance Monad Task where
+  {-# INLINE return #-}
+  return = pure
+
+  {-# INLINE (>>=) #-}
+  (>>=) (Task taskA) callback =
+    Task $ \env ok err ->
+      let
+        okA a =
+          case callback a of
+            Task taskB -> taskB env ok err
+      in
+      taskA env okA err
