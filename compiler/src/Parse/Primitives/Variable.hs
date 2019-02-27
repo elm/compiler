6d8a50e32ce4fd9daf3c6db08a1a69aadbd85226
radrow <radrowicki@gmail.com>
2019-02-27 21:32:00 +0100

    Rename `foreign` local variable to `foreign_` to remove syntax error on GHC 8.2.2

--- compiler/src/Parse/Primitives/Variable.hs
+++ compiler/src/Parse/Primitives/Variable.hs
@@ -133,14 +133,14 @@ foreignUpper =
       let
         !newState = State fp end terminal indent row newCol ctx
         !name = N.fromForeignPtr fp start (end - start)
-        !foreign =
+        !foreign_ =
           if start == offset then
             Unqualified name
           else
             let !home = N.fromForeignPtr fp offset ((start - 1) - offset) in
             Qualified home name
       in
-      cok foreign newState noError
+      cok foreign_ newState noError
 
 
 foreignUpperHelp :: ForeignPtr Word8 -> Int -> Int -> Int -> (# Int, Int, Int #)
