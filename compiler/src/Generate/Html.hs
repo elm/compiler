bc102dda22b25e5b1d873572c1e9afa478ad27f4
Andrius Každailis <andrius.kazdailevicius@gmail.com>
2019-03-12 17:52:36 +0700

    Reactor viewport initial scale
    elm reactor does not set viewport meta attribute which 
    makes it hard to develop pages for mobile devices.

--- compiler/src/Generate/Html.hs
+++ compiler/src/Generate/Html.hs
@@ -23,6 +23,7 @@ sandwich moduleName javascript =
 <html>
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>|] <> name <> [r|</title>
 </head>
 
