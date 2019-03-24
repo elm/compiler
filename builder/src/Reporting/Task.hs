29b966f7d3cf3a186617adc9754a43aa4de5a129
Evan Czaplicki <evancz@users.noreply.github.com>
2019-03-24 14:57:41 -0400

    Decouple reporting from tasks, tasks are just for error tracking

--- builder/src/Reporting/Task.hs
+++ builder/src/Reporting/Task.hs
@@ -1,78 +1,34 @@
-{-# LANGUAGE OverloadedStrings, Rank2Types #-}
+{-# LANGUAGE Rank2Types #-}
 module Reporting.Task
   ( Task
   , run
   , throw
-  -- progress
-  , notify
-  , ask
-  -- io
+  --
   , io
   , eio
-  , withKey
   )
   where
-
-
-import qualified System.Exit as Exit
-
-import qualified Reporting.Doc as D
-import qualified Reporting.Exit as Exit
-import qualified Reporting.Progress as Progress
 
 
 
 -- TASKS
 
 
-newtype Task a =
+newtype Task x a =
   Task
   (
-    forall result.
-      Env
-      -> (a -> IO result)
-      -> (Exit.Exit -> IO result)
-      -> IO result
+    forall result. (a -> IO result) -> (x -> IO result) -> IO result
   )
 
 
-data Env =
-  Env
-    { _tell :: Progress.Progress -> IO ()
-    , _ask  :: D.Doc -> IO Bool
-    }
+run :: Task x a -> IO (Either x a)
+run (Task task) =
+  task (return . Right) (return . Left)
 
 
-run :: Progress.Reporter -> Task a -> IO ()
-run (Progress.Reporter pTell pAsk pEnd) (Task task) =
-  task (Env pTell pAsk)
-    (\_ ->
-        do  pEnd Nothing
-            return ()
-    )
-    (\exit ->
-        do  pEnd (Just exit)
-            Exit.exitFailure
-    )
-
-
-throw :: Exit.Exit -> Task a
-throw exit =
-  Task $ \_ _ err -> err exit
-
-
-
--- PROGRESS
-
-
-notify :: Progress.Progress -> Task ()
-notify progress =
-  Task $ \(Env pTell _) ok _ -> pTell progress >>= ok
-
-
-ask :: D.Doc -> Task Bool
-ask question =
-  Task $ \(Env _ pAsk) ok _ -> pAsk question >>= ok
+throw :: x -> Task x a
+throw x =
+  Task $ \_ err -> err x
 
 
 
@@ -80,71 +36,62 @@ ask question =
 
 
 {-# INLINE io #-}
-io :: IO a -> Task a
+io :: IO a -> Task x a
 io work =
-  Task $ \_ ok _ -> work >>= ok
+  Task $ \ok _ -> work >>= ok
 
 
-eio :: (x -> Exit.Exit) -> IO (Either x a) -> Task a
+eio :: (x -> y) -> IO (Either x a) -> Task y a
 eio func work =
-  Task $ \_ ok err ->
+  Task $ \ok err ->
     do  result <- work
         case result of
           Right a -> ok a
-          Left e -> err (func e)
-
-
-withKey :: (Progress.Key -> IO (Either Exit.Exit a)) -> Task a
-withKey callback =
-  Task $ \(Env pTell _) ok err ->
-    do  result <- callback (Progress.Key pTell)
-        case result of
-          Right a -> ok a
-          Left e -> err e
+          Left x -> err (func x)
 
 
 
 -- INSTANCES
 
 
-instance Functor Task where
+instance Functor (Task x) where
   {-# INLINE fmap #-}
   fmap func (Task taskA) =
-    Task $ \env ok err ->
+    Task $ \ok err ->
       let
         okA arg = ok (func arg)
       in
-      taskA env okA err
+      taskA okA err
 
 
-instance Applicative Task where
+instance Applicative (Task x) where
   {-# INLINE pure #-}
   pure a =
-    Task $ \_ ok _ -> ok a
+    Task $ \ok _ -> ok a
 
   {-# INLINE (<*>) #-}
   (<*>) (Task taskFunc) (Task taskArg) =
-    Task $ \env ok err ->
+    Task $ \ok err ->
       let
         okFunc func =
           let
             okArg arg = ok (func arg)
           in
-          taskArg env okArg err
+          taskArg okArg err
       in
-      taskFunc env okFunc err
+      taskFunc okFunc err
 
 
-instance Monad Task where
+instance Monad (Task x) where
   {-# INLINE return #-}
   return = pure
 
   {-# INLINE (>>=) #-}
   (>>=) (Task taskA) callback =
-    Task $ \env ok err ->
+    Task $ \ok err ->
       let
         okA a =
           case callback a of
-            Task taskB -> taskB env ok err
+            Task taskB -> taskB ok err
       in
-      taskA env okA err
+      taskA okA err
