429d4b62eedd5950926e9277fd6f80b24eda2a09
Evan Czaplicki <evancz@users.noreply.github.com>
2019-06-03 12:48:43 -0700

    use D.customString and D.KeyDecoder in Deps.Registry
    Cleans things up a decent bit when it comes to validating keys with
    complex structure.

--- builder/src/Deps/Registry.hs
+++ builder/src/Deps/Registry.hs
@@ -16,11 +16,8 @@ module Deps.Registry
 import Prelude hiding (read)
 import Control.Monad (liftM2)
 import Data.Binary (Binary, get, put)
-import Data.Function (on)
 import qualified Data.List as List
 import qualified Data.Map.Strict as Map
-import qualified Data.Name as Name
-import qualified Data.Utf8 as Utf8
 
 import qualified Deps.Website as Website
 import qualified Elm.Package as Pkg
@@ -28,8 +25,8 @@ import qualified Elm.Version as V
 import qualified File
 import qualified Http
 import qualified Json.Decode as D
+import qualified Parse.Primitives as P
 import qualified Reporting.Exit as Exit
-import qualified Reporting.Suggest as Suggest
 import qualified Stuff
 
 
@@ -86,26 +83,19 @@ addEntry (KnownVersions _ vs) count =
 
 allPkgsDecoder :: D.Decoder () (Map.Map Pkg.Name KnownVersions)
 allPkgsDecoder =
-  do  pairs <- D.pairs (D.list (D.mapError (\_ -> ()) V.decoder))
-      checkKeys pairs Map.empty
-  where
-    checkKeys pairs pkgs =
-      case pairs of
-        [] ->
-          return pkgs
+  let
+    keyDecoder =
+      Pkg.keyDecoder bail
 
-        (key, versions) : rest ->
-          case Pkg.fromString key of
-            Right pkg ->
-              case List.sortBy (flip compare) versions of
-                [] ->
-                  D.failure ()
+    versionsDecoder =
+      D.list (D.mapError (\_ -> ()) V.decoder)
 
-                v:vs ->
-                  checkKeys rest (Map.insert pkg (KnownVersions v vs) pkgs)
-
-            Left _ ->
-              D.failure ()
+    toKnownVersions versions =
+      case List.sortBy (flip compare) versions of
+        v:vs -> return (KnownVersions v vs)
+        []   -> D.failure ()
+  in
+  D.dict keyDecoder (toKnownVersions =<< versionsDecoder)
 
 
 
@@ -144,22 +134,26 @@ addNew (name, version) versions =
   Map.alter (Just . add) name versions
 
 
+
+-- NEW PACKAGE DECODER
+
+
 newPkgDecoder :: D.Decoder () (Pkg.Name, V.Version)
 newPkgDecoder =
-  do  str <- D.string
-      case Utf8.split 0x40 {-@-} str of
-        [key, value] ->
-          case Pkg.fromString key of
-            Right pkg ->
-              case V.fromString value of
-                Just vsn -> return (pkg, vsn)
-                Nothing -> D.failure ()
+  D.customString newPkgParser bail
 
-            Left _ ->
-              D.failure ()
 
-        _ ->
-          D.failure ()
+newPkgParser :: P.Parser () (Pkg.Name, V.Version)
+newPkgParser =
+  do  pkg <- P.specialize (\_ _ _ -> ()) Pkg.parser
+      P.word1 0x40 {-@-} bail
+      vsn <- P.specialize (\_ _ _ -> ()) V.parser
+      return (pkg, vsn)
+
+
+bail :: row -> col -> ()
+bail _ _ =
+  ()
 
 
 
@@ -190,33 +184,7 @@ getVersions' :: Pkg.Name -> Registry -> Either [Pkg.Name] KnownVersions
 getVersions' name (Registry _ versions) =
   case Map.lookup name versions of
     Just kvs -> Right kvs
-    Nothing -> Left $ nearbyNames name (Map.keys versions)
-
-
-nearbyNames :: Pkg.Name -> [Pkg.Name] -> [Pkg.Name]
-nearbyNames (Pkg.Name author1 project1) possibleNames =
-  let
-    authorDist = authorDistance (Name.toChars author1)
-    projectDist = projectDistance (Name.toChars project1)
-
-    addDistance name@(Pkg.Name author2 project2) =
-      ( authorDist author2 + projectDist project2, name )
-  in
-  map snd $ take 4 $
-    List.sortBy (compare `on` fst) $
-      map addDistance possibleNames
-
-
-authorDistance :: String -> Name.Name -> Int
-authorDistance bad possibility =
-  abs (Suggest.distance bad (Name.toChars possibility))
-
-
-projectDistance :: String -> Name.Name -> Int
-projectDistance bad possibility =
-  if possibility == "elm" || possibility == "elm-explorations"
-  then 0
-  else abs (Suggest.distance bad (Name.toChars possibility))
+    Nothing -> Left $ Pkg.nearbyNames name (Map.keys versions)
 
 
 
