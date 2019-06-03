18820a0d01454ea536fb044f4fedceaa30dcce86
Evan Czaplicki <evancz@users.noreply.github.com>
2019-06-03 11:12:11 -0700

    start using D.customString in Elm.Package
    This also introduces Pkg.keyDecoder for decoding constraints and
    versions in elm.json
    
    I also moved some code for finding nearby names into this module since
    the AUTHOR and PROJECT types are internal to the module. This actually
    revealed a bug in which the check for elm and elm_explorations was
    actually happening in the projectDistance function, which should never
    happen!

--- compiler/src/Elm/Package.hs
+++ compiler/src/Elm/Package.hs
@@ -1,38 +1,53 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns, EmptyDataDecls, FlexibleInstances, UnboxedTuples #-}
 module Elm.Package
   ( Name(..)
+  , Author
+  , Project
   , Canonical(..)
   , isKernel
-  , toString
   , toChars
   , toUrl
   , toFilePath
-  , BadName(..)
-  , fromString
+  , toJsonString
+  --
   , dummyName, kernel, core
   , browser, virtualDom, html
   , json, http, url
   , webgl, linearAlgebra
+  --
   , suggestions
+  , nearbyNames
+  --
   , decoder
   , encode
+  , keyDecoder
+  --
+  , parser
   )
   where
 
 
 import Control.Monad (liftM2)
 import Data.Binary (Binary, get, put)
-import qualified Data.Char as Char
-import qualified Data.Name as Name
+import qualified Data.Coerce as Coerce
+import Data.Function (on)
+import qualified Data.List as List
 import qualified Data.Map as Map
+import qualified Data.Name as Name
 import Data.Monoid ((<>))
 import qualified Data.Utf8 as Utf8
+import Data.Word (Word8)
+import Foreign.Ptr (Ptr, plusPtr, minusPtr)
 import System.FilePath ((</>))
 
 import qualified Elm.Version as V
-import qualified Json.Decode as Decode
-import qualified Json.Encode as Encode
+import qualified Json.Decode as D
+import qualified Json.Encode as E
+import qualified Json.String as Json
+import qualified Parse.Primitives as P
+import Parse.Primitives (Row, Col)
+import qualified Reporting.Suggest as Suggest
 
 
 
@@ -41,9 +56,16 @@ import qualified Json.Encode as Encode
 
 data Name =
   Name
-    { _author :: !Name.Name
-    , _project :: !Name.Name
+    { _author :: !Author
+    , _project :: !Project
     }
+
+
+type Author = Utf8.Utf8 AUTHOR
+type Project = Utf8.Utf8 PROJECT
+
+data AUTHOR
+data PROJECT
 
 
 data Canonical =
@@ -60,127 +82,114 @@ data Canonical =
 
 isKernel :: Name -> Bool
 isKernel (Name author _) =
-  author == "elm" || author == "elm-explorations"
-
-
-toString :: Name -> Utf8.String
-toString (Name author project) =
-  Utf8.join 0x2F [ Name.toUtf8 author, Name.toUtf8 project ]
+  author == elm || author == elm_explorations
 
 
 toChars :: Name -> String
 toChars (Name author project) =
-  Name.toChars author <> "/" <> Name.toChars project
+  Utf8.toChars author <> "/" <> Utf8.toChars project
 
 
 toUrl :: Name -> String
 toUrl (Name author project) =
-  Name.toChars author ++ "/" ++ Name.toChars project
+  Utf8.toChars author ++ "/" ++ Utf8.toChars project
 
 
 toFilePath :: Name -> FilePath
 toFilePath (Name author project) =
-  Name.toChars author </> Name.toChars project
+  Utf8.toChars author </> Utf8.toChars project
 
 
-fromString :: Utf8.String -> Either BadName Name
-fromString str =
-  case Utf8.split 0x2F {- / -} str of
-    [author, project]
-      | Utf8.isEmpty author                      -> Left BadStructure
-      | Utf8.isEmpty project                     -> Left BadStructure
-      | Utf8.size project >= 256                 -> Left TooLong
-      | Utf8.containsDouble 0x2D {- - -} project -> Left HasDoubleDash
-      | Utf8.contains       0x5F {- _ -} project -> Left HasUnderscore
-      | Utf8.contains       0x2E {- . -} project -> Left HasDot
-      | Utf8.any Char.isUpper            project -> Left HasUppercase
-      | Utf8.endsWithWord   0x2D {- - -} project -> Left HasDashEnd
-      | Utf8.startsWithChar Char.isLower project -> Right (Name (Name.fromUtf8 author) (Name.fromUtf8 project))
-      | True                                     -> Left HasNonLowercaseStart
-
-    _ ->
-      Left BadStructure
-
-
-data BadName
-  = BadStructure
-  | TooLong
-  | HasDoubleDash
-  | HasUnderscore
-  | HasDot
-  | HasUppercase
-  | HasDashEnd
-  | HasNonLowercaseStart
+toJsonString :: Name -> Json.String
+toJsonString (Name author project) =
+  Utf8.join 0x2F {-/-} [ Coerce.coerce author, Coerce.coerce project ]
 
 
 
 -- COMMON PACKAGE NAMES
 
 
+toName :: Author -> [Char] -> Name
+toName author project =
+  Name author (Utf8.fromChars project)
+
+
 {-# NOINLINE dummyName #-}
 dummyName :: Name
 dummyName =
-  Name "author" "project"
+  toName (Utf8.fromChars "author") "project"
 
 
 {-# NOINLINE kernel #-}
 kernel :: Name
 kernel =
-  Name "elm" "kernel"
+  toName elm "kernel"
 
 
 {-# NOINLINE core #-}
 core :: Name
 core =
-  Name "elm" "core"
+  toName elm "core"
 
 
 {-# NOINLINE browser #-}
 browser :: Name
 browser =
-  Name "elm" "browser"
+  toName elm "browser"
 
 
 {-# NOINLINE virtualDom #-}
 virtualDom :: Name
 virtualDom =
-  Name "elm" "virtual-dom"
+  toName elm "virtual-dom"
 
 
 {-# NOINLINE html #-}
 html :: Name
 html =
-  Name "elm" "html"
+  toName elm "html"
 
 
 {-# NOINLINE json #-}
 json :: Name
 json =
-  Name "elm" "json"
+  toName elm "json"
 
 
 {-# NOINLINE http #-}
 http :: Name
 http =
-  Name "elm" "http"
+  toName elm "http"
 
 
 {-# NOINLINE url #-}
 url :: Name
 url =
-  Name "elm" "url"
+  toName elm "url"
 
 
 {-# NOINLINE webgl #-}
 webgl :: Name
 webgl =
-  Name "elm-explorations" "webgl"
+  toName elm_explorations "webgl"
 
 
 {-# NOINLINE linearAlgebra #-}
 linearAlgebra :: Name
 linearAlgebra =
-  Name "elm-explorations" "linear-algebra"
+  toName elm_explorations "linear-algebra"
+
+
+{-# NOINLINE elm #-}
+elm :: Author
+elm =
+  Utf8.fromChars "elm"
+
+
+{-# NOINLINE elm_explorations #-}
+elm_explorations :: Author
+elm_explorations =
+  Utf8.fromChars "elm-explorations"
 
 
 
@@ -190,26 +199,61 @@ linearAlgebra =
 suggestions :: Map.Map Name.Name Name
 suggestions =
   let
-    random = Name "elm" "random"
-    time = Name "elm" "time"
-    file = Name "elm" "file"
+    random = toName elm "random"
+    time = toName elm "time"
+    file = toName elm "file"
   in
   Map.fromList
-    [ ("Browser", browser)
-    , ("File", file)
-    , ("File.Download", file)
-    , ("File.Select", file)
-    , ("Html", html)
-    , ("Html.Attributes", html)
-    , ("Html.Events", html)
-    , ("Http", http)
-    , ("Json.Decode", json)
-    , ("Json.Encode", json)
-    , ("Random", random)
-    , ("Time", time)
-    , ("Url.Parser", url)
-    , ("Url", url)
+    [ "Browser" ==> browser
+    , "File" ==> file
+    , "File.Download" ==> file
+    , "File.Select" ==> file
+    , "Html" ==> html
+    , "Html.Attributes" ==> html
+    , "Html.Events" ==> html
+    , "Http" ==> http
+    , "Json.Decode" ==> json
+    , "Json.Encode" ==> json
+    , "Random" ==> random
+    , "Time" ==> time
+    , "Url.Parser" ==> url
+    , "Url" ==> url
     ]
+
+
+(==>) :: [Char] -> Name -> (Name.Name, Name)
+(==>) moduleName package =
+  ( Utf8.fromChars moduleName, package )
+
+
+
+-- NEARBY NAMES
+
+
+nearbyNames :: Name -> [Name] -> [Name]
+nearbyNames (Name author1 project1) possibleNames =
+  let
+    authorDist = authorDistance (Utf8.toChars author1)
+    projectDist = projectDistance (Utf8.toChars project1)
+
+    addDistance name@(Name author2 project2) =
+      ( authorDist author2 + projectDist project2, name )
+  in
+  map snd $ take 4 $
+    List.sortBy (compare `on` fst) $
+      map addDistance possibleNames
+
+
+authorDistance :: [Char] -> Author -> Int
+authorDistance given possibility =
+  if possibility == elm || possibility == elm_explorations
+  then 0
+  else abs (Suggest.distance given (Utf8.toChars possibility))
+
+
+projectDistance :: [Char] -> Project -> Int
+projectDistance given possibility =
+  abs (Suggest.distance given (Utf8.toChars possibility))
 
 
 
@@ -234,9 +278,13 @@ instance Ord Name where
       GT -> GT
 
 
-instance Binary Name where
-  get = liftM2 Name get get
-  put (Name a b) = put a >> put b
+
+-- BINARY
+
+
+instance Binary Name where -- TODO try storing as a Word16
+  get = liftM2 Name Utf8.getUnder256 Utf8.getUnder256
+  put (Name a b) = Utf8.putUnder256 a >> Utf8.putUnder256 b
 
 
 instance Binary Canonical where
@@ -248,17 +296,89 @@ instance Binary Canonical where
 -- JSON
 
 
-decoder :: Decode.Decoder BadName Name
+decoder :: D.Decoder (Row, Col) Name
 decoder =
-  do  str <- Decode.string
-      case fromString str of
-        Right name ->
-          return name
-
-        Left err ->
-          Decode.failure err
+  D.customString parser (,)
 
 
-encode :: Name -> Encode.Value
+encode :: Name -> E.Value
 encode name =
-  Encode.string (toString name)
+  E.chars (toChars name)
+
+
+keyDecoder :: (Row -> Col -> x) -> D.KeyDecoder x Name
+keyDecoder toError =
+  let
+    keyParser =
+      P.specialize (\(r,c) _ _ -> toError r c) parser
+  in
+  D.KeyDecoder keyParser toError
+
+
+
+-- PARSER
+
+
+parser :: P.Parser (Row, Col) Name
+parser =
+  do  author <- parseName isAlphaOrDigit isAlphaOrDigit
+      P.word1 0x2F {-/-} (,)
+      project <- parseName isLower isLowerOrDigit
+      return (Name author project)
+
+
+parseName :: (Word8 -> Bool) -> (Word8 -> Bool) -> P.Parser (Row, Col) (Utf8.Utf8 t)
+parseName isGoodStart isGoodInner =
+  P.Parser $ \(P.State src pos end indent row col) cok _ cerr eerr ->
+    if pos >= end then
+      eerr row col (,)
+    else
+      let !word = P.unsafeIndex pos in
+      if not (isGoodStart word) then
+        eerr row col (,)
+      else
+        let
+          (# isGood, newPos #) = chompName isGoodInner (plusPtr pos 1) end False
+          !len = fromIntegral (minusPtr newPos pos)
+          !newCol = col + len
+        in
+        if isGood && len < 256 then
+          let !newState = P.State src newPos end indent row newCol in
+          cok (Utf8.fromPtr pos newPos) newState
+        else
+          cerr row newCol (,)
+
+
+isLower :: Word8 -> Bool
+isLower word =
+  0x61 {-a-} <= word && word <= 0x7A {-z-}
+
+
+isLowerOrDigit :: Word8 -> Bool
+isLowerOrDigit word =
+     0x61 {-a-} <= word && word <= 0x7A {-z-}
+  || 0x30 {-0-} <= word && word <= 0x39 {-9-}
+
+
+isAlphaOrDigit :: Word8 -> Bool
+isAlphaOrDigit word =
+     0x61 {-a-} <= word && word <= 0x7A {-z-}
+  || 0x41 {-A-} <= word && word <= 0x5A {-Z-}
+  || 0x30 {-0-} <= word && word <= 0x39 {-9-}
+
+
+chompName :: (Word8 -> Bool) -> Ptr Word8 -> Ptr Word8 -> Bool -> (# Bool, Ptr Word8 #)
+chompName isGoodChar pos end prevWasDash =
+  if pos >= end then
+    (# not prevWasDash, pos #)
+  else
+    let !word = P.unsafeIndex pos in
+    if isGoodChar word then
+      chompName isGoodChar (plusPtr pos 1) end False
+    else if word == 0x2D {---} then
+      if prevWasDash then
+        (# False, pos #)
+      else
+        chompName isGoodChar (plusPtr pos 1) end True
+    else
+      (# True, pos #)
