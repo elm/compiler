92ee73befdcbc28bc30d202fdfbfb64440790679
Evan Czaplicki <evancz@users.noreply.github.com>
2019-06-11 11:34:55 -0400

    update Main now that UTF-8 is handled by Terminal.Args and imports are reduced

--- terminal/src/Main.hs
+++ terminal/src/Main.hs
@@ -6,15 +6,12 @@ module Main
 
 
 import Prelude hiding (init)
-import GHC.IO.Encoding (setLocaleEncoding, utf8)
 import qualified Data.List as List
 import qualified Text.PrettyPrint.ANSI.Leijen as P
 import Text.PrettyPrint.ANSI.Leijen ((<>))
 import Text.Read (readMaybe)
 
-import qualified Elm.Diff as Diff
 import qualified Elm.Version as V
-import qualified Generate.Output as Output
 import Terminal.Args
 import Terminal.Args.Helpers
 
@@ -34,17 +31,16 @@ import qualified Repl
 
 main :: IO ()
 main =
-  do  setLocaleEncoding utf8
-      complex intro outro
-        [ repl
-        , init
-        , reactor
-        , make
-        , install
-        , bump
-        , diff
-        , publish
-        ]
+  complex intro outro
+    [ repl
+    , init
+    , reactor
+    , make
+    , install
+    , bump
+    , diff
+    , publish
+    ]
 
 
 intro :: P.Doc
@@ -196,7 +192,7 @@ make =
       flags Make.Flags
         |-- onOff "debug" "Turn on the time-travelling debugger. It allows you to rewind and replay events. The events can be imported/exported into a file, which makes for very precise bug reports!"
         |-- onOff "optimize" "Turn on optimizations to make code smaller and faster. For example, the compiler renames record fields to be as short as possible and unboxes values to reduce allocation."
-        |-- flag "output" Output.output "Specify the name of the resulting JS file. For example --output=assets/elm.js to generate the JS at assets/elm.js or --output=/dev/null to generate no output at all!"
+        |-- flag "output" Make.output "Specify the name of the resulting JS file. For example --output=assets/elm.js to generate the JS at assets/elm.js or --output=/dev/null to generate no output at all!"
         |-- flag "report" Make.reportType "You can say --report=json to get error messages as JSON. This is only really useful if you are an editor plugin. Humans should avoid it!"
         |-- flag "docs" Make.docsFile "Generate a JSON file of documentation for a package. Eventually it will be possible to preview docs with `reactor` because it is quite hard to deal with these JSON files directly."
   in
