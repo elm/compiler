3e9455d097d171eb7a3a42d535c9d61c53895c65
Evan Czaplicki <evancz@users.noreply.github.com>
2019-07-05 18:28:33 -0400

    get worker/ working
    This includes a couple important changes:
    
    - Switch to using reactor/src/Errors.elm to show error messages. It may
    be better to have two separate versions such that the two can have
    different styles though.
    - get the multipart/form-data logic working. Files are treated
    differently than non-files in foldMulitpart, which was pretty confusing!
    - move the initialization of Elm.Errors into <body> so that it could
    take over that node. Otherwise the code runs before it exists.
    - resolve some of the -Wall errors

--- worker/src/Main.hs
+++ worker/src/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 module Main
   ( main
@@ -5,7 +6,6 @@ module Main
   where
 
 
-import Control.Applicative ((<|>))
 import Control.Concurrent (readMVar)
 import Control.Monad (liftM2, msum)
 import qualified Data.ByteString as B
@@ -20,23 +20,24 @@ import qualified Data.OneOrMore as OneOrMore
 import Network.URI (parseURI)
 import Snap.Core
 import Snap.Http.Server
-import Snap.Util.CORS
-import Snap.Util.FileServe (serveFile)
+import Snap.Util.CORS (CORSOptions(..), HashableMethod(..), OriginList(Origins), applyCORS, mkOriginSet)
 import Snap.Util.FileUploads
 import qualified System.Directory as Dir
-import qualified System.Exit as Exit
 import qualified System.IO.Streams as Stream
 import Text.RawString.QQ (r)
 
 import qualified AST.Source as Src
 import qualified AST.Canonical as Can
 import qualified AST.Optimized as Opt
+import qualified BackgroundWriter as BW
+import qualified Build
 import qualified Compile
 import qualified Elm.Details as Details
 import qualified Elm.Interface as I
 import qualified Elm.ModuleName as ModuleName
 import qualified Elm.Package as Pkg
 import qualified File
+import qualified Generate
 import qualified Generate.Html as Html
 import qualified Generate.JavaScript as Generate
 import qualified Generate.Mode as Mode
@@ -47,6 +48,7 @@ import qualified Reporting.Annotation as A
 import qualified Reporting.Error as Error
 import qualified Reporting.Error.Import as Import
 import qualified Reporting.Exit as Exit
+import qualified Reporting.Task as Task
 
 
 
@@ -56,10 +58,10 @@ import qualified Reporting.Exit as Exit
 main :: IO ()
 main =
   do  artifacts <- loadArtifacts
-      errorJS <- compileErrorViewer artifacts
+      errorJS <- compileErrorViewer
       httpServe config $ msum $
         [ path "compile" $ compile artifacts
-        , path "compile/error.js" $ writeBS errorJS
+        , path "compile/errors.js" $ writeBS errorJS
         , notFound
         ]
 
@@ -67,7 +69,6 @@ main =
 config :: Config Snap a
 config =
   defaultConfig
-    |> setVerbose False
     |> setPort 8000
     |> setAccessLog ConfigNoLog
     |> setErrorLog ConfigNoLog
@@ -96,22 +97,27 @@ notFound =
 compile :: Artifacts -> Snap ()
 compile artifacts =
   applyCORS corsOptions $ method POST $
-    do  parts <- handleMultipart defaultUploadPolicy handlePart
-        case parts of
-          [Just source] ->
-            case compileToBuilder artifacts source of
-              Right builder -> writeBuilder builder
-              Left exit     -> writeBuilder (exitToHtmlBuilder exit)
+    do  result <- foldMultipart defaultUploadPolicy ignoreFile 0
+        case result of
+          ([("code",source)], 0) ->
+            do  modifyResponse $ setContentType "text/html; charset=utf-8"
+                case compileToBuilder artifacts source of
+                  Right builder -> writeBuilder builder
+                  Left exit     -> writeBuilder (exitToHtmlBuilder exit)
 
           _ ->
-            pass
+            do  modifyResponse $ setResponseStatus 400 "Bad Request"
+                modifyResponse $ setContentType "text/html; charset=utf-8"
+                writeBS
+                  "<p>Unexpected request format. This should not be possible!</p>\
+                  \<p>Please report this\
+                  \ <a href=\"https://github.com/elm/compiler/issues\">here</a>\
+                  \ along with the URL and your browser version.</p>"
 
 
-handlePart :: PartInfo -> Stream.InputStream B.ByteString -> IO (Maybe B.ByteString)
-handlePart info stream =
-  if partFieldName info == "code" && partDisposition info == DispositionFormData
-  then Just . LBS.toStrict <$> storeAsLazyByteString stream
-  else return Nothing
+ignoreFile :: PartInfo -> Stream.InputStream B.ByteString -> Int -> IO Int
+ignoreFile _ _ count =
+  return (count + 1)
 
 
 exitToHtmlBuilder :: Exit.Worker -> B.Builder
@@ -124,11 +130,12 @@ exitToHtmlBuilder exit =
 <html>
 <head>
   <meta charset="UTF-8">
-  <style>body { padding: 0; margin: 0; }</style>
-  <script src="https://worker.elm-lang.org/compile/error.js"></script>
-  <script>Elm.Error.init({flags:|] <> json <> [r|})</script>
+  <style>body { padding: 0; margin: 0; background-color: black; }</style>
+  <script src="http://localhost:8000/compile/errors.js"></script>
 </head>
-<body></body>
+<body>
+  <script>Elm.Errors.init({flags:|] <> json <> [r|});</script>
+</body>
 </html>|]
 
 
@@ -152,17 +159,17 @@ compileToBuilder (Artifacts interfaces objects) source =
             Left err ->
               Left $ toInputError (Src.getName modul) source err
 
-            Right artifacts@(Compile.Artifacts modul _ locals) ->
+            Right (Compile.Artifacts canModule _ locals) ->
               case locals of
                 Opt.LocalGraph Nothing _ _ ->
                   Left Exit.WorkerNoMain
 
-                Opt.LocalGraph (Just main) _ _ ->
+                Opt.LocalGraph (Just main_) _ _ ->
                   let
                     mode  = Mode.Dev Nothing
-                    home  = Can._name modul
+                    home  = Can._name canModule
                     name  = ModuleName._module home
-                    mains = Map.singleton home main
+                    mains = Map.singleton home main_
                     graph = Opt.addLocalGraph locals objects
                   in
                   Right $ Html.sandwich name $ Generate.generate mode graph mains
@@ -197,17 +204,21 @@ toInputError name source err =
 -- COMPILE ERROR VIEWER
 
 
-compileErrorViewer :: Artifacts -> IO B.ByteString
-compileErrorViewer artifacts =
-  do  source <- File.readUtf8 "src/Error.elm"
-      case compileToBuilder artifacts source of
-        Left exit ->
-          do  putStrLn "Problem in src/Error.elm"
-              Exit.toStderr (Exit.workerToReport exit)
-              Exit.exitFailure
-
-        Right builder ->
-          return (LBS.toStrict (B.toLazyByteString builder))
+compileErrorViewer :: IO B.ByteString
+compileErrorViewer =
+  let
+    run work =
+      do  result <- work
+          case result of
+            Right a -> return a
+            Left _ -> error "problem building reactor/src/Errors.elm"
+  in
+  Dir.withCurrentDirectory "../reactor" $ BW.withScope $ \scope ->
+    do  let root = "."
+        details <- run $ Details.load Reporting.silent scope root
+        artifacts <- run $ Build.fromMains Reporting.silent root details (NE.List "src/Errors.elm" [])
+        javascript <- run $ Task.run $ Generate.prod root details artifacts
+        return $ LBS.toStrict $ B.toLazyByteString javascript
 
 
 
@@ -250,9 +261,10 @@ data Artifacts =
 
 loadArtifacts :: IO Artifacts
 loadArtifacts =
+  BW.withScope $ \scope ->
   do  style <- Reporting.terminal
       root <- Dir.getCurrentDirectory
-      result <- Details.load style root
+      result <- Details.load style scope root
       case result of
         Left _ ->
           error "Ran into some problem loading elm.json details"
