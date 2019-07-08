8e1ee22a73ca2ed976b386f43dbcf7a36da809e8
Evan Czaplicki <evancz@users.noreply.github.com>
2019-07-08 13:57:35 -0400

    typo: add missing word in a couple type error messages
    fix https://github.com/elm/compiler/pull/1842

--- compiler/src/Reporting/Error/Type.hs
+++ compiler/src/Reporting/Error/Type.hs
@@ -1077,7 +1077,7 @@ opRightToDocs localizer category op tipe expected =
         ( D.reflow $ "I cannot send this through the (<|) pipe:"
         , typeComparison localizer tipe expected
             "The argument is:"
-            "But (<|) is piping it a function that expects:"
+            "But (<|) is piping it to a function that expects:"
             []
         )
 
@@ -1088,7 +1088,7 @@ opRightToDocs localizer category op tipe expected =
             ( D.reflow $ "This function cannot handle the argument sent through the (|>) pipe:"
             , typeComparison localizer argType expectedArgType
                 "The argument is:"
-                "But (|>) is piping it a function that expects:"
+                "But (|>) is piping it to a function that expects:"
                 []
             )
 
