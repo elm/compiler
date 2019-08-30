c404f227011c9a162c9592b6d581084ca8eafa53
Evan Czaplicki <evancz@users.noreply.github.com>
2019-08-30 12:40:03 -0400

    get rid of indirection

--- installers/npm/download.js
+++ installers/npm/download.js
@@ -13,20 +13,12 @@ var zlib = require('zlib');
 
 module.exports = function(destinationPath, callback)
 {
+	// figure out URL of binary
 	var version = package.version.replace(/^(\d+\.\d+\.\d+).*$/, '$1'); // turn '1.2.3-alpha' into '1.2.3'
 	var os = { 'darwin': 'mac', 'win32': 'windows', 'linux': 'linux' }[process.platform];
 	var arch = { 'x64': '64-bit', 'ia32': '32-bit' }[process.arch];
 	var url = 'https://github.com/elm/compiler/releases/download/' + version + '/binary-for-' + os + '-' + arch + '.gz';
-	download(destinationPath, url, callback);
-};
 
-
-
-// DOWNLOAD
-
-
-function download(destinationPath, url, callback)
-{
 	// set up handler for request failure
 	function reportDownloadFailure(error)
 	{
