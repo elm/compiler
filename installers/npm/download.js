f632e0518923f3d5e4b363508d5925b02e315104
Evan Czaplicki <evancz@users.noreply.github.com>
2019-08-30 17:19:21 -0400

    document all the weird facts about Windows

--- installers/npm/download.js
+++ installers/npm/download.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var package = require('./package.json');
+var path = require('path');
 var request = require('request');
 var zlib = require('zlib');
 
@@ -11,13 +12,16 @@ var zlib = require('zlib');
 // called when --ignore-scripts is enabled. That's why install.js is so weird.
 
 
-module.exports = function(destinationPath, callback)
+module.exports = function(callback)
 {
 	// figure out URL of binary
 	var version = package.version.replace(/^(\d+\.\d+\.\d+).*$/, '$1'); // turn '1.2.3-alpha' into '1.2.3'
 	var os = { 'darwin': 'mac', 'win32': 'windows', 'linux': 'linux' }[process.platform];
 	var arch = { 'x64': '64-bit', 'ia32': '32-bit' }[process.arch];
 	var url = 'https://github.com/elm/compiler/releases/download/' + version + '/binary-for-' + os + '-' + arch + '.gz';
+
+	// figure out where to put the binary (calls path.resolve() to get path separators right on Windows)
+	var binaryPath = path.resolve(__dirname, package.bin) + (process.platform === 'win32' ? '.exe' : '');
 
 	// set up handler for request failure
 	function reportDownloadFailure(error)
@@ -31,7 +35,7 @@ module.exports = function(destinationPath, callback)
 	});
 
 	// set up file write pipe
-	var write = fs.createWriteStream(destinationPath, {
+	var write = fs.createWriteStream(binaryPath, {
 		encoding: 'binary',
 		mode: 0o755
 	}).on('finish', callback).on('error', function(error) {
--- installers/npm/install.js
+++ installers/npm/install.js
@@ -1,10 +1,4 @@
 
 var download = require('./download.js');
-var package = require('./package.json');
-var path = require('path');
 
-
-// get slashes right for Windows
-var targetPath = path.resolve(__dirname, package.bin);
-
-download(targetPath, function() {});
+download(function() {});
